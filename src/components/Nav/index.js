e0934841f900c1e75b5c3337302a972a43f7b663
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2019-04-02 18:01:01 +0300

    Add header Nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import {mainNav} from 'appConfig'
 import {NavLink} from 'react-router-dom'
+import './index.scss'
 
 const Nav = () => <nav>
-    <ul id="main-navigation">
+    <ul className="main-nav">
         {
             mainNav.map(navItem =>
-                <li key={navItem.to}>
+                <li className="main-nav__item" key={navItem.to}>
                     <NavLink to={navItem.to}>{navItem.title}</NavLink>
                 </li>
             )
