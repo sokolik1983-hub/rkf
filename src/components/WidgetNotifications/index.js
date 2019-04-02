8a6620ca17a2769173b70a5d1353a0e9dbe0abd1
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2019-04-02 17:57:05 +0300

    Add header Notification Widget

---
+++ src/components/WidgetNotifications/index.js
@@ -0,0 +1,39 @@
+import React, {PureComponent} from 'react'
+import {RingBell} from './RingBell'
+import './index.scss'
+
+export default class WidgetNotifications extends PureComponent {
+    static defaultProps = {
+        notifications: ['some notification']
+    };
+    state = {
+        showNotifications: false
+    };
+
+    toggleShowNotifications = () => {
+        this.setState(prevState => ({showNotifications: !prevState.showNotifications}))
+    };
+
+    render() {
+        const {notifications} = this.props;
+        return <div onClick={this.toggleShowNotifications} className="notifications-widget">
+            <RingBell notifications={notifications.length > 0}
+
+            />
+            {
+                (notifications && this.state.showNotifications) ?
+                    <div className="notifications-widget__notifications">
+                        {
+                            notifications.map(notification =>
+                                <div
+                                    key={notification.id}
+                                    className="notifications-widget__notification"
+                                >{notification.text}</div>
+                            )
+                        }
+                    </div>
+                    : null
+            }
+        </div>
+    }
+}
