20aac1166a68841cbbdbf09c994f2775150aa6e1
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2019-04-05 14:00:35 +0300

    quick fix LangWidget

--- src/components/WidgetLang/index.js
+++ src/components/WidgetLang/index.js
@@ -3,6 +3,7 @@ import './index.scss'
 
 const Lang = ({currentLang, children, langCode, onClick}) => {
     const handleClick = () => {
+        console.log(onClick, langCode)
         onClick(langCode)
     };
     const isActive = () => (currentLang === langCode);
@@ -18,15 +19,18 @@ export default class WidgetLang extends PureComponent {
     state = {
         currentLang: 'ru',
     };
+    onLangClick = (lang) => {
+        this.setState({currentLang: lang})
+    };
 
     render() {
         const {currentLang} = this.state;
         return (
 
             <div className="lang-widget">
-                <Lang currentLang={currentLang} langCode="ru">Рус</Lang>
-                <span>&nbsp;/&nbsp;</span>
-                <Lang currentLang={currentLang} langCode="en">Eng</Lang>
+                <Lang onClick={this.onLangClick} currentLang={currentLang} langCode="ru">Рус</Lang>
+                <span>&nbsp;|&nbsp;</span>
+                <Lang onClick={this.onLangClick} currentLang={currentLang} langCode="en">Eng</Lang>
             </div>
         )
     }
