5dd6ac6128f37a6dbcd1fb91f6f1b5de705fc4b6
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2019-04-08 13:40:05 +0300

    refactor filter and make it interactive

--- src/apps/Exhibitions/components/FilterDate/index.js
+++ src/apps/Exhibitions/components/FilterDate/index.js
@@ -2,17 +2,56 @@ import React from "react";
 import Button from 'components/Button'
 import './index.scss'
 
-const FilterDateRange = () => (
-    <div className="exhibition-list__header">
-        <div className="exhibition-list__date-range-filter">
-            <button>Архив</button>
-            <button>Год</button>
-            <button>Месяц</button>
-            <button>Неделя</button>
-            <button>Сегодня</button>
-        </div>
-        <Button className={'exhibition-list__btn-create btn-primary btn-lg'}>Создать выставку</Button>
-    </div>
-);
 
-export default FilterDateRange;
+class CustomButton extends React.PureComponent {
+    onClick = () => {
+        this.props.onClick(this.props.type)
+    };
+    getClassName = () => this.props.filter === this.props.type ? "exhibition-list__btn  exhibition-list__btn--active" : ' exhibition-list__btn'
+
+    render() {
+        return <button
+            onClick={this.onClick}
+            className={this.getClassName()}
+        >{this.props.children}</button>
+    }
+}
+
+export default class FilterDateRange extends React.PureComponent {
+    state = {
+        filter: 'week',
+    };
+    onClick = (filter) => {
+        this.setState({filter: filter})
+    };
+
+
+    render() {
+        return (
+            <div className="exhibition-list__header">
+                <div className="exhibition-list__date-range-filter">
+                    <button className="exhibition-list__btn exhibition-list__btn--archive">Архив</button>
+                    <CustomButton
+                        type={'year'}
+                        filter={this.state.filter}
+                        onClick={this.onClick}
+                    >Год
+                    </CustomButton>
+                    <CustomButton
+                        type={'month'}
+                        filter={this.state.filter}
+                        onClick={this.onClick}>Месяц</CustomButton>
+                    <CustomButton
+                        type={'week'}
+                        filter={this.state.filter}
+                        onClick={this.onClick}>Неделя</CustomButton>
+                    <CustomButton
+                        type={'today'}
+                        filter={this.state.filter}
+                        onClick={this.onClick}>Сегодня</CustomButton>
+                </div>
+                <Button className={'exhibition-list__btn-create btn-primary btn-lg'}>Создать выставку</Button>
+            </div>
+        );
+    }
+}
