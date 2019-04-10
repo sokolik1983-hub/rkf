aa1e7e86e1f6cae9c4bbb0bec7567055df7e25e4
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2019-04-10 17:11:49 +0300

    add data file and render data to details

--- src/apps/Exhibitions/containers/Details/index.js
+++ src/apps/Exhibitions/containers/Details/index.js
@@ -1,9 +1,9 @@
 import React, {PureComponent} from 'react'
 
 import DetailsLayout from 'apps/Exhibitions/components/DetailsLayout'
-
+import {exhibitionDetails} from './data'
 export default class ExhibitionDetails extends PureComponent {
     render() {
-        return <DetailsLayout/>
+        return <DetailsLayout exhibitionDetails={exhibitionDetails}/>
     }
 }
