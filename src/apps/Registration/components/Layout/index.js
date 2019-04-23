4375f8b85fc27bff2ac4d5ed6efe3c2f9d25d016
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2019-04-23 10:16:47 +0300

    mock other forms

--- src/apps/Registration/components/Layout/index.js
+++ src/apps/Registration/components/Layout/index.js
@@ -1,11 +1,9 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
-
-import Button from 'components/Button'
-import InputPassword from 'components/Form/InputPassword'
-import FormInput from 'components/Form/FormInput'
-import FormGroup from 'components/Form/FormGroup'
-import InputPhone from 'components/Form/InputPhone'
+import {
+    RegistrationForm,
+    RegistrationFormIP,
+} from '../Form'
 import Tabs from "components/Tabs";
 import {TabContent} from "components/Tabs";
 
@@ -27,81 +25,25 @@ export default class RegistrationLayout extends Component {
             <div content className="registration__wrap">
                 <div className="registration__image"><img src="/static/images/registration/banner.png"/></div>
                 <div content className="registration__holder">
-                    <div className= "registration__logo"/>
+                    <div className="registration__logo"/>
                     <div className="registration__title">Регистрацая</div>
                     <Tabs className="registration__tabs">
                         <TabContent label="Я - владелец">
-                            <form>
-                                <FormGroup inline>
-                                    <FormInput>
-                                        <label>Имя</label>
-                                        <input
-                                            className="form-input__input"
-                                            type="text"
-                                            placeholder="Иван"
-                                        />
-                                    </FormInput>
-                                    <FormInput>
-                                        <label>Фамилия</label>
-                                        <input
-                                            className="form-input__input"
-                                            type="text"
-                                            placeholder="Иванов"
-                                        />
-                                    </FormInput>
-                                </FormGroup>
-                                <FormGroup inline>
-                                    <FormInput>
-                                        <label>E-mail</label>
-                                        <input
-                                            className="form-input__input"
-                                            type="email"
-                                            placeholder="Ведите вашу личную почту"
-                                        />
-                                    </FormInput>
-                                    <FormInput>
-                                        <label>Пароль</label>
-                                        <InputPassword
-                                            className="form-input__input"
-                                            placeholder="Минимум 8 символов"
-                                        />
-                                    </FormInput>
-                                </FormGroup>
-                                <FormGroup inline>
-                                    <FormInput>
-                                        <label>Телефон</label>
-                                        <InputPhone
-                                            className="form-input__input"
-                                            type="text"
-                                            placeholder="7 ( ) ___ __ __"
-                                        />
-                                    </FormInput>
-                                    <FormInput>
-                                        <label>Проверочный код</label>
-                                        <input
-                                            className="form-input__input"
-                                            type="password"
-                                            placeholder="******"
-                                        />
-                                    </FormInput>
-                                </FormGroup>
-                                <div className="form-controls">
-                                    <Button className="btn-primary btn-lg">Зарегистрироваться</Button>
-                                </div>
-                            </form>
+                            <RegistrationForm/>
                         </TabContent>
                         <TabContent label="Я - заводчик">
-                            Я - заводчик
+                            <RegistrationFormIP/>
                         </TabContent>
                         <TabContent label="Я - клуб">
-                            Я - клуб
+                            <RegistrationFormIP/>
                         </TabContent>
                         <TabContent label="Я - питомник">
-                            Я - питомник
+                            <RegistrationFormIP/>
                         </TabContent>
                     </Tabs>
                     <div className="registration__signature">
-                        Нажимая кнопку «Зарегистрироваться», вы принимаете <Link to="/">Условия использования</Link> и&nbsp;<Link to="/">Политику конфиденциальности</Link>
+                        Нажимая кнопку «Зарегистрироваться», вы принимаете <Link to="/">Условия
+                        использования</Link> и&nbsp;<Link to="/">Политику конфиденциальности</Link>
                     </div>
                     <div className="registration__switch-to-login">
                         Уже есть аккаунт? <Link to="/login">Войти</Link>
