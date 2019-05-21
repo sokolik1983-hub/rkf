36f553bf8a93a5bf986ba2bd5d5c0360caf25e76
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2019-05-21 11:33:43 +0300

    move common Login/Registration layout parts to components

---
+++ src/apps/Registration/components/Registration/index.js
@@ -0,0 +1,64 @@
+import React, {PureComponent} from 'react'
+import {Link} from 'react-router-dom'
+import Tabs,{TabContent} from "components/Tabs";
+import CommonLayout from 'components/Layout/CommonRegistrationLogin'
+import RegistrationForm from 'apps/Registration/containers/Register'
+import {
+    PhysicalPerson,
+    LegalEntity,
+} from '../Form/FormFields'
+
+import {
+    registrationFormPhysicalPerson,
+    registrationFormLegalEntity,
+} from 'apps/Registration/config'
+
+import './style.scss'
+
+export default class Registration extends PureComponent {
+
+    render() {
+        return (
+
+            <CommonLayout image={'/static/images/registration/banner.png'}>
+
+                <div className="registration__logo"/>
+                <div className="registration__title">Регистрацая</div>
+                <Tabs className="registration__tabs">
+                    <TabContent label="Я - владелец">
+                        <RegistrationForm
+                            fields={registrationFormPhysicalPerson.fields}
+                            validationSchema={registrationFormPhysicalPerson.validationSchema}
+                        >
+                            <PhysicalPerson/>
+                        </RegistrationForm>
+                    </TabContent>
+                    <TabContent label="Я - клуб">
+                        <RegistrationForm
+                            fields={registrationFormLegalEntity.fields}
+                            validationSchema={registrationFormLegalEntity.validationSchema}
+                        >
+                            <LegalEntity registrationType="2"/>
+                        </RegistrationForm>
+                    </TabContent>
+                    <TabContent label="Я - питомник">
+                        <RegistrationForm
+                            fields={registrationFormLegalEntity.fields}
+                            validationSchema={registrationFormLegalEntity.validationSchema}
+                        >
+                            <LegalEntity registrationType="3"/>
+                        </RegistrationForm>
+                    </TabContent>
+                </Tabs>
+                <div className="registration__signature">
+                    Нажимая кнопку «Зарегистрироваться», вы принимаете <Link to="/auth/registration/terms">Условия
+                    использования</Link> и&nbsp;<Link to="/auth/registration/policy">Политику конфиденциальности</Link>
+                </div>
+                <div className="registration__switch-to-login">
+                    Уже есть аккаунт? <Link to="/auth/login">Войти</Link>
+                </div>
+
+            </CommonLayout>
+        );
+    }
+}
