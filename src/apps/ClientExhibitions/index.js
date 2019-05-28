334b388f64e109b2a2eae5068b3f92742779e387
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2019-05-28 16:11:17 +0300

    move component

--- src/apps/ClientExhibitions/index.js
+++ src/apps/ClientExhibitions/index.js
@@ -1,18 +1,11 @@
 import React, {PureComponent} from "react"
 import {Route, Switch, Link} from 'react-router-dom'
 import ExhibitionsCreate from './components/Create'
+import ExhibitionDetails from './containers/Details'
+import ExhibitionList from './containers/List'
 import {ClientExhibitionsPathContext} from './context'
 
-const List = () => <ClientExhibitionsPathContext.Consumer>{
-    ({path}) =>
-        <div>
 
-            <div style={{textAlign:'right'}}><Link className="btn btn-primary" to={`${path}/add`}>Создать выставку</Link></div>
-            exhibitions list
-        </div>
-}
-
-</ClientExhibitionsPathContext.Consumer>
 
 const Edit = () => <div>exhibitions edit</div>
 
@@ -23,8 +16,8 @@ class ClientExhibitionsProxy extends PureComponent {
             <ClientExhibitionsPathContext.Provider value={{path}}>
                 <Switch>
                     <Route path={`${path}/add`} component={ExhibitionsCreate}/>
-                    <Route path={`${path}/:id/edit`} component={Edit}/>
-                    <Route path={`${path}`} component={List}/>
+                    <Route path={`${path}/:id/details`} component={ExhibitionDetails}/>
+                    <Route path={`${path}`} component={ExhibitionList}/>
                 </Switch>
             </ClientExhibitionsPathContext.Provider>
         );
