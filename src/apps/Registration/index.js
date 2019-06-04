0b5963292ae614be28179c0566e475873a64bab9
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2019-06-04 13:12:25 +0300

    Add confirm RegSuccess RegFailed

--- src/apps/Registration/index.js
+++ src/apps/Registration/index.js
@@ -1,18 +1,29 @@
 import React, {PureComponent} from "react";
+import {Switch, Route} from 'react-router-dom'
 import {compose} from "redux";
 
 import Registration from './components/Registration'
-
+import ConfirmRegistrationSuccess from './components/ConfirmRegistrationSuccess'
+import ConfirmRegistrationFailed from './components/ConfirmRegistrationFailed'
 import injectReducer from 'utils/injectReducer'
 import injectSaga from 'utils/injectSaga'
 import reducer from './reducer'
 import saga from "./saga";
+import {RegistrationPathContext} from './context'
 
 class RegistrationProxy extends PureComponent {
 
     render() {
+        //console.log(this.props);
+        const {path} = this.props.match;
         return (
-            <Registration/>
+            <RegistrationPathContext.Provider value={{path}}>
+                <Switch>
+                    <Route exact path={`${path}/confirm/success`} component={ConfirmRegistrationSuccess}/>
+                    <Route exact path={`${path}/confirm/failed`} component={ConfirmRegistrationFailed}/>
+                    <Route path={path} component={Registration}/>
+                </Switch>
+            </RegistrationPathContext.Provider>
         )
     }
 }
