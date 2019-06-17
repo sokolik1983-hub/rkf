6203f7c555739cfcf4ba4eeca76ad4b7d598432f
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2019-06-17 15:34:50 +0300

    fix

--- src/apps/Auth/index.js
+++ src/apps/Auth/index.js
@@ -1,11 +1,7 @@
 import React, {Component} from "react";
-import {compose} from "redux";
 import {connect} from "react-redux";
 import {Redirect} from 'react-router-dom'
 import Login from './components/Login'
-import injectSaga from 'utils/injectSaga'
-import saga from "./saga";
-
 
 class AuthorizationProxy extends Component {
     render() {
@@ -15,16 +11,7 @@ class AuthorizationProxy extends Component {
             <Login/>
     }
 }
-
 const mapStateToProps = state => ({
     isAuthenticated: state.authentication.isAuthenticated,
 });
-const withConnect = connect(
-    mapStateToProps,
-);
-const withSaga = injectSaga({key: 'authorization', saga});
-
-export default compose(
-    withConnect,
-    withSaga,
-)(AuthorizationProxy)
+export default connect(mapStateToProps)(AuthorizationProxy)
