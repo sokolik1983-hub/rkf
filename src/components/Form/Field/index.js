8818568c4cdebade8935b7815485702ee07d8073
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2019-06-19 12:06:05 +0300

    rename components

--- src/components/Form/Field/index.js
+++ src/components/Form/Field/index.js
@@ -4,36 +4,35 @@ import classnames from 'classnames'
 import FormInput from 'components/Form/FormInput'
 import FieldError from './Error'
 import Label from './Label'
-import ImageInput from './image'
+import ImageInput from './ImageInput'
 import TextArea from "./textarea";
-import MaskedField from './masked'
-import EmailField from './email'
-import PhoneField from './phone'
-import ReactSelect from './select'
-import ReactSelectAsync from './AsyncSelect'
+import MaskedField from './MaskedInput'
+import CustomEmail from './CustomEmail'
+import CustomPhone from './CustomPhone'
+import ReactSelect from './ReactSelect'
+import ReactSelectAsync from './ReactSelectAsync'
+
 
 class FastFormField extends PureComponent {
     static defaultProps = {
         type: "text",
     };
     getComponent = () => {
-        if (this.props.mask) {
-
-            return MaskedField
-        }
         switch (this.props.fieldType) {
             case "textarea":
                 return TextArea;
             case "image":
                 return ImageInput;
             case "customEmail":
-                return EmailField;
+                return CustomEmail;
             case "customPhone":
-                return PhoneField;
+                return CustomPhone;
             case "reactSelect":
                 return ReactSelect;
             case "reactSelectAsync":
                 return ReactSelectAsync;
+            case "masked":
+                return MaskedField;
             default:
                 return Field;
         }
@@ -42,19 +41,20 @@ class FastFormField extends PureComponent {
     render() {
         const FieldInput = this.getComponent();
         const {fieldType, className, style, ...fieldProps} = this.props;
+        console.log(fieldProps)
         return (
             <FormInput
                 style={style}
                 name={fieldProps.name}
                 className={classnames(
                     {[className]: className},
-                    {[fieldProps.type]: fieldProps.type},
+                    {[`formInput--${fieldProps.type}`]: fieldProps.type},
                 )}
             >
                 <Label field={fieldProps}/>
                 <FieldInput
                     id={fieldProps.name}
-                    className={'form-input__input'}
+                    className={'formInput__input'}
                     {...fieldProps}
                 />
                 <FieldError name={fieldProps.name}/>
