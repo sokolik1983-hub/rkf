584c1fc4266769d055bbebe7b00b788497ec91e3
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2019-07-01 11:47:56 +0300

    quick fix set common parts

--- src/apps/Registration/config.js
+++ src/apps/Registration/config.js
@@ -1,12 +1,66 @@
 import {object, string} from 'yup'
-
-import {
-    DEFAULT_PHONE_INPUT_PLACEHOLDER
-} from 'appConfig'
-
+import {VALIDATE_EMAIL, VALIDATE_PHONE, validationTestAsync} from 'utils/validationRequest'
+import {DEFAULT_PHONE_INPUT_PLACEHOLDER, DEFAULT_PHONE_INPUT_MASK} from 'appConfig'
+// vars
 export const defaultReduxKey = 'registration';
-
 export const registrationSuccessPath = '/auth/registration/success';
+
+// default .test() configs
+const testPasswordLength = [
+    'Длина пароля', 'Ваш пароль короче 8 символов',
+    value => value && value.length > 7];
+const testEmailAsync = [
+    'Email тест', 'Данная почта уже существует',
+    value => value ? validationTestAsync({url: VALIDATE_EMAIL, name: 'email', value}) : void 0
+];
+const testPhoneAsync = [
+    '', 'Данный номер телефона уже существует',
+    value => value ? validationTestAsync({url: VALIDATE_PHONE, name: 'phone_number', value}) : void 0
+];
+
+
+const commonValidations = {
+    email: string()
+        .required('Поле не может быть пустым')
+        .email('Неверный формат электронного адреса')
+        .test(...testEmailAsync),
+    phone_number: string()
+        .required('Поле не может быть пустым')
+        .test(...testPhoneAsync),
+    password: string()
+        .required('Поле не может быть пустым')
+        .test(...testPasswordLength)
+};
+
+const commonFields = {
+    email: {
+        name: 'email',
+        type: 'text',
+        label: 'E-mail',
+        placeholder: 'Введите вашу личную почту',
+    },
+    phone_number: {
+        name: 'phone_number',
+        type: 'text',
+        label: 'Телефон',
+        placeholder: DEFAULT_PHONE_INPUT_PLACEHOLDER,
+        mask: DEFAULT_PHONE_INPUT_MASK,
+        fieldType: 'masked'
+    },
+    password: {
+        name: 'password',
+        type: 'password',
+        label: 'Пароль',
+        placeholder: 'Минимум 8 символов'
+    },
+    submit_phone_code: {
+        name: 'submit_phone_code',
+        type: 'text',
+        label: 'Проверочный код',
+        placeholder: '******'
+    },
+};
+
 
 export const registrationFormPhysicalPerson = {
     fields: {
@@ -27,48 +81,14 @@ export const registrationFormPhysicalPerson = {
             label: 'Фамилия',
             placeholder: 'Иванов'
         },
-        email: {
-            name: 'email',
-            type: 'text',
-            label: 'E-mail',
-            placeholder: 'Введите вашу личную почту',
-            fieldType: 'customEmail'
-        },
-        password: {
-            name: 'password',
-            type: 'password',
-            label: 'Пароль',
-            placeholder: 'Минимум 8 символов'
-        },
-        phone_number: {
-            name: 'phone_number',
-            type: 'text',
-            label: 'Телефон',
-            placeholder: DEFAULT_PHONE_INPUT_PLACEHOLDER,
-            fieldType: 'customPhone'
-        },
-        submit_phone_code: {
-            name: 'submit_phone_code',
-            type: 'text',
-            label: 'Проверочный код',
-            placeholder: '* * * * * *'
-        }
+        ...commonFields
     },
     validationSchema: object().shape({
         first_name: string()
             .required('Поле не может быть пустым'),
         second_name: string()
             .required('Поле не может быть пустым'),
-        email: string()
-            .required('Поле не может быть пустым')
-            .email('Неверный формат электронного адреса'),
-        phone_number: string()
-            .required('Поле не может быть пустым'),
-        password: string()
-            .required('Поле не может быть пустым')
-            .test('', 'Ваш пароль короче 8 символов',
-                value => value && value.length > 8
-            ),
+        ...commonValidations,
     }),
 };
 export const registrationFormLegalEntity = {
@@ -95,48 +115,14 @@ export const registrationFormLegalEntity = {
                 {label: "Региональный", value: 2}
             ]
         },
-        email: {
-            name: 'email',
-            type: 'text',
-            label: 'E-mail',
-            placeholder: 'Введите вашу личную почту',
-            fieldType: 'customEmail'
-        },
-        password: {
-            name: 'password',
-            type: 'password',
-            label: 'Пароль',
-            placeholder: 'Минимум 8 символов'
-        },
-        phone_number: {
-            name: 'phone_number',
-            type: 'text',
-            label: 'Телефон',
-            placeholder: '7 (   ) ___ __ __',
-            fieldType: 'customPhone',
-        },
-        submit_phone_code: {
-            name: 'submit_phone_code',
-            type: 'text',
-            label: 'Проверочный код',
-            placeholder: '******'
-        }
+        ...commonFields
     },
     validationSchema: object().shape({
         company_name: string()
             .required('Поле не может быть пустым'),
         company_type: string()
             .required('Поле не может быть пустым'),
-        email: string()
-            .required('Поле не может быть пустым')
-            .email('Неверный формат электронного адреса'),
-        phone_number: string()
-            .required('Поле не может быть пустым'),
-        password: string()
-            .required('Поле не может быть пустым')
-            .test('', 'Ваш пароль короче 8 символов',
-                value => value && value.length > 7
-            ),
+        ...commonValidations,
     }),
 };
 
