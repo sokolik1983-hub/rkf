34f1cd035e704621cb2d1db8a6661b498b7905e1
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2019-07-15 12:38:39 +0300

    start Dictionaries app

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,12 +1,14 @@
 import {combineReducers} from 'redux';
-import { connectRouter } from 'connected-react-router';
+import {connectRouter} from 'connected-react-router';
 import history from 'utils/history';
 import authenticationReducer from 'apps/Auth/reducer'
+import dictionariesReducer from 'apps/Dictionaries/reducer'
 
 export default function createReducer(injectedReducers) {
     return combineReducers({
         router: connectRouter(history),
         authentication: authenticationReducer,
+        dictionaries: dictionariesReducer,
         ...injectedReducers,
     });
 }
