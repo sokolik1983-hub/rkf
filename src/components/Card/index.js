0a39bb0b063d70d5c13ea086464dde0bf8b9d159
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2019-07-16 15:55:19 +0300

    refactor Card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,15 +2,17 @@ import React from 'react'
 import classnames from 'classnames'
 import './styles.scss'
 
-const Card = ({children, className, lg}) =>
-    <div className={
-        classnames(
-            'card',
-            {[className]: className},
-            {'card--lg': lg},
-        )
-    }>
-        {children}
-    </div>;
-
-export default Card;
+export default function Card({children, className, lg, ...rest}) {
+    return (
+        <div
+            className={classnames(
+                'Card',
+                {[className]: className},
+                {'Card--lg': lg},
+            )}
+            {...rest}
+        >
+            {children}
+        </div>
+    )
+}
