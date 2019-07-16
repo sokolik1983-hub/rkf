bd66ebbfea0f38a20dcecb67682258bf9a974819
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2019-07-16 15:55:41 +0300

    add generateSize util

--- src/utils/index.js
+++ src/utils/index.js
@@ -51,6 +51,13 @@ export const sortBy = (array, key) => {
 };
 
 
+export const generateSize = size => ({
+    minWidth: size,
+    minHeight: size,
+    maxWidth: size,
+    maxHeight: size,
+});
+
 export const scrollSmoothTop = () => window.scrollTo({
     top: 0,
     behavior: "smooth"
@@ -79,16 +86,16 @@ export const getPropsBySchema = (obj, schema) => {
 
 
 export function debounce(func, wait, immediate) {
-	var timeout;
-	return function() {
-		var context = this, args = arguments;
-		var later = function() {
-			timeout = null;
-			if (!immediate) func.apply(context, args);
-		};
-		var callNow = immediate && !timeout;
-		clearTimeout(timeout);
-		timeout = setTimeout(later, wait);
-		if (callNow) func.apply(context, args);
-	};
+    var timeout;
+    return function () {
+        var context = this, args = arguments;
+        var later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
 };
