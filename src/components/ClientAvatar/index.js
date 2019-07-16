e56a6bd5f52e420ae03dc4b3bc33b73df38c4f72
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2019-07-16 15:55:05 +0300

    add ClientAvatar

---
+++ src/components/ClientAvatar/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import classnames from 'classnames'
+import ActivityIndicator from 'components/ActivityIndicator'
+import {generateSize} from 'utils/index'
+import './styles.scss'
+
+export default function ClientAvatar({
+                                         size = 110,
+                                         avatar = '/static/images/noimg/no-avatar.png',
+                                         activity,
+                                         className,
+                                         ...rest
+                                     }) {
+    const styles = {
+        backgroundImage: `url(${avatar})`,
+        ...generateSize(size),
+    };
+    return (
+        <div
+
+            className={classnames(
+                "ClientAvatar",
+                {[className]: className}
+            )}
+            style={styles}
+            {...rest}
+        >
+            <ActivityIndicator size={size/5} active={activity}/>
+        </div>
+    )
+}
