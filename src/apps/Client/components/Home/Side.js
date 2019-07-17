edbb14001f59f355368fc8bcb23d94f7eee53ab0
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2019-07-17 12:44:32 +0300

    move Side to file

---
+++ src/apps/Client/components/Home/Side.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Side({children}) {
+    return (<div className="ClientHome__sidebar">{children}</div>)
+};
--- src/apps/Client/components/Home/index.js
+++ src/apps/Client/components/Home/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Header from './Header'
 import ClientNews from 'apps/ClientNews'
+import Side from './Side'
 import Description from './Description'
 import './styles.scss'
 
 const Wrap = ({children}) => <div className="ClientHome__wrap">{children}</div>;
 const Content = ({children}) => <div className="ClientHome__content">{children}</div>;
-const Side = ({children}) => <div className="ClientHome__sidebar">{children}</div>;
 
 
 export default function ClientHome() {
@@ -18,8 +18,9 @@ export default function ClientHome() {
                     <Description>
                         <h4>Описание</h4>
                         <p>Общественная Организация DoggyDog, является членом Международного Союза «International Kennel
-                        Union» (IKU), объединяющего страны СНГ и Балтии. СКОР является учредителем и членом Российской
-                        Ассоциации зоотехнии, ветеринарии и зооиндустрии. Зарегистрирован Министерством Юстиции РФ и
+                            Union» (IKU), объединяющего страны СНГ и Балтии. СКОР является учредителем и членом
+                            Российской
+                            Ассоциации зоотехнии, ветеринарии и зооиндустрии. Зарегистрирован Министерством Юстиции РФ и
                             объединяет более 1500 кинологических организаций Москвы и большинства регионов России.</p>
                     </Description>
                     <ClientNews/>
