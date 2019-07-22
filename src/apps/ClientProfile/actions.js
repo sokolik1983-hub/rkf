6cd2db81b466a0c35b7381fd54ecf74f14707e3b
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2019-07-22 12:25:44 +0300

    set actions

--- src/apps/ClientProfile/actions.js
+++ src/apps/ClientProfile/actions.js
@@ -1,4 +1,6 @@
 import * as actionTypes from './actiontypes';
 import {makeActionCreator} from 'utils/index'
 
-export const loadingApi = makeActionCreator(actionTypes.LOADING, 'bool');
+export const getProfile = makeActionCreator(actionTypes.GET_PROFILE, 'data');
+export const getProfileSuccess = makeActionCreator(actionTypes.GET_PROFILE_SUCCESS, 'data');
+export const getProfileFailed = makeActionCreator(actionTypes.GET_PROFILE_FAILED, 'errors');
--- src/apps/ClientProfile/actiontypes.js
+++ src/apps/ClientProfile/actiontypes.js
@@ -1,2 +1,4 @@
-export const LOADING = 'client/LOADING';
+export const GET_PROFILE = 'client/Profile/Get';
+export const GET_PROFILE_SUCCESS = 'client/Profile/GetSuccess';
+export const GET_PROFILE_FAILED = 'client/Profile/GetFailed';
 
