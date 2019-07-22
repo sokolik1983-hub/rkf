96f8530e14120678762936ad3593d6d4a09ab040
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2019-07-22 12:25:07 +0300

    organize forms

---
+++ src/apps/ClientProfile/components/Form/index.js
@@ -0,0 +1,9 @@
+import CompanyInfoForm from './CompanyInfoForm'
+import CompanyContactsForm from './CompanyContactsForm'
+import CompanySocialForm from './CompanySocialForm'
+
+export {
+    CompanyInfoForm,
+    CompanyContactsForm,
+    CompanySocialForm
+}
