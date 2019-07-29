4f16ca8c32384fa6b52d9bd95a7e518f3d3e7e7c
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2019-07-29 12:16:31 +0300

    refactor formikHandleSubmit to use axios

--- src/components/Form/formikHandleSubmit.js
+++ src/components/Form/formikHandleSubmit.js
@@ -1,57 +1,62 @@
 import {isDevEnv} from "utils/index";
 import {SERVER} from "appConfig";
 import {getHeaders} from "utils/request";
+import axios from 'axios'
 
 const isDev = isDevEnv();
 
-const defaultOptions = isDev ? {
-        headers: getHeaders(),
+const defaultOptions = (isMultipart) => isDev ? {
+        headers: getHeaders(isMultipart),
         mode: "cors"
     }
     :
     {
-        headers: getHeaders(),
+        headers: getHeaders(isMultipart),
     };
+
+const getFormData = data => {
+    const formData = new FormData();
+    Object.keys(data).forEach(key => formData.append(key, data[key],));
+    return formData;
+};
 
 
 export async function formikHandleSubmit({
+                                             isMultipart,
                                              isUpdate,
                                              url, // POST Url
-                                             options = defaultOptions,
+                                             options,
                                              data, // Form data
                                              successAction, // called with response JSON
                                              formik,
                                              storageVariableName
                                          }) {
-    let text; // define text to be avalible in catch
+    let text; // define text to be available in catch
     // Block Form
     //formik.setSubmitting(true);
+    const requestOptions = {...defaultOptions(isMultipart), ...options};
+    requestOptions.method = isUpdate ? "PUT" : "POST";
+    const body = isMultipart ? getFormData(data) : JSON.stringify(data);
+
+
+    const requestUrl = isDev ? `${SERVER}${url}` : url;
     try {
-        options.method = isUpdate ? "PUT" : "POST";
-        const requestUrl = isDev ? `${SERVER}${url}` : url;
-        const response = await fetch(requestUrl, {
-            ...options,
-            body: data && JSON.stringify(data) // no data if method isn't POST
-        });
-        const {status} = response;
-
-        text = await response.text();
-
-        const json = JSON.parse(text);
+        const response = await axios.post(requestUrl, body, requestOptions);
+        const {status, data} = response;
         if (status >= 200 && status < 300) {
-            successAction(json.result);
+            successAction(data.result);
             // clear stored formData
             localStorage.removeItem(storageVariableName);
             formik.setSubmitting(false);
         } else {
             const touched = {};
-            Object.keys(json).forEach(key => touched[key] = true);
+            Object.keys(data.errors).forEach(key => touched[key] = true);
             //TODO Handle this in future
             //formik.setTouched(touched);
-            formik.setErrors(json.errors);
+            formik.setErrors(data.errors);
             formik.setSubmitting(false);
         }
-        return {json, status, text,}
+        return {response}
 
     } catch (error) {
         formik.setSubmitting(false);
