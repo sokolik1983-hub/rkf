13dab8e8cb1073eec07382356beec7e8c9cba794
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2019-07-31 16:15:24 +0300

    form index file

--- src/apps/Dictionaries/index.js
+++ src/apps/Dictionaries/index.js
@@ -1,8 +1,17 @@
 import {useDictionary} from "./hooks";
 
-const getDictElement = (dict, elementId) => dict.loaded ? dict.dictionary[String(elementId)] : null;
+const getDictElement = (dict, elementId) => dict.loaded ?
+    dict.dictionary[String(elementId)]
+    :
+    null;
+
+const getDictElementsArray = (dict, elementsIdsArray) => dict.loaded ?
+    (elementsIdsArray.map(id=>dict.dictionary[String(id)]))
+    :
+    [];
 
 export {
     getDictElement,
+    getDictElementsArray,
     useDictionary,
 }
