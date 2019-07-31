0deafc134405a47880e93a5bdf95b2ebcf8ec7d1
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2019-07-31 17:58:38 +0300

    fixes

--- src/apps/Exhibitions/components/DetailsAside/index.js
+++ src/apps/Exhibitions/components/DetailsAside/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
 import './index.scss'
-const Aside=({children})=><div className="exhibition-details__aside">{children}</div>;
+const Aside=({children})=><div className="ExhibitionDetails__aside">{children}</div>;
 
 export default Aside
