8bf455f784857820f60c2006489ae2319f4aa87f
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2019-07-31 17:56:15 +0300

    rename class

--- src/apps/Exhibitions/components/DetailsContent/index.js
+++ src/apps/Exhibitions/components/DetailsContent/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './index.scss'
 
-const Content=({children})=><div className="exhibition-details__content">{children}</div>;
+const Content=({children})=><div className="ExhibitionDetails__content">{children}</div>;
 
 export default Content
