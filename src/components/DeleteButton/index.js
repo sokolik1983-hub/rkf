43b97281b8816df10e5c7925298089aa501c5928
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2019-08-06 10:57:56 +0300

    add DeleteButton component

---
+++ src/components/DeleteButton/index.js
@@ -0,0 +1,42 @@
+import React, {useState} from 'react'
+import Button from 'components/Button'
+import {useConfirmDialog} from 'shared/hooks'
+import axios from 'axios'
+import {isDevEnv} from "../../utils";
+import {SERVER} from "../../appConfig";
+
+export default function DeleteButton(props) {
+    const {actionUrl, params, onDeleteSuccess, children} = props;
+    const {confirm, onConfirm, onCancel} = useConfirmDialog();
+    const [state, setState] = useState({loading: false});
+
+    const url = isDevEnv() ? SERVER + actionUrl : actionUrl;
+
+    const onDelete = async () => {
+        setState({...state, loading: true});
+
+        const response = await axios.delete(url, {data: params});
+
+        console.log(response);
+
+        setState({...state, loading: false});
+
+        onDeleteSuccess();
+    };
+
+    const onConfirmDelete = () => {
+        onDelete()
+    };
+
+    return confirm ? (
+            <Button disabled={state.loading} onClick={onConfirm}>
+                {children}
+            </Button>
+        )
+        : (
+            <React.Fragment>
+                <Button disabled={state.loading} onClick={onConfirmDelete}>Подвердить</Button>
+                <Button disabled={state.loading} onClick={onCancel}>Отмена</Button>
+            </React.Fragment>
+        )
+}
