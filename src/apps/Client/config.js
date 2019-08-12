aa2e4b1144b671017c676b06de7fb4cd9344c9af
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2019-08-12 14:36:38 +0300

    remove request, rename context, add profile_id to context

--- src/apps/Client/config.js
+++ src/apps/Client/config.js
@@ -3,4 +3,4 @@ import {createContext} from "react";
 
 export const clubInfoUrl = '/api/Club/full';
 export const defaultReduxKey = 'client_common';
-export const ClientPathContext = createContext();
+export const ClientCommonContext = createContext();
--- src/apps/Client/selectors.js
+++ src/apps/Client/selectors.js
@@ -1,5 +1,20 @@
 import {defaultReduxKey} from "./config";
 
+import {selectProfileId} from 'apps/Auth/selectors'
+
+export const getClientState = state => {
+    return state[defaultReduxKey];
+};
+
+export const selectClientData = state => {
+    const {profile_id} = selectProfileId(state);
+    const client_common = getClientState(state);
+    return {
+        ...client_common,
+        profile_id,
+    }
+};
+
 export const selectClubInfo = state => ({
     ...state[defaultReduxKey].club_info
 });
