8102b1c62b59021cddfdfc15b88695eb07f7dc6e
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2019-08-12 14:40:08 +0300

    add documents to profile

--- src/apps/ClientProfile/components/Profile/index.js
+++ src/apps/ClientProfile/components/Profile/index.js
@@ -4,25 +4,22 @@ import {connectProfile} from 'apps/ClientProfile/connectors'
 import Card from 'components/Card'
 import ClubInfo from 'apps/ClubInfo'
 import ClientProfileAvatar from '../Avatar'
+import ClientClubContacts from 'apps/ClubContacts'
+import ClientClubDocuments from 'apps/ClubDocuments'
 import LegalInfo from 'apps/ClubLegalInfo'
 import BankInfo from 'apps/ClubBankInfo'
 
 function ClientProfile(props) {
     const {
-        getProfile,
         legal_information_id,
         bank_data_id,
     } = props;
-    useEffect(() => {
-        getProfile()
-    }, []);
-
     return (
         <>
             <h2>Профиль</h2>
             <Card lg>
-                <h3>ClubInfo</h3>
-                <ClubInfo clubId={12}/>
+                <h3>Данные клуба</h3>
+                <ClubInfo/>
             </Card>
             <Card lg>
                 <h3>LegalInfo</h3>
@@ -34,7 +31,11 @@ function ClientProfile(props) {
             </Card>
             <Card lg>
                 <h3>Контакты</h3>
-                <CompanyContactsForm/>
+                <ClientClubContacts/>
+            </Card>
+            <Card lg>
+                <h3>Ссылки на документы</h3>
+                <ClientClubDocuments/>
             </Card>
             <Card lg>
                 <h3>Социальные сети</h3>
