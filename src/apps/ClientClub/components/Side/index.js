82b1818859910d6b74b8c190e9554639db878c00
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2019-08-14 15:44:08 +0300

    add alias form to sidebar

--- src/apps/ClientClub/components/Side/index.js
+++ src/apps/ClientClub/components/Side/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ClientClubContacts from '../Contacts'
 import Card from 'components/Card'
 import ClientClubDocuments from 'apps/ClubDocuments'
+import ClientClubAlias from '../Alias'
 
 export default function Side({children}) {
     return (
@@ -9,6 +10,7 @@ export default function Side({children}) {
             <Card>
                 <ClientClubContacts/>
                 <ClientClubDocuments/>
+                <ClientClubAlias/>
                 {children}
             </Card>
         </div>)
