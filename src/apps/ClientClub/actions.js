79427ae8f6a55d3d7a98a47475ee541f6a2916c8
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2019-08-14 15:43:52 +0300

    club header pictures

--- src/apps/ClientClub/actions.js
+++ src/apps/ClientClub/actions.js
@@ -2,3 +2,5 @@ import * as actionTypes from './actiontypes';
 import {makeActionCreator} from 'utils/index'
 
 export const getClubSuccess = makeActionCreator(actionTypes.GET_CLUB_SUCCESS, 'data');
+export const getClubPictureUpdateSuccess = makeActionCreator(actionTypes.CLUB_HEADER_UPDATE_SUCCESS, 'data');
+export const getClubLogoUpdateSuccess = makeActionCreator(actionTypes.CLUB_LOGO_UPDATE_SUCCESS, 'data');
--- src/apps/ClientClub/actiontypes.js
+++ src/apps/ClientClub/actiontypes.js
@@ -1,3 +1,5 @@
 export const GET_CLUB_SUCCESS = 'client/Club/Get/Success';
+export const CLUB_HEADER_UPDATE_SUCCESS = 'client/Club/HeaderPicture/Update/Success';
+export const CLUB_LOGO_UPDATE_SUCCESS = 'client/Club/LogoPicture/Update/Success';
 
 export const ADD_CONTACT_SUCCESS = 'client/Club/Contacts/Add/Success';
