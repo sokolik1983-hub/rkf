1fe856f7cca511ba26d98f8902a2851106cc1151
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2019-08-14 13:59:28 +0300

    add icons to contacts

--- src/apps/ClientClub/components/Contacts/Contact.js
+++ src/apps/ClientClub/components/Contacts/Contact.js
@@ -4,7 +4,7 @@ import classnames from "classnames";
 
 export default function Contact({description, value, contact_type_id}) {
     return (
-        <div className={classnames('ClubContact', `--type-${contact_type_id}`)}>
+        <div className={classnames('ClubContact', `ClubContact--type-${contact_type_id}`)}>
             <span className="ClubContact__description">{description}</span>
             <span className="ClubContact__value">{value}</span>
         </div>
