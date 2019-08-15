1b92d4bb5b919418ea8514b767bb8041c729c6ae
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2019-08-15 15:47:16 +0300

    refactor ClientClub app

--- src/apps/ClientClub/actions.js
+++ src/apps/ClientClub/actions.js
@@ -4,3 +4,5 @@ import {makeActionCreator} from 'utils/index'
 export const getClubSuccess = makeActionCreator(actionTypes.GET_CLUB_SUCCESS, 'data');
 export const getClubPictureUpdateSuccess = makeActionCreator(actionTypes.CLUB_HEADER_UPDATE_SUCCESS, 'data');
 export const getClubLogoUpdateSuccess = makeActionCreator(actionTypes.CLUB_LOGO_UPDATE_SUCCESS, 'data');
+export const clubAliasUpdateSuccess = makeActionCreator(actionTypes.CLUB_ALIAS_UPDATE_SUCCESS, 'data');
+export const clubInfoUpdateSuccess = makeActionCreator(actionTypes.CLUB_INFO_UPDATE_SUCCESS, 'data');
--- src/apps/ClientClub/index.js
+++ src/apps/ClientClub/index.js
@@ -1,17 +1,26 @@
 import React from "react"
 import {compose} from 'redux'
+import {Route, Switch} from 'react-router-dom'
 import ClientClubLayout from './components/Layout'
 import {useResourceAndStoreToRedux} from 'shared/hooks'
 import {defaultReduxKey, endpointUrl} from "./config";
 import {connectClientClub} from './connectors'
-import injectReducer from "../../utils/injectReducer";
+import injectReducer from "utils/injectReducer";
 import reducer from "./reducer";
+import ClubEditPage from './components/CommonEditPage'
 
-function ClientClubProxy({id, route, getClubSuccess}) {
+function ClientClubProxy(props) {
+
+    const {id, route, getClubSuccess, match} = props;
+    const {path} = match;
+    // get club information
     const {loading} = useResourceAndStoreToRedux(endpointUrl, getClubSuccess);
-
     return (
-        <ClientClubLayout/>
+        <Switch>
+            {loading ? 'загрузка' : null}
+            <Route exact path={path} component={ClientClubLayout}/>
+            <Route path={`${path}/club`} component={ClubEditPage}/>
+        </Switch>
     )
 }
 
