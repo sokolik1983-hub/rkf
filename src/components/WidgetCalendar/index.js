b45508840d3687a8d18ff90a64a136d7293c23e9
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2019-08-16 15:19:09 +0300

    connect calendar to exhibition dates

--- src/components/WidgetCalendar/index.js
+++ src/components/WidgetCalendar/index.js
@@ -1,42 +1,43 @@
-import React, {PureComponent} from 'react'
+import React, {useMemo} from 'react'
 import DayPicker from 'react-day-picker';
 import './index.scss'
-const modifiers={
+
+const modifiers = {
     green: [new Date(2019, 3, 5), new Date(2019, 3, 3)],
     blue: [new Date(2019, 3, 5), new Date(2019, 3, 3)],
 };
 
 const WEEKDAYS_SHORT = ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'];
 const MONTHS = [
-  'Январь',
-  'Февраль',
-  'Март',
-  'Апрель',
-  'Май',
-  'Июнь',
-  'Июль',
-  'Август',
-  'Сентябрь',
-  'Октябрь',
-  'Ноябрь',
-  'Декабрь',
+    'Январь',
+    'Февраль',
+    'Март',
+    'Апрель',
+    'Май',
+    'Июнь',
+    'Июль',
+    'Август',
+    'Сентябрь',
+    'Октябрь',
+    'Ноябрь',
+    'Декабрь',
 ];
 
 
-export default class CalendarWidget extends PureComponent {
-    onDayPickerClick = e => {
+export default function CalendarWidget({modifiers = {}}) {
+    const onDayPickerClick = e => {
         console.log(e)
     };
 
-    render() {
-        return <DayPicker
-            showOutsideDays={true}
-            months={MONTHS}
-            weekdaysShort={WEEKDAYS_SHORT}
-            modifiers={modifiers}
-            locale="ru"
-            onDayClick={this.onDayPickerClick}
-            firstDayOfWeek={1}
-        />
-    }
+
+    return useMemo(() => <DayPicker
+        showOutsideDays={true}
+        months={MONTHS}
+        weekdaysShort={WEEKDAYS_SHORT}
+        modifiers={modifiers}
+        locale="ru"
+        onDayClick={onDayPickerClick}
+        firstDayOfWeek={1}
+    />, [modifiers])
+
 }
