aa335a0b6309af5df3055363e2264dca609c51b6
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2019-08-21 15:43:27 +0300

    del

--- src/apps/ClubDocuments__/actions.js
+++
@@ -1,5 +0,0 @@
-import * as actionTypes from './actiontypes';
-import {makeActionCreator} from 'utils/index'
-
-export const getClubDocumentsListSuccess = makeActionCreator(actionTypes.GET_LIST_SUCCESS, 'data');
-export const addClubDocumentSuccess = makeActionCreator(actionTypes.ADD_CONTACT_SUCCESS, 'data');
--- src/apps/ClubDocuments__/actiontypes.js
+++
@@ -1,3 +0,0 @@
-export const GET_LIST_SUCCESS = 'client/Club/Documents/Get/Success';
-
-export const ADD_CONTACT_SUCCESS = 'client/Club/Documents/Add/Success';
