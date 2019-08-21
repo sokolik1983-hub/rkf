a153b108329ebcb854054cecf924eb364769322b
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2019-08-21 13:23:21 +0300

    rename className

--- src/apps/Exhibitions/components/ListItem/index.js
+++ src/apps/Exhibitions/components/ListItem/index.js
@@ -7,22 +7,21 @@ import './index.scss'
 import {ExhibitionsPathContext} from 'apps/Exhibitions/context'
 
 function ExhibitionsListItem(props) {
-    console.log(props)
     const {id, city, name, time_start, time_end} = props;
     const {path} = useContext(ExhibitionsPathContext);
     return (
-        <div id={'exhibitionsListItem_' + id} className="exhibition-list-item">
-            <div className="exhibition-list-item__header">
-                <div className="exhibition-list-item__city">
+        <div id={'ExhibitionsListItem_' + id} className="ExhibitionsListItem">
+            <div className="ExhibitionsListItem__header">
+                <div className="ExhibitionsListItem__city">
                     {city}
                 </div>
                 <div
-                    className="exhibition-list-item__datetime">
+                    className="ExhibitionsListItem__datetime">
                     {timeSecondsCutter(time_start)} - {timeSecondsCutter(time_end)}
                 </div>
             </div>
-            <div className="exhibition-list-item__title">{name}</div>
-            <div className="exhibition-list-item__controls">
+            <div className="ExhibitionsListItem__title">{name}</div>
+            <div className="ExhibitionsListItem__controls">
                 <Link to={`${path}/${id}/details`} className="btn btn-primary">Смотреть</Link>
             </div>
         </div>
