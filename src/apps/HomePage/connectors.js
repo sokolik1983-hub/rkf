288df8c0de6446e359c7ec20515751cc76174b00
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2019-08-25 10:30:43 +0300

    commit changes in app controllers

--- src/apps/HomePage/connectors.js
+++ src/apps/HomePage/connectors.js
@@ -4,20 +4,88 @@ import {
     selectExhibition,
     selectNews,
     selectNewsStory,
+    selectClubHeader,
+    selectFeaturedExhibitionsList,
+    selectClubDescription,
+    selectClubAddress,
+    selectClubContacts,
+    selectClubDocuments,
+
 } from './selectors'
 
-export const connectExhibitionsAnnouncementList=connect(
-    selectExhibitions
+import {
+    getCommonSuccess,
+    getNewsSuccess,
+    getExhibitionsSuccess,
+    storeExhibitions
+} from './actions'
+import {bindActionCreators} from "redux";
+
+
+export const connectNewsList = connect(
+    selectNews,
+    dispatch => bindActionCreators(
+        {
+            getNewsSuccess
+        }, dispatch)
 );
 
-export const connectExhibitionsAnnouncement=connect(
-    selectExhibition
+export const connectExh = connect(
+    selectNews,
+    dispatch => bindActionCreators(
+        {
+            getNewsSuccess
+        }, dispatch)
 );
 
-export const connectNewsList=connect(
-    selectNews
-);
-
-export const connectNewsStory=connect(
+export const connectNewsStory = connect(
     selectNewsStory
+);
+
+export const connectClubCommon = connect(
+    null,
+    dispatch => bindActionCreators(
+        {
+            getCommonSuccess
+        }, dispatch)
+);
+
+
+export const connectClubCommonExhibitions = connect(
+    selectFeaturedExhibitionsList,
+    dispatch => bindActionCreators(
+        {
+            storeExhibitions
+        }, dispatch)
+);
+
+export const connectClubHeader = connect(
+    selectClubHeader
+);
+
+
+export const connectFeaturedExhibitionsList = connect(
+    selectFeaturedExhibitionsList,
+    dispatch => bindActionCreators(
+        {
+            storeExhibitions
+        }, dispatch)
+);
+
+export const connectClubDescription = connect(
+    selectClubDescription,
+);
+
+
+export const connectClubAddress = connect(
+    selectClubAddress,
+);
+
+export const connectClubContacts = connect(
+    selectClubContacts,
+);
+
+
+export const connectClubDocuments = connect(
+    selectClubDocuments,
 );
