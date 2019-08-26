26ae997b2667059ab8d816da592686a5206121c2
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2019-08-26 15:51:23 +0300

    fix merge

--- src/components/Layout/Header/index.js
+++ src/components/Layout/Header/index.js
@@ -9,23 +9,17 @@ import WidgetNotifications from 'components/WidgetNotifications'
 import HeaderLogin from 'apps/Auth/containers/HeaderLogin'
 import './index.scss'
 
-const Header = ({ children }) =>
+const Header = ({children}) =>
     <header>
         <Container pad>
-<<<<<<< HEAD
-            <HeaderLogo />
-            <Nav />
-            <CitySelect />
-            <WidgetLang />
-            <WidgetNotifications />
-            <HeaderLogin />
-=======
             <HeaderLogo/>
+            <CitySelect/>
             <Nav/>
+
             {/*<WidgetLang/>*/}
             {/*<WidgetNotifications/>*/}
             <HeaderLogin/>
->>>>>>> 12386d998087b915a419998e598911c0fbf7e74f
+
         </Container>
     </header>
 
