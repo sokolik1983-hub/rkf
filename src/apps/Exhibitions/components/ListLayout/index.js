7312a250abdd4001ad08a7230d0cb23c287d9dbc
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2019-08-26 11:23:28 +0300

    hide components

--- src/apps/Exhibitions/components/ListLayout/index.js
+++ src/apps/Exhibitions/components/ListLayout/index.js
@@ -1,24 +1,26 @@
-import React, {Fragment} from "react";
-import FilterDate from 'apps/Exhibitions/components/FilterDate'
+import React from "react";
+//import FilterDate from 'apps/Exhibitions/components/FilterDate'
 //import Paginator from 'components/Paginator'
 import Aside from 'components/Layout/Aside'
 import Content from 'components/Layout/Content'
 import ExhibitionsAside from 'apps/Exhibitions/Aside'
+import './styles.scss'
+
 
 function ExhibitionsListLayout({children}) {
 
     return (
-        <Fragment>
+        <div className="ExhibitionsListLayout">
             <Aside>
                 <ExhibitionsAside/>
             </Aside>
             <Content>
                 <div className="exhibitions__holder">
-                    <FilterDate/>
+                    {/*<FilterDate/>*/}
                     {children}
                 </div>
             </Content>
-        </Fragment>
+        </div>
     )
 }
 
