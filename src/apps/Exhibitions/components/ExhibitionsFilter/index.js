82eb811742876bc8251df492616940cdd3c24208
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2019-09-02 10:39:01 +0300

    quick fix. Forgot to push this changes

--- src/apps/Exhibitions/components/ExhibitionsFilter/index.js
+++ src/apps/Exhibitions/components/ExhibitionsFilter/index.js
@@ -1,39 +1,70 @@
-import React, { useState } from 'react'
-import { connectExhibitionsFilter } from 'apps/Exhibitions/connectors'
-import { ExhibitionsFilterContext } from 'apps/Exhibitions/context'
-import { useDictionary } from "apps/Dictionaries";
+import React, {useState} from 'react'
+import {connectExhibitionsFilter} from 'apps/Exhibitions/connectors'
+import {ExhibitionsFilterContext} from 'apps/Exhibitions/context'
+import {useDictionary} from "apps/Dictionaries";
+import {useResourceAndStoreToRedux} from "../../../../shared/hooks";
+import {endpointExhibitionsList} from "../../config";
 
-const { Provider } = ExhibitionsFilterContext;
+const {Provider} = ExhibitionsFilterContext;
 const filterInitialState = {
     cities: [],
-    breeds: [],
-    dates: {}
+    //breeds: [],
+    //dates: {}
 };
 
 
-function ExhibitionsFilter({ city_ids, children }) {
-    // city_ids - города которые присутствуют в выставке
+function ExhibitionsFilter({city_ids, children, className, fetchExhibitionsSuccess}) {
+    const [url, setUrl] = useState(endpointExhibitionsList);
+    const {loading} = useResourceAndStoreToRedux(url, fetchExhibitionsSuccess);
+
+    // city_ids - ids of cities of exhibitions in selected list
     const [filter, setFilter] = useState(filterInitialState);
     //const {dictionary: breed_types} = useDictionary('breed_types');
-    const { dictionary: cities } = useDictionary('cities');
+    const {dictionary: cities} = useDictionary('cities');
     // cities - словарь всех городов
-    const changeCitiesFilter = cities => setFilter({ ...filter, cities });
+    const changeCitiesFilter = cities => setFilter({...filter, cities});
     const clearFilter = () => {
-        setFilter({ ...filterInitialState });
+        setFilter({...filterInitialState});
+        setUrl(endpointExhibitionsList);
     };
+    const mapParams = (array, apiParamName) => {
+        let str = '';
+        array.forEach(el => {
+            str = str + apiParamName + '=' + el + '&'
+        });
+        return str
+    };
+    const buildUri = filter => {
+        let str = '';
+
+        Object.keys(filter).forEach(key => {
+            str = str + mapParams(filter[key], 'CityIds')
+        });
+
+        return str
+    };
+    const applyFilter = () => {
+        const url = `${endpointExhibitionsList}?${buildUri(filter)}`;
+        setUrl(url)
+    };
+
     return (
         <Provider value={{
+            loading,
             city_ids,
             filter,
             setFilter,
             //breed_types,
             cities,
             changeCitiesFilter,
-            clearFilter
+            clearFilter,
+            applyFilter,
         }}>
-            {children}
+            <div className={className}>
+                {children}
+            </div>
         </Provider>
     )
 }
 
-export default connectExhibitionsFilter(React.memo(ExhibitionsFilter))
+export default React.memo(connectExhibitionsFilter(ExhibitionsFilter))
