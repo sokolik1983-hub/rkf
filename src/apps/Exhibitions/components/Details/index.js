1a754de2d1c308bc6ac869652551661e265713d0
Den <den.bessarab@outlook.com>
2019-09-03 10:36:18 +0000

    Merged in bugfix/exhibition_details (pull request #14)
    Bugfix/exhibition details
    
    * fix exhibitions details page
    
    * fix featured exhibitions image resize
    
    * add featured exhibitions
    
    * merge from dev to bugfix/exhibition_details

--- src/apps/Exhibitions/components/Details/index.js
+++ src/apps/Exhibitions/components/Details/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Container from "components/Layout/Container";
 import Head from "./Head";
 import Content from './Content'
@@ -12,10 +12,22 @@ import { SponsorsData, PartnersData } from 'apps/HomePage/components/Partners/da
 import ExhibitionDetailsPrices from '../DetailsPrices'
 import './styles.scss'
 // import FooterSmall from 'components/Layout/FooterSmall'
+import 'apps/HomePage/components/FeaturedExhibitions'
+import FeaturedExhibition from 'apps/HomePage/components/FeaturedExhibitions/Exhibition'
 
 function ExhibitionDetails(props) {
     const { getDetailsSuccess, exhibitionId, details } = props;
     const { loading } = useResourceAndStoreToRedux('/api/exhibitions/exhibition/' + String(exhibitionId), getDetailsSuccess);
+
+    // TODO: replace with dynamic data
+    const [featuredExh, setFeaturedExh] = useState(false);
+    if (!featuredExh) {
+        fetch('/api/exhibitions/Exhibition/featured?Alias=real_tyu')
+        .then(response => response.json())
+        .catch(error => console.error('Error:', error))
+        .then(response => setFeaturedExh(response.result));
+    }
+
     return (
         <>
             <Container className="ExhibitionDetails">
@@ -23,6 +35,20 @@ function ExhibitionDetails(props) {
                 <div className="ExhibitionDetails__wrap">
                     <Content {...details} />
                     <ExhibitionAsideContent {...details} />
+                </div>
+                {exhibitionId && <ExhibitionDetailsPrices exhibition_id={exhibitionId} />}
+                <PaymentDetails />
+                <Partners title="Наши спонсоры" items={SponsorsData} />
+                <Partners title="Наши партнеры" items={PartnersData} />
+                <Address />
+
+                <h3 className="FeaturedExhibitions__title">Другие выставки</h3>
+                <div className="FeaturedExhibitionsList">
+                    {
+                        featuredExh
+                            ? featuredExh.map(e => <FeaturedExhibition {...e} />)
+                            : null
+                    }
                 </div>
                 {exhibitionId && <ExhibitionDetailsPrices exhibition_id={exhibitionId} />}
                 <PaymentDetails />
