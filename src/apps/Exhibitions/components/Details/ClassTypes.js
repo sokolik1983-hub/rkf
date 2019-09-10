97de2c019d4a5ae2c206e2d54068ddf57a5fa53d
Den <den.bessarab@outlook.com>
2019-09-10 07:51:13 +0000

    Merged in bugfix/exhibition_class_types (pull request #35)
    fix ExhibitionClassTypes to string
    
    * fix ExhibitionClassTypes to string

--- src/apps/Exhibitions/components/Details/ClassTypes.js
+++ src/apps/Exhibitions/components/Details/ClassTypes.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import {getDictElementsArray, useDictionary} from "apps/Dictionaries";
+import { getDictElementsArray, useDictionary } from "apps/Dictionaries";
 
-export default function ExhibitionClassTypes({classTypesIds = []}) {
-    const {dictionary} = useDictionary('class_types');
+export default function ExhibitionClassTypes({ classTypesIds = [] }) {
+    const { dictionary } = useDictionary('class_types');
     const classTypes = getDictElementsArray(dictionary, classTypesIds);
     return (
         <React.Fragment>
-            {classTypes.map(classType => <div key={classType} className="ExhibitionClassTypes">{classType}</div>)}
+            {classTypes.map((classType, i) => <span key={classType} className="ExhibitionClassTypes">{i === 0 ? classType : `, ${classType}`}</span>)}
         </React.Fragment>
     )
 }
