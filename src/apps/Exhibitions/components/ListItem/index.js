1ecfd4975dcdb25418310d454aabeaed09af5718
Den <den.bessarab@outlook.com>
2019-09-12 14:58:05 +0000

    Merged in bugfix/exh_list_item (pull request #42)
    fix exh list item link
    
    * fix exh list item link

--- src/apps/Exhibitions/components/ListItem/index.js
+++ src/apps/Exhibitions/components/ListItem/index.js
@@ -1,5 +1,5 @@
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext, useState } from 'react';
+import { Redirect } from 'react-router-dom';
 import ExhibitionDateHelper from 'components/Exhibitions/ExhibitionDateHelper';
 import { connectExhibitionsListItem } from 'apps/Exhibitions/connectors';
 import EditLink from './EditLink'
@@ -12,23 +12,31 @@ import { ExhibitionsPathContext } from 'apps/Exhibitions/context';
 function ExhibitionsListItem(props) {
     const { id, city, exhibition_name, dates, clubId, club_id } = props;
     const { path } = useContext(ExhibitionsPathContext);
+    const [redirect, setRedirect] = useState(false);
+
+    const handleOnClick = () => {
+        setRedirect(true);
+    }
+
     return (
-        <div id={'ExhibitionsListItem_' + id} className="ExhibitionsListItem">
-            <div className="ExhibitionsListItem__header">
-                <div className="ExhibitionsListItem__city">{city}</div>
-                <div className="ExhibitionsListItem__datetime">
-                    {dates.map((date, index) => (
-                        <ExhibitionDateHelper key={index} {...date} />
-                    ))}
+        redirect
+            ? <Redirect push to={`${path}/${id}/details`} />
+            : <div id={'ExhibitionsListItem_' + id} className="ExhibitionsListItem" onClick={handleOnClick}>
+                <div className="ExhibitionsListItem__header">
+                    <div className="ExhibitionsListItem__city">{city}</div>
+                    <div className="ExhibitionsListItem__datetime">
+                        {dates.map((date, index) => (
+                            <ExhibitionDateHelper key={index} {...date} />
+                        ))}
+                    </div>
+                </div>
+                <div className="ExhibitionsListItem__title">
+                    {exhibition_name}
+                </div>
+                <div className="Controls">
+                    <EditLink exhibitionId={id} canEdit={club_id === clubId} />
                 </div>
             </div>
-            <div className="ExhibitionsListItem__title">
-                <Link to={`${path}/${id}/details`}>{exhibition_name}</Link>
-            </div>
-            <div className="Controls">
-                <EditLink exhibitionId={id} canEdit={club_id === clubId} />
-            </div>
-        </div>
     );
 }
 
