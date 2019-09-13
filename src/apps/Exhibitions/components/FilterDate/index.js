f13419c782d9bcefbbba089df0b826bb974ec15c
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2019-09-13 15:13:50 +0300

    save progress on FilterDate before merge master to dev

--- src/apps/Exhibitions/components/FilterDate/index.js
+++ src/apps/Exhibitions/components/FilterDate/index.js
@@ -1,57 +1,89 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from 'react';
+import classnames from 'classnames';
+import { ExhibitionsFilterContext } from 'apps/Exhibitions/context';
+import { formatDateToString } from 'utils/datetime';
+import FilterButton from './FilterButton';
+import './index.scss';
 
-import './index.scss'
+const FILTERS = {
+    week: 'week',
+    month: 'month',
+    year: 'year',
+    today: 'today'
+};
 
-
-class CustomButton extends React.PureComponent {
-    onClick = () => {
-        this.props.onClick(this.props.type)
+function FilterDateRange() {
+    const [currentDate] = useState(
+        formatDateToString(new Date())
+    );
+    const [filter, setFilter] = useState(null);
+    const [archive, setArchive] = useState(false);
+    const { setDatesRange, clearDatesRange } = useContext(ExhibitionsFilterContext);
+    const onClick = newFilter => {
+        if(newFilter!==filter){
+            setFilter(newFilter);
+        }
+        else{
+            setFilter('');
+        }
     };
-    getClassName = () => this.props.filter === this.props.type ? "exhibition-list__btn  exhibition-list__btn--active" : ' exhibition-list__btn'
 
-    render() {
-        return <button
-            onClick={this.onClick}
-            className={this.getClassName()}
-        >{this.props.children}</button>
-    }
-}
-
-export default class FilterDateRange extends React.PureComponent {
-    state = {
-        filter: 'week',
-    };
-    onClick = (filter) => {
-        this.setState({filter: filter})
+    const onArchiveClick = () => {
+        setArchive(!archive);
     };
 
+    useEffect(() => {
+        if (filter === FILTERS.today) {
+            setDatesRange({ dateFrom: currentDate, dateTo: currentDate });
+        }
+        if(filter===''){
+            clearDatesRange()
+        }
+    }, [filter, archive]);
 
-    render() {
-        return (
-            <div className="exhibition-list__header">
-                <div className="exhibition-list__date-range-filter">
-                    <button className="exhibition-list__btn exhibition-list__btn--archive">Архив</button>
-                    <CustomButton
-                        type={'year'}
-                        filter={this.state.filter}
-                        onClick={this.onClick}
-                    >Год
-                    </CustomButton>
-                    <CustomButton
-                        type={'month'}
-                        filter={this.state.filter}
-                        onClick={this.onClick}>Месяц</CustomButton>
-                    <CustomButton
-                        type={'week'}
-                        filter={this.state.filter}
-                        onClick={this.onClick}>Неделя</CustomButton>
-                    <CustomButton
-                        type={'today'}
-                        filter={this.state.filter}
-                        onClick={this.onClick}>Сегодня</CustomButton>
-                </div>
-                {/*<Button className={'exhibition-list__btn-create btn-primary btn-lg'}>Создать выставку</Button>*/}
+    return (
+        <div className="exhibition-list__header">
+            <div className="exhibition-list__date-range-filter">
+                <button
+                    onClick={onArchiveClick}
+                    className={classnames('exhibition-list__btn', {
+                        'exhibition-list__btn--archive': archive
+                    })}
+                >
+                    Архив
+                </button>
+                <FilterButton
+                    type={FILTERS.year}
+                    filter={filter}
+                    onClick={onClick}
+                >
+                    Год
+                </FilterButton>
+                <FilterButton
+                    type={FILTERS.month}
+                    filter={filter}
+                    onClick={onClick}
+                >
+                    Месяц
+                </FilterButton>
+                <FilterButton
+                    type={FILTERS.week}
+                    filter={filter}
+                    onClick={onClick}
+                >
+                    Неделя
+                </FilterButton>
+                <FilterButton
+                    type={FILTERS.today}
+                    filter={filter}
+                    onClick={onClick}
+                >
+                    Сегодня
+                </FilterButton>
             </div>
-        );
-    }
+            {/*<Button className={'exhibition-list__btn-create btn-primary btn-lg'}>Создать выставку</Button>*/}
+        </div>
+    );
 }
+
+export default FilterDateRange;
