68e613a612f1822e2d657308093eb0628427bfd2
Leon <leon1258@yandex.ru>
2019-09-19 14:06:16 +0000

    Merged in bugfix/60_loose_info_in_news (pull request #68)
    fix loose info in news
    
    Approved-by: Den <den.bessarab@outlook.com>

--- src/apps/ClientNews/components/Create/index.js
+++ src/apps/ClientNews/components/Create/index.js
@@ -3,31 +3,23 @@ import { useVisibility } from 'shared/hooks'
 import NewsStoryCreateForm from 'apps/ClientNews/components/Form'
 import Card from 'components/Card'
 import Button from 'components/Button'
-import OutsideClickHandler from "react-outside-click-handler";
 import { connectAuthVisible } from 'apps/Auth/connectors'
 
 
 function CreateArticleForm({ isAuthenticated }) {
     const { visibility, toggleVisibility, setInvisible } = useVisibility(false);
+
+    if(!isAuthenticated) return null;
+
     return (
-        isAuthenticated
-            ? <Card style={{ margin: '24px 0' }}>
-                <div style={{ textAlign: 'right' }}>
-                    {
-                        !visibility
-                            ? <Button className="btn btn-primary" onClick={toggleVisibility}>Добавить новость</Button>
-                            : null
-                    }
-                </div>
-                {
-                    visibility ?
-                        <OutsideClickHandler onOutsideClick={setInvisible}>
-                            <NewsStoryCreateForm hideForm={setInvisible} />
-                        </OutsideClickHandler>
-                        : null
-                }
-            </Card>
-            : null
+         <Card style={{ margin: '24px 0' }}>
+             {!visibility ?
+                 <div style={{ textAlign: 'right' }}>
+                     <Button className="btn btn-primary" onClick={toggleVisibility}>Добавить новость</Button>
+                 </div> :
+                 <NewsStoryCreateForm hideForm={setInvisible} />
+             }
+        </Card>
     )
 }
 
