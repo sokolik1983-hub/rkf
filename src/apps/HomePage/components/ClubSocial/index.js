3fe5727297092046efc31224b7395e22af84700f
Den <den.bessarab@outlook.com>
2019-11-22 16:39:37 +0300

    quickfix ClubSocial isLoaded check

--- src/apps/HomePage/components/ClubSocial/index.js
+++ src/apps/HomePage/components/ClubSocial/index.js
@@ -5,13 +5,18 @@ import request, { getHeaders } from "utils/request";
 
 function ClubSocial({ clubCommon }) {
     const [socials, setSocials] = useState([]);
-    request({
-        url: `/api/clubs/SocialNetwork/list/${clubCommon.id}`,
-        options: {
-            method: "GET",
-            headers: getHeaders(),
-        }
-    }).then(data => setSocials(data.result));
+    const [loaded, setLoaded] = useState(false);
+    if (!loaded) {
+        request({
+            url: `/api/clubs/SocialNetwork/list/${clubCommon.id}`,
+            options: {
+                method: "GET",
+                headers: getHeaders(),
+            }
+        })
+            .then(data => setSocials(data.result));
+        setLoaded(true);
+    }
     return (
         <div className="ClubSocial">
             {socials.map(document => <SocialLink key={document.id} {...document} />)}
