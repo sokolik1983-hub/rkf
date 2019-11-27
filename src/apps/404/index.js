4dcf20265f80b8332a923b5731c158a2d30a0b0a
Leon <leon1258@yandex.ru>
2019-11-27 09:53:27 +0000

    Merged in bugfix/fix_404_page (pull request #195)
    fix 404 page layout

--- src/apps/404/index.js
+++ src/apps/404/index.js
@@ -2,9 +2,12 @@ import React from "react";
 import PublicLayout from 'components/Layout';
 import Container from 'components/Layout/Container';
 import FooterSmall from 'components/Layout/FooterSmall';
+import {useWrapClassName} from "../../shared/hooks";
 import './styles.scss';
 
 export default function PageNotFound() {
+    useWrapClassName('not-found');
+
     return (
         <PublicLayout>
             <Container className="page-404">
