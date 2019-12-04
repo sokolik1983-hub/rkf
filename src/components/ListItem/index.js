28f53f375f7e8b811605479ae2401006c15a5590
Den <den.bessarab@outlook.com>
2019-12-04 14:43:51 +0300

    quickfix Homepage news no-image height

--- src/components/ListItem/index.js
+++ src/components/ListItem/index.js
@@ -1,10 +1,10 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Modal from "../Modal";
-import {formatDateTime} from "../../utils/datetime";
+import { formatDateTime } from "../../utils/datetime";
 import './index.scss';
 
 
-const ListItem = ({date, photo, clubName, text, modalInner, modalClass}) => {
+const ListItem = ({ date, photo, clubName, text, modalInner, modalClass }) => {
     const [showModal, setShowModal] = useState(false);
 
     text = JSON.parse(JSON.stringify(text).replace(/\\r\\n/g, '<br>'));
@@ -20,9 +20,9 @@ const ListItem = ({date, photo, clubName, text, modalInner, modalClass}) => {
                 </span>
             </div>
             <div className="list-item__body" onClick={() => setShowModal(true)}>
-                {photo && <div className="list-item__photo" style={{backgroundImage: `url(${photo})`}} />}
+                {photo && <div className="list-item__photo" style={{ backgroundImage: `url(${photo})` }} />}
                 <div className="list-item__info">
-                    <p className="list-item__text" dangerouslySetInnerHTML={{__html: text}} />
+                    <p className={photo ? 'list-item__text' : 'list-item__text-short'} dangerouslySetInnerHTML={{ __html: text }} />
                     <button className="list-item__show-all" >Подробнее</button>
                 </div>
             </div>
