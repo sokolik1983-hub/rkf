9fec0fe77f008da3845ce58401cefdd7383822e6
Den <den.bessarab@outlook.com>
2019-12-11 18:25:28 +0300

    quickfix add SocialLink classname

--- src/apps/HomePage/components/ClubSocial/SocialLink.js
+++ src/apps/HomePage/components/ClubSocial/SocialLink.js
@@ -4,7 +4,7 @@ import './SocialLink.scss';
 export default function SocialLink({ description, site }) {
     return (
         <div className="SocialLink">
-            <div className="SocialLink__description"><a href={site} target="_blank" rel="noopener noreferrer">{description}</a></div>
+            <div className="SocialLink__description"><a href={site} className="link" target="_blank" rel="noopener noreferrer">{description}</a></div>
         </div>
     );
 }
