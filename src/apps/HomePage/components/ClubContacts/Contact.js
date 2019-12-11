856ed381e82a7dcd5374dea7c7b7bb2b3b190c93
Den <den.bessarab@outlook.com>
2019-12-11 18:26:01 +0300

    quickfix ClubContacts styles

--- src/apps/HomePage/components/ClubContacts/Contact.js
+++ src/apps/HomePage/components/ClubContacts/Contact.js
@@ -11,14 +11,14 @@ export default function ClubContact({ description, value, contact_type_id }) {
                 `ClubContact--${contact_type_id}`
             )}
         >
-            <div className="ClubContact__description">{description}:</div>
             <div className="ClubContact__value">
-                {String(contact_type_id) === CONTACT_TYPES.email.value ? (
-                    <a className="link" href={`mailto:${value}`}>{value}</a>
-                ) : (
-                    <span>{value}</span>
-                )}
+                {
+                    String(contact_type_id) === CONTACT_TYPES.email.value
+                        ? <a className="link" href={`mailto:${value}`}>{value}</a>
+                        : <span>{value}</span>
+                }
             </div>
+            <div className="ClubContact__description"> {description}</div>
         </div>
     );
 }
