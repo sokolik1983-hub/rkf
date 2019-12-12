a99e567229e6a63937024baba80cc6cd8cbd3ab2
Den <den.bessarab@outlook.com>
2019-12-12 17:20:26 +0300

    quickfix replace Homepage Slider images

--- src/components/News/FixedArticle.js
+++ src/components/News/FixedArticle.js
@@ -7,31 +7,31 @@ const slides = [
         id: 1,
         title: 'IDS «Eurasia»',
         url: 'http://rkf.org.ru/vystavochnaja-dejatelnost/eurasia/',
-        img: '/static/images/exhibitions/eur-exh.jpg'
+        img: '/static/images/exhibitions/eur-big.png'
     },
     {
         id: 2,
         title: 'Мемориал А.П. Мазовера',
         url: 'http://rkf.org.ru/vystavochnaja-dejatelnost/mazover/',
-        img: '/static/images/exhibitions/maz-exh.jpg'
+        img: '/static/images/exhibitions/maz-big.png'
     },
     {
         id: 3,
         title: 'IDS «Russia»',
         url: 'http://rkf.org.ru/vystavochnaja-dejatelnost/russia/',
-        img: '/static/images/exhibitions/rus-exh.jpg'
+        img: '/static/images/exhibitions/rus-big.png'
     },
     {
         id: 4,
         title: 'Шоу чемпионов «Золотой ошейник»',
         url: 'http://rkf.org.ru/vystavochnaja-dejatelnost/golden_collar/',
-        img: '/static/images/exhibitions/zol-exh.jpg'
+        img: '/static/images/exhibitions/zol-big.png'
     }
 ];
 
 const FixedArticle = () => {
     return (
-        <Card style={{ padding: '4px' }}>
+        <Card>
             <div className="list-item__body">
                 <Slider>
                     {
