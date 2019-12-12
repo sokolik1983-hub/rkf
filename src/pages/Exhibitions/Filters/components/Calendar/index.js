f2e4ebb4b613c6143044daf1af3a69cd5532ffb4
Den <den.bessarab@outlook.com>
2019-12-12 15:27:26 +0300

    quickfix hide CitySelect, add ClearAll

--- src/pages/Exhibitions/Filters/components/Calendar/index.js
+++ src/pages/Exhibitions/Filters/components/Calendar/index.js
@@ -1,36 +1,36 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import DayPicker from "react-day-picker";
 import Loading from "../../../../../components/Loading";
-import {MONTHS, WEEKDAYS_SHORT} from "../../../../../appConfig";
-import {formatDateToString} from "../../../../../utils/datetime";
-import {getYears} from "../../../utils";
-import {Request} from "../../../../../utils/request";
-import {endpointExhibitionsDates} from "../../../config";
-import {connectFilters} from "../../../connectors";
+import { MONTHS, WEEKDAYS_SHORT } from "../../../../../appConfig";
+import { formatDateToString } from "../../../../../utils/datetime";
+import { getYears } from "../../../utils";
+import { Request } from "../../../../../utils/request";
+import { endpointExhibitionsDates } from "../../../config";
+import { connectFilters } from "../../../connectors";
 import './index.scss';
 
 
-const Calendar = ({setFiltersSuccess, DateFrom}) => {
+const Calendar = ({ setFiltersSuccess, DateFrom }) => {
     const [day, setDay] = useState(new Date(DateFrom));
-    const [modifier, setModifier] = useState({selectedDate: day});
+    const [modifier, setModifier] = useState({ selectedDate: day });
     const [loading, setLoading] = useState(true);
     const years = getYears();
 
     const setNewDate = (date) => {
         setDay(date);
-        setModifier({...modifier, selectedDate: date});
+        setModifier({ ...modifier, selectedDate: date });
     };
 
     useEffect(() => {
         (() => Request({
-                url: endpointExhibitionsDates
-            }, data => {
-                setModifier({...modifier, green: data.map(day => new Date(day))});
-                setLoading(false);
-            },
+            url: endpointExhibitionsDates
+        }, data => {
+            setModifier({ ...modifier, green: data.map(day => new Date(day)) });
+            setLoading(false);
+        },
             error => {
                 console.log(error.response);
-                if(error.response) alert(`Ошибка: ${error.response.status}`);
+                if (error.response) alert(`Ошибка: ${error.response.status}`);
                 setLoading(false);
             }))();
     }, []);
@@ -40,12 +40,12 @@ const Calendar = ({setFiltersSuccess, DateFrom}) => {
     }, [DateFrom]);
 
     const handleFormChange = e => {
-        const {year, month} = e.target.form;
+        const { year, month } = e.target.form;
         setNewDate(new Date(year.value, month.value));
     };
 
     return loading ?
-        <Loading/> :
+        <Loading /> :
         <div className="exhibitions-calendar">
             <h4 className="exhibitions-calendar__title">Календарь выставок</h4>
             <DayPicker
@@ -55,10 +55,10 @@ const Calendar = ({setFiltersSuccess, DateFrom}) => {
                 weekdaysShort={WEEKDAYS_SHORT}
                 modifiers={modifier}
                 locale="ru"
-                onDayClick={date => setFiltersSuccess({DateFrom: formatDateToString(date), PageNumber: 1})}
+                onDayClick={date => setFiltersSuccess({ DateFrom: formatDateToString(date), DateTo: formatDateToString(date), PageNumber: 1 })}
                 onMonthChange={date => setNewDate(date)}
                 firstDayOfWeek={1}
-                captionElement={({date}) => (
+                captionElement={({ date }) => (
                     <form className="DayPicker-Caption DayPickerForm">
                         <select name="month" onChange={handleFormChange} value={date.getMonth()}>
                             {MONTHS.map((month, i) => (
--- src/pages/Exhibitions/Filters/index.js
+++ src/pages/Exhibitions/Filters/index.js
@@ -1,19 +1,21 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import Card from "../../../components/Card";
 import Aside from "../../../components/Layouts/Aside";
 import Calendar from "./components/Calendar";
-import MyExhibitionsFilter from "./components/MyExhibition";
-import CitiesFilter from "./components/CitiesFilter/CititesFilter";
-import {connectAuthVisible} from "../../../apps/Auth/connectors";
-import {connectShowFilters} from "../../../components/Layouts/connectors";
+// import MyExhibitionsFilter from "./components/MyExhibition";
+// import CitiesFilter from "./components/CitiesFilter/CititesFilter";
+import { connectAuthVisible } from "../../../apps/Auth/connectors";
+import { connectShowFilters } from "../../../components/Layouts/connectors";
+import { connectFilters } from "pages/Exhibitions/connectors";
+import { getEmptyFilters } from "pages/Exhibitions/utils";
 import './index.scss';
 
 
-const Filters = ({isAuthenticated, isOpenFilters}) => {
+const Filters = ({ isAuthenticated, isOpenFilters, setFiltersSuccess }) => {
     const setOverflow = (isOpen) => {
-        if(window.innerWidth <= 768) {
+        if (window.innerWidth <= 768) {
             document.body.style.overflow = isOpen ? 'hidden' : '';
-        } else if(window.innerWidth > 768 && isOpen) {
+        } else if (window.innerWidth > 768 && isOpen) {
             document.body.style.overflow = '';
         }
     };
@@ -24,16 +26,24 @@ const Filters = ({isAuthenticated, isOpenFilters}) => {
         return () => window.removeEventListener('resize', () => setOverflow(isOpenFilters));
     }, [isOpenFilters]);
 
+    const clearAll = (e) => {
+        e.preventDefault();
+        setFiltersSuccess(getEmptyFilters());
+        const searchCancel = document.getElementsByClassName('ExhibitionsSearch__cancel')[0]; // TODO: make this better
+        if (searchCancel) searchCancel.click();
+    }
+
     return (
         <Aside className={`exhibitions-page__left${isOpenFilters ? ' _open' : ''}`}>
             <Card>
-                <Calendar/>
-                <h4 className="exhibitions-filters__title">Фильтры</h4>
-                {isAuthenticated && <MyExhibitionsFilter/>}
-                <CitiesFilter/>
+                <Calendar />
+                {/* <h4 className="exhibitions-filters__title">Фильтры</h4>
+                {isAuthenticated && <MyExhibitionsFilter />}
+                <CitiesFilter /> */}
+                <a href="/" className="link" onClick={clearAll}>Сбросить все параметры</a>
             </Card>
         </Aside>
     )
 };
 
-export default connectShowFilters(connectAuthVisible(React.memo(Filters)));
+export default connectShowFilters(connectAuthVisible(connectFilters(React.memo(Filters))));
