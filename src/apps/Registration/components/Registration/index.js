dae9a78e55a3c769b0aacc682cd853b946535bef
Den <den.bessarab@outlook.com>
2019-12-17 14:26:15 +0300

    quickfix Registraion layout style

--- src/apps/Registration/components/Registration/index.js
+++ src/apps/Registration/components/Registration/index.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
-import Layout from './Layout';
+import CommonLayout from 'components/Layout/CommonRegistrationLogin';
 import Modal from 'components/Modal';
 import { useDictionary } from 'apps/Dictionaries';
 import { DICTIONARIES } from 'apps/Dictionaries/config';
 import ActivateClub from './ActivateClub';
 import Select from 'react-select';
+import './style.scss';
 
 const Registration = () => {
     const [clubs, setClubs] = useState(undefined);
@@ -35,43 +36,48 @@ const Registration = () => {
     };
 
     return (
-        <Layout title="Регистрация">
-            <Select
-                className="registration__select"
-                placeholder={'Введите название города'}
-                noOptionsMessage={() => 'Город не найден'}
-                options={selectOptions}
-                onChange={onCityChange}
-                styles={{
-                    input: styles => ({
-                        ...styles,
-                        width: 200
-                    }),
-                    container: styles => ({
-                        ...styles,
-                        width: 300
-                    })
-                }}
-            />
-            {clubs &&
-                <>
-                    {clubs.length ?
-                        <ul>
-                            {clubs.map(club =>
-                                <li key={club.club_id} onClick={() => onClubClick(club.club_id)}>
-                                    {club.name ? club.name : club.legal_name}
-                                </li>
-                            )}
-                        </ul> :
-                        <h3>Ничего не найдено</h3>
-                    }
-                    <p className="registration__not-found">Если Вы не нашли Ваш клуб в списке, воспользуйтесь формой обратной связи</p>
-                </>
-            }
+        <CommonLayout image={'/static/images/registration/banner.png'}>
+            <div className="registration__holder registration">
+                <div className="registration__logo" />
+                <div className="registration__title">Регистрация</div>
+                <Select
+                    className="registration__select"
+                    placeholder={'Введите название города'}
+                    noOptionsMessage={() => 'Город не найден'}
+                    options={selectOptions}
+                    onChange={onCityChange}
+                    styles={{
+                        input: styles => ({
+                            ...styles,
+                            width: 200
+                        }),
+                        container: styles => ({
+                            ...styles,
+                            width: 300
+                        })
+                    }}
+                />
+                {clubs &&
+                    <>
+                        {clubs.length ?
+                            <ul className="registration__list">
+                                {clubs.map(club =>
+                                    <li key={club.club_id} onClick={() => onClubClick(club.club_id)}>
+                                        {club.name ? club.name : club.legal_name}
+                                    </li>
+                                )}
+                            </ul> :
+                            <h3>Ничего не найдено</h3>
+                        }
+                        <p className="registration__not-found">Если Вы не нашли Ваш клуб в списке, воспользуйтесь формой обратной связи</p>
+                    </>
+                }
+            </div>
+
             {activeClub && <Modal showModal={showModal} handleClose={onModalClose}>
                 <ActivateClub club={activeClub} />
             </Modal>}
-        </Layout>
+        </CommonLayout>
     );
 };
 
