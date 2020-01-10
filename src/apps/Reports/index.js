bd8c746d1edceeccd8f7a34f95cb242e8beaaf49
Leon <leon1258@yandex.ru>
2020-01-10 07:13:21 +0000

    Merged in bugfix/1589_fix_menu_on_reports (pull request #231)
    fix menu on reports page

--- src/apps/Reports/index.js
+++ src/apps/Reports/index.js
@@ -5,25 +5,24 @@ import { getPathFromRouterParams } from 'utils/index';
 import reducer from './reducer';
 import ExhibitionsList from './components/ExhibitionsList/';
 import ReportDetails from './components/ReportDetails/';
-import PublicLayout from 'components/Layout';
 import { defaultReduxKey } from "./config";
 import { ReportsPathContext } from './context'
-import FooterSmall from 'components/Layout/FooterSmall';
 import injectReducer from "utils/injectReducer";
+import Layout from "../../components/Layouts";
+import Container from "../../components/Layouts/Container";
 
 function ExhibitionsProxy(props) {
     const path = getPathFromRouterParams(props);
     return (
         <ReportsPathContext.Provider value={{ path }}>
-            <PublicLayout>
-                <div style={{ padding: '60px 10px 10px', maxWidth: 'none', minHeight: 'calc(100vh - 267px)' }}>
+            <Layout>
+                <Container className="content reports-page">
                     <Switch>
                         <Route exact path={path} component={() => <ExhibitionsList path={path} />} />
                         <Route path={`${path}/:id`} component={ReportDetails} />
                     </Switch>
-                </div>
-                <FooterSmall />
-            </PublicLayout>
+                </Container>
+            </Layout>
         </ReportsPathContext.Provider>
     );
 
