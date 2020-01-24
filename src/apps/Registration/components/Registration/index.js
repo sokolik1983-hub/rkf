f1e9e1b64a29a4df5a7525106125c3d3c48107dd
Leon <leon1258@yandex.ru>
2020-01-24 08:52:30 +0000

    Merged in feature/new_registration_page (pull request #252)
    add tabs to registration page

--- src/apps/Registration/components/Registration/index.js
+++ src/apps/Registration/components/Registration/index.js
@@ -1,84 +1,42 @@
 import React, { useState } from 'react';
-import CommonLayout from 'components/Layout/CommonRegistrationLogin';
-import Modal from 'components/Modal';
-import { useDictionary } from 'apps/Dictionaries';
-import { DICTIONARIES } from 'apps/Dictionaries/config';
-import ActivateClub from './ActivateClub';
-import Select from 'react-select';
+import CommonLayout from "../../../../components/Layout/CommonRegistrationLogin";
+import ClubRegistration from "./ClubRegistration";
 import './style.scss';
 
+
 const Registration = () => {
-    const [clubs, setClubs] = useState(undefined);
-    const [activeClub, setActiveClub] = useState(null);
-    const [showModal, setShowModal] = useState(false);
-    const onClubClick = (id) => {
-        setShowModal(true);
-        setActiveClub(...clubs.filter((club) => club.club_id === id));
-    }
-    const onModalClose = () => {
-        if (showModal && window.confirm("Закрыть?")) {
-            setShowModal(false);
-            setActiveClub(false);
-        }
-    }
-    const { dictionary } = useDictionary(DICTIONARIES.cities);
-    const selectOptions = [
-        { value: 'reset', label: 'Все города' },
-        ...dictionary.options
-    ];
-    const onCityChange = value => {
-        if (value.value && value.value !== 'reset') {
-            fetch('/api/Club/short_all?LegalCityId=' + value.value + '&QueryDate=' + new Date().toISOString().split('T')[0], { method: "GET" })
-                .then(response => response.json())
-                .then(({ result }) => setClubs([...result]))
-        }
-        if (value.value === 'reset') setClubs(undefined);
-    };
+    const [activeTab, setActiveTab] = useState('club');
 
     return (
         <CommonLayout image={'/static/images/registration/banner.png'}>
-            <div className="registration__holder registration">
-                <div className="registration__logo" />
-                <div className="registration__title">Регистрация</div>
-                <Select
-                    className="registration__select"
-                    placeholder={'Введите название города'}
-                    noOptionsMessage={() => 'Город не найден'}
-                    options={selectOptions}
-                    onChange={onCityChange}
-                    styles={{
-                        input: styles => ({
-                            ...styles,
-                            width: 200
-                        }),
-                        container: styles => ({
-                            ...styles,
-                            width: 300
-                        })
-                    }}
-                />
-                {clubs &&
-                    <>
-                        {clubs.length ?
-                            <ul className="registration__list">
-                                {clubs.map(club =>
-                                    <li key={club.club_id} onClick={() => onClubClick(club.club_id)}>
-                                        {club.name ? club.name : club.legal_name}
-                                    </li>
-                                )}
-                            </ul> :
-                            <h3>Ничего не найдено</h3>
-                        }
-                        <p className="registration__not-found">Если Вы не нашли Ваш клуб в списке, воспользуйтесь формой обратной связи</p>
-                    </>
-                }
+            <h1 className="registration__title">Регистрация</h1>
+            <div className="registration__tabs">
+                <div className={`registration__tab${activeTab === 'club' ? ' _active' : ''}`}
+                    onClick={() => setActiveTab('club')}
+                >
+                    Клуб
+                </div>
+                <div className={`registration__tab${activeTab === 'owner' ? ' _active' : ''}`}
+                     onClick={() => null} //setActiveTab('owner')
+                >
+                    Владелец
+                </div>
+                <div className={`registration__tab${activeTab === 'kennel' ? ' _active' : ''}`}
+                     onClick={() => null} //setActiveTab('kennel')
+                >
+                    Питомник
+                </div>
+                <div className={`registration__tab${activeTab === 'nkp' ? ' _active' : ''}`}
+                     onClick={() => null} //setActiveTab('nkp')
+                >
+                    НКП
+                </div>
             </div>
-
-            {activeClub && <Modal showModal={showModal} handleClose={onModalClose}>
-                <ActivateClub club={activeClub} />
-            </Modal>}
+            {activeTab === 'club' &&
+                <ClubRegistration/>
+            }
         </CommonLayout>
     );
 };
 
-export default Registration;
+export default React.memo(Registration);
