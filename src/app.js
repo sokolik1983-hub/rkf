a9af476a63336ad6607536516b97cfddaa0f5ba9
Den <den.bessarab@outlook.com>
2020-01-30 14:14:07 +0300

    quickfix add ScrollToTop

--- src/app.js
+++ src/app.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
 import App from "./apps/App/index";
 import configureStore from './store'
 import history from 'utils/history';
-import {ConnectedRouter} from 'connected-react-router';
+import ScrollToTop from 'utils/ScrollToTop'
+import { ConnectedRouter } from 'connected-react-router';
 //Store
 const store = configureStore({}, history);
 
 ReactDOM.render(
     <Provider store={store}>
         <ConnectedRouter history={history}>
-            <App/>
+            <ScrollToTop>
+                <App />
+            </ScrollToTop>
         </ConnectedRouter>
     </Provider>,
     document.getElementById('wrap')
