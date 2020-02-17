6a5bed199d36d140b02c0d3b43a530092add0386
dadychenkoleonid <leon1258@yandex.ru>
2020-02-17 15:51:20 +0300

    add cities filter on clubs page

--- src/pages/Clubs/components/ClubsList/ListItem.js
+++ src/pages/Clubs/components/ClubsList/ListItem.js
@@ -1,103 +1,87 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import Card from "components/Card";
-import { connectFilters } from "pages/Exhibitions/connectors";
-//import { getEmptyFilters } from "pages/Exhibitions/utils";
-
-const ListItem = ({
-    id,
-    is_active,
-    setFiltersSuccess,
-    city,
-    federation_name,
-    club_alias,
-    club_owner,
-    club_name,
-    content,
-    club_logo,
-    federation_link
-}) => {
+import {Link} from "react-router-dom";
+import Card from "../../../../components/Card";
+import {DEFAULT_IMG} from "../../../../appConfig";
+import {connectFilters} from "../../connectors";
+import {getEmptyFilters} from "../../utils";
 
 
+const ListItem = ({is_active, setFilters, city, cityId, federation_name, club_alias, club_owner, club_name, content, club_logo, federation_link}) => {
     const handleCityClick = (e) => {
         e.preventDefault();
-        // setFiltersSuccess({
-        //     ...getEmptyFilters(),
-        //     ExhibitionName: city,
-        //     PageNumber: 1
-        // });
+        setFilters({
+            ...getEmptyFilters(),
+            city_ids: [cityId],
+            page: 1
+        });
     };
 
-    return <Card className="ListItem">
-        <div className="ListItem__content">
-            <div className="ListItem__content-inner">
-
-                <div className="ListItem__header">
-                    {
-                        is_active
-                            ? <Link className="ListItem__author" to={`/${club_alias}`}>
+    return (
+        <Card className="ListItem">
+            <div className="ListItem__content">
+                <div className="ListItem__content-inner">
+                    <div className="ListItem__header">
+                        {is_active ?
+                            <Link className="ListItem__author" to={`/${club_alias}`}>
                                 <div className="ListItem__club-logo" style={{
-                                    backgroundImage: `url(${club_logo ? club_logo : '/static/images/noimg/no-avatar.png'})`
+                                    backgroundImage: `url(${club_logo ? club_logo : DEFAULT_IMG.clubAvatar})`
                                 }} />
                                 <span>{club_name ? club_name : 'Название клуба отсутствует'}</span>
-                            </Link>
-                            : <p className="ListItem__author">
-                                <div className="ListItem__club-logo" style={{
-                                    backgroundImage: `url(${club_logo ? club_logo : '/static/images/noimg/no-avatar.png'})`
+                            </Link> :
+                            <p className="ListItem__author">
+                                <span className="ListItem__club-logo" style={{
+                                    backgroundImage: `url(${club_logo ? club_logo : DEFAULT_IMG.clubAvatar})`
                                 }} />
                                 <span>{club_name ? club_name : 'Название клуба отсутствует'}</span>
                             </p>
-                    }
-                    {city && <a onClick={handleCityClick} className="ListItem__city" href="/" >{city}</a>}
-                </div>
-
-                <div className="ListItem__info">
-                    <div>
-                        <span className="ListItem__subtitle">Федерация</span>
-                        <p>{federation_name && federation_link ? <Link to={federation_link}>{federation_name}</Link> : 'Отсутствует'}</p>
-                    </div>
-                    {is_active &&
-                        <div>
-                            <span className="ListItem__subtitle">Руководитель клуба</span>
-                            <p>{club_owner ? <Link to={`/${club_alias}`}>{club_owner}</Link> : 'Отсутствует'}</p>
-                        </div>
-                    }
-                </div>
-                {
-                    is_active
-                        ? <div className="ListItem__text">{content}</div>
-                        : <div className="ListItem__text centered">Клуб не прошёл регистрацию в электронной системе РКФ</div>
-                }
-                <div className="ListItemMobile">
-                    <div className="ListItemMobile__header">
+                        }
                         {city && <a onClick={handleCityClick} className="ListItem__city" href="/" >{city}</a>}
                     </div>
-                    {
-                        is_active
-                            ? <Link className="ListItemMobile__author" to={`/${club_alias}`}>
+                    <div className="ListItem__info">
+                        <div>
+                            <span className="ListItem__subtitle">Федерация</span>
+                            <p>{federation_name && federation_link ? <Link to={federation_link}>{federation_name}</Link> : 'Отсутствует'}</p>
+                        </div>
+                        {is_active &&
+                            <div>
+                                <span className="ListItem__subtitle">Руководитель клуба</span>
+                                <p>{club_owner ? <Link to={`/${club_alias}`}>{club_owner}</Link> : 'Отсутствует'}</p>
+                            </div>
+                        }
+                    </div>
+                    {is_active ?
+                        <div className="ListItem__text">{content}</div> :
+                        <div className="ListItem__text centered">Клуб не прошёл регистрацию в электронной системе РКФ</div>
+                    }
+                    <div className="ListItemMobile">
+                        <div className="ListItemMobile__header">
+                            {city && <a onClick={handleCityClick} className="ListItem__city" href="/" >{city}</a>}
+                        </div>
+                        {is_active ?
+                            <Link className="ListItemMobile__author" to={`/${club_alias}`}>
                                 <div style={{
-                                    backgroundImage: `url(${club_logo ? club_logo : '/static/images/noimg/no-avatar.png'})`
+                                    backgroundImage: `url(${club_logo ? club_logo : DEFAULT_IMG.clubAvatar})`
                                 }} />
                                 <span>{club_name ? club_name : 'Название клуба отсутствует'}</span>
-                            </Link>
-                            : <p className="ListItemMobile__author">
-                                <div style={{
-                                    backgroundImage: `url(${club_logo ? club_logo : '/static/images/noimg/no-avatar.png'})`
+                            </Link> :
+                            <p className="ListItemMobile__author">
+                                <span style={{
+                                    backgroundImage: `url(${club_logo ? club_logo : DEFAULT_IMG.clubAvatar})`
                                 }} />
                                 <span>{club_name ? club_name : 'Название клуба отсутствует'}</span>
                             </p>
-                    }
-                    <div className="ListItemMobile__info">
-                        {is_active && <span>{club_owner ? <Link to={`/${club_alias}`}>{club_owner}</Link> : 'Отсутствует'}</span>}
-                        <span>{federation_name && federation_link ? <Link to={federation_link}>{federation_name}</Link> : 'Отсутствует'}</span>
+                        }
+                        <div className="ListItemMobile__info">
+                            {is_active && <span>{club_owner ? <Link to={`/${club_alias}`}>{club_owner}</Link> : 'Отсутствует'}</span>}
+                            <span>{federation_name && federation_link ? <Link to={federation_link}>{federation_name}</Link> : 'Отсутствует'}</span>
+                        </div>
+                        {!is_active && <div className="ListItemMobile__text">Клуб не прошёл регистрацию<br /> в электронной системе РКФ</div>}
                     </div>
-                    {!is_active && <div className="ListItemMobile__text">Клуб не прошёл регистрацию<br /> в электронной системе РКФ</div>}
                 </div>
-
+                {is_active && <Link className="ListItem__show-all" to={club_alias}>Подробнее</Link>}
             </div>
-            {is_active && <Link className="ListItem__show-all" to={club_alias}>Подробнее</Link>}
-        </div>
-    </Card>
+        </Card>
+    )
 };
 
 export default connectFilters(React.memo(ListItem));
---
+++ src/pages/Clubs/components/Filters/components/CitiesFilter/index.js
@@ -0,0 +1,75 @@
+import React, {useState, useEffect} from 'react';
+import Select, {components} from 'react-select';
+import Loading from "../../../../../../components/Loading";
+import CustomCheckbox from "../../../../../../components/Form/CustomCheckbox";
+import {Request} from "../../../../../../utils/request";
+import {endpointGetCities} from "../../../../config";
+import {connectFilters} from "../../../../connectors";
+import './index.scss';
+
+
+const Option = props => (
+    <components.Option {...props}>
+        <CustomCheckbox
+            id={props.value}
+            label={props.label}
+            checked={props.isSelected}
+            onChange={() => null}
+        />
+    </components.Option>
+);
+
+const CitiesFilter = ({city_ids, setFilters}) => {
+    const [loading, setLoading] = useState(true);
+    const [cities, setCities] = useState([]);
+    const [values, setValues] = useState([]);
+    const [optionsNotInValues, setOptionsNotInValues] = useState([]);
+
+    useEffect(() => {
+        (() => Request({
+            url: endpointGetCities
+        }, data => {
+            setCities(data);
+            setLoading(false);
+        },
+        error => {
+            console.log(error.response);
+            if(error.response) alert(`Ошибка: ${error.response.status}`);
+            setLoading(false);
+        }))();
+    }, []);
+
+    useEffect(() => {
+        if(cities.length) {
+            setOptionsNotInValues(cities.filter(option => city_ids.indexOf(option.value) === -1));
+            setValues(cities.filter(option => city_ids.indexOf(option.value) !== -1));
+        }
+    }, [cities, city_ids]);
+
+    const handleChange = options => {
+        setFilters({city_ids: options.map(option => option.value), page: 1});
+    };
+
+    return loading ?
+        <Loading/> :
+        <Select
+            id={'cities-filter'}
+            isMulti={true}
+            closeMenuOnSelect={false}
+            options={[...values, ...optionsNotInValues]}
+            defaultMenuIsOpen={true}
+            hideSelectedOptions={false}
+            menuIsOpen={true}
+            controlShouldRenderValue={false}
+            onChange={handleChange}
+            clearable={true}
+            isSearchable
+            classNamePrefix={'cities-filter'}
+            placeholder={'Начните вводить город'}
+            noOptionsMessage={() => 'Город не найден'}
+            value={values}
+            components={{Option}}
+        />
+};
+
+export default connectFilters(React.memo(CitiesFilter));
