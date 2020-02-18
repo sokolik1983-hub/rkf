f355bd12aa69b2b23ff8ac92c78900250f0fbecb
Den <den.bessarab@outlook.com>
2020-02-18 17:01:57 +0300

    quickfix Homepage map handle placemark click

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -26,10 +26,15 @@ const ClubsMap = () => {
     return <YMaps>
         <Map defaultState={{ center: [55.76, 37.64], zoom: 10 }} width="100%" height="100%">
             <ObjectManager
-                options={{ clusterize: true, gridSize: 32, clusterDisableClickZoom: true }}
-                objects={{ preset: 'islands#greenDotIcon' }}
+                options={{ clusterize: true, gridSize: 32, clusterDisableClickZoom: true, geoObjectOpenBalloonOnClick: true }}
+                objects={{ preset: 'islands#greenDotIcon'}}
                 clusters={{ preset: 'islands#greenClusterIcons' }}
                 defaultFeatures={data.features}
+                modules={[
+                    'objectManager.addon.objectsBalloon',
+                    'objectManager.addon.clustersBalloon',
+                    'objectManager.addon.objectsHint',
+                ]}
             />
         </Map>
     </YMaps>
