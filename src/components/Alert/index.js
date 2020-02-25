1f938e6a87314319fc5d42c1bb77085de46e5ddc
Николай Чистяков <grotezg@live.com>
2020-02-25 12:23:35 +0300

    [!] alert close buttons

--- src/components/Alert/index.js
+++ src/components/Alert/index.js
@@ -13,7 +13,12 @@ const Alert = ({
         <div className="Alert__title">Поделиться</div>
         <div className="Alert__text">Ссылка скопирована в буфер обмена</div>
         <div className="Alert__bt">
-            <Button>OK</Button>
+            <Button primary>OK</Button>
+        </div>
+        <div className="Alert__cross">
+            <svg width="10.5" height="10.5" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M11.25 1.8075L10.1925 0.75L6 4.9425L1.8075 0.75L0.75 1.8075L4.9425 6L0.75 10.1925L1.8075 11.25L6 7.0575L10.1925 11.25L11.25 10.1925L7.0575 6L11.25 1.8075Z" fill="#90999E"/>
+            </svg>
         </div>
     </div>
 ), document.querySelector("body"));
