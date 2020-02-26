bc6f16d2c18323a8c532259d26fef25a240e7c2d
dadychenkoleonid <leon1258@yandex.ru>
2020-02-26 17:06:30 +0300

    fix reducer

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
 import history from 'utils/history';
+import authenticationReducer from "../pages/Login/reducer";
 // import messagesReducer from 'apps/Messages/reducer'
 // import clientCommonReducer from 'apps/Client/reducer'
 import dictionariesReducer from 'apps/Dictionaries/reducer'
@@ -9,6 +10,7 @@ import homePageReducer from 'pages/Home/reducer';
 export default function createReducer(injectedReducers) {
     return combineReducers({
         router: connectRouter(history),
+        authentication: authenticationReducer,
         // messages: messagesReducer,
         dictionaries: dictionariesReducer,
         homepage: homePageReducer,
