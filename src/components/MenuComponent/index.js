7171f7c376d74303f30a2d95e2455f58d9f9c639
Den <den.bessarab@outlook.com>
2020-02-27 15:06:07 +0300

    quickfix disable MenuComponent links, add Alert

--- src/components/MenuComponent/index.js
+++ src/components/MenuComponent/index.js
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../Card";
+import Alert from 'components/Alert'
 import "./index.scss";
 
 
-const MenuComponent = ({alias, name, btnName, btnHref, items}) => (
+const MenuComponent = ({ alias, name, btnName, btnHref, items }) => {
+    const [showAlert, setShowAlert] = useState(false);
 
-    <Card className="menu-component">
+    const handleClick = e => {
+        e.preventDefault();
+        setShowAlert(true);
+    }
+
+    const handleClose = e => setShowAlert(false);
+
+    return <Card className="menu-component">
+        {showAlert && <Alert
+            title="Внимание!"
+            text="Раздел находится в разработке."
+            autoclose={1.5}
+            onOk={handleClose}
+        />}
         <h4 className="menu-component__title">{name}</h4>
         <ul className="menu-component__list">
             {items.map((item, index) => (
                 <li key={index} className="menu-component__item">
-                <a href={item.href} className="menu-component__link">{item.title}</a>
-            </li>
+                    <a href={item.href} className="menu-component__link disabled" onClick={handleClick}>{item.title}</a>
+                </li>
             ))}
         </ul>
         <a href={btnHref} className="menu-component__button">{btnName}</a>
     </Card>
-);
+};
 export default React.memo(MenuComponent);
