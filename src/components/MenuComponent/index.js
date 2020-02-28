03f4e5d0c730f3e1bd7273bc9972b3b45c466202
dadychenkoleonid <leon1258@yandex.ru>
2020-02-28 17:53:55 +0300

    add presidium in menu component

--- src/components/MenuComponent/index.js
+++ src/components/MenuComponent/index.js
@@ -3,36 +3,35 @@ import {Link} from "react-router-dom";
 import Card from "../Card";
 import Alert from "../Alert";
 import "./index.scss";
+import Modal from "../Modal";
 
 
-const MenuComponent = ({alias, name, btnName}) => {
+const MenuComponent = ({alias, name, btnName, presidium}) => {
     const [showAlert, setShowAlert] = useState(false);
+    const [showModal, setShowModal] = useState(false);
 
     const handleClick = e => {
         e.preventDefault();
         setShowAlert(true);
     };
 
-    const handleClose = () => setShowAlert(false);
+    const onPresidiumClick = e => {
+        e.preventDefault();
+        setShowModal(true);
+    };
 
     return (
         <Card className="menu-component">
-            {showAlert &&
-                <Alert
-                    title="Внимание!"
-                    text="Раздел находится в разработке."
-                    autoclose={1.5}
-                    onOk={handleClose}
-                />
-            }
             <h4 className="menu-component__title" title={`Меню ${name}`}>{`Меню ${name}`}</h4>
             <ul className="menu-component__list">
                 <li className="menu-component__item">
                     <Link to={`/exhibitions?Alias=${alias}`} className="menu-component__link">Мероприятия</Link>
                 </li>
-                <li className="menu-component__item">
-                    <Link to="/" onClick={handleClick} className="menu-component__link">Президиум</Link>
-                </li>
+                {presidium &&
+                    <li className="menu-component__item">
+                        <Link to="/" onClick={onPresidiumClick} className="menu-component__link">Президиум</Link>
+                    </li>
+                }
                 <li className="menu-component__item">
                     <Link to="/" onClick={handleClick} className="menu-component__link not-active">Новости</Link>
                 </li>
@@ -43,6 +42,26 @@ const MenuComponent = ({alias, name, btnName}) => {
                 }
             </ul>
             <Link to={`/${alias}`} className="menu-component__button">{btnName}</Link>
+            {showAlert &&
+                <Alert
+                    title="Внимание!"
+                    text="Раздел находится в разработке."
+                    autoclose={1.5}
+                    onOk={() => setShowAlert(false)}
+                />
+            }
+            {showModal &&
+                <Modal className="menu-component__modal" showModal={showModal} handleClose={() => setShowModal(false)} noBackdrop={true}>
+                    <div className="menu-component__presidium">
+                        <h4 className="menu-component__presidium-title">{presidium.title}</h4>
+                        <ol className="menu-component__presidium-list">
+                            {presidium.members.map((member, i) =>
+                                <li className="menu-component__presidium-item" key={i}>{member}</li>
+                            )}
+                        </ol>
+                    </div>
+                </Modal>
+            }
         </Card>
     )
 };
