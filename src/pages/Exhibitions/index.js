14ebbb478085399b24dcd33af7dc5f09d001dbcd
Николай Чистяков <grotezg@live.com>
2020-03-06 16:07:54 +0300

    [+] move exhibitions calendar request up the component tree

--- src/pages/Exhibitions/index.js
+++ src/pages/Exhibitions/index.js
@@ -6,14 +6,45 @@ import ExhibitionsSearch from "./components/Filters/components/Search";
 import ExhibitionsList from "./components/ExhibitionsList";
 import ClickGuard from "../../components/ClickGuard";
 import TopComponent from "../../components/TopComponent";
+import {Request} from "utils/request";
 import {connectShowFilters} from "../../components/Layouts/connectors";
 import {buildUrl, getFiltersFromUrl, getInitialFilters} from "./utils";
 import './index.scss';
 
 
 const Exhibitions = ({history, isOpenFilters, setShowFilters}) => {
+    const [exhibitions, setExhibitions] = useState(null);
+    const [pagesCount, setPagesCount] = useState(1);
+    const [loading, setLoading] = useState(true);
+
     const [filters, setFilters] = useState({...getInitialFilters()});
     const [url, setUrl] = useState(buildUrl({...filters}));
+    
+    const getExhibitions = async (url) => {
+        setLoading(true);
+        await Request({
+            url: url
+        }, data => {
+            const modifiedExhibitions = data.exhibitions.map(exhibition => {
+                exhibition.title = exhibition.city;
+                exhibition.create_date = new Date(exhibition.dates[0].year, exhibition.dates[0].month - 1, exhibition.dates[0].day);
+                exhibition.content = exhibition.exhibition_name;
+                exhibition.url = `/exhibitions/${exhibition.id}`;
+                return exhibition;
+            });
+            setExhibitions(modifiedExhibitions);
+            setPagesCount(data.page_count);
+            setLoading(false);
+        }, error => {
+            console.log(error.response);
+            if (error.response) alert(`Ошибка: ${error.response.status}`);
+            setLoading(false);
+        });
+    };
+
+    useEffect(() => {
+        if (url) (() => getExhibitions(url))();
+    }, [url]);
 
     useEffect(() => {
         const unListen = history.listen(() => {
@@ -28,17 +59,24 @@ const Exhibitions = ({history, isOpenFilters, setShowFilters}) => {
     return (
         <Layout withFilters>
             {filters.Alias && 
-                <TopComponent
-                    logo={"/static/images/header/rkf-logo-transparent.svg"}
-                    name={filters.Alias}
-                />
+                <div className="exhibitions-page__top-wrap">
+                    <TopComponent
+                        logo={"/static/images/header/rkf-logo-transparent.svg"}
+                        name={filters.Alias}
+                    />
+                </div>
             }
             <ClickGuard value={isOpenFilters} callback={() => setShowFilters({isOpenFilters: false})}/>
             <Container className="content exhibitions-page">
                 <Filters filters={filters}/>
                 <div className="exhibitions-page__content">
                     <ExhibitionsSearch ExhibitionName={filters.ExhibitionName} />
-                    <ExhibitionsList url={url} PageNumber={filters.PageNumber}/>
+                    <ExhibitionsList 
+                        exhibitions={exhibitions}
+                        loading={loading}
+                        pagesCount={pagesCount}
+                        PageNumber={filters.PageNumber}
+                    />
                 </div>
             </Container>
         </Layout>
