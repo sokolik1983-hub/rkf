d0370e6d1d89672cebf85e4289103744c1176fba
dadychenkoleonid <leon1258@yandex.ru>
2020-03-06 12:12:07 +0300

    fix exhibition page

--- src/pages/Exhibition/components/ExhibitionInfo/index.js
+++ src/pages/Exhibition/components/ExhibitionInfo/index.js
@@ -51,106 +51,108 @@ const ExhibitionInfo = ({
         } else return breeds.join(', ');
     };
 
-    return (<>
-        <Card className="exhibition-info">
-            <div className="exhibition-info__left">
-                <h4 className="exhibition-info__title">Информация о мероприятии</h4>
-                {dates &&
-                    <>
-                        <h5 className="exhibition-info__subtitle">{`${declension(dates.length, ['Дата', 'Даты', 'Даты'])} проведения:`}</h5>
-                        <div className="exhibition-info__dates">
-                            {dates.map((date, i) => (
-                                <p key={i} className="exhibition-info__date">
-                                    {`${capitalizeFirstLetter(getLocalizedWeekDay(transformDateSafariFriendly(date)))}, ${date.day < 10 ? '0' + date.day : date.day}.${date.month < 10 ? '0' + date.month : date.month}.${date.year}${date.time_start ? ' c ' + timeSecondsCutter(date.time_start) : ''}${date.time_end ? ' до ' + timeSecondsCutter(date.time_end) + ' по МСК' : ''}`}
-                                </p>
-                            ))}
-                        </div>
-                    </>
-                }
-                {city && <p className="exhibition-info__address">{`г. ${city}${address ? ', ' + address : ''}`}</p>}
-                <table className="exhibition-info__table">
-                    <tbody>
-                        {rankTypes && !!rankTypes.length && <tr>
-                            <td>Ранг:</td>
-                            <td>{rankTypes.join(', ')}</td>
-                        </tr>}
-                        {breedTypes && !!breedTypes.length && <tr>
-                            <td>Породы:</td>
-                            <td>{resolveBreeds(rankTypes, breedTypes)}</td>
-                        </tr>}
-                    </tbody>
-                </table>
-            </div>
-            <div className="exhibition-info__right">
-                {dates && !!dates.length &&
-                    <CountDown startDate={dateStart} endDate={dateEnd} reportsDateEnd={reportsDateEnd}/>
-                }
-                {/*<ul className="exhibition-info__block-list">*/}
-                {/*    <li className="exhibition-info__block-item not-active">*/}
-                {/*        <a href="/" onClick={clickOnLink}>Судьи</a>*/}
-                {/*    </li>*/}
-                {/*    <li className="exhibition-info__block-item">*/}
-                {/*        <Link to={`/exhibitions?Alias=${club_information.club_alias}`}>Другие мероприятия организатора</Link>*/}
-                {/*    </li>*/}
-                {/*    <li className="exhibition-info__block-item not-active">*/}
-                {/*        <a href="/" onClick={clickOnLink}>Платные услуги</a>*/}
-                {/*    </li>*/}
-                {/*</ul>*/}
-                {/*showAlert &&
-                    <Alert
-                        title="Внимание!"
-                        text="Раздел находится в разработке."
-                        autoclose={1.5}
-                        onOk={() => setShowAlert(false)}
-                    />
-                */}
-            </div>
-        </Card>
-        <Card className="exhibition-info">
-            <div className="exhibition-page__description">
-                <h3 className="exhibition-page__description-title">Описание</h3>
-                {description ?
-                    <p dangerouslySetInnerHTML={{ __html: description }} /> :
-                    <p>Описание отсутствует</p>
-                }
-            </div>
-        </Card>
-        <Card className="exhibition-info">
-            <div className="exhibition-page__schedule">
-                <h3 className="exhibition-page__schedule-title">Расписание</h3>
-                <p className="exhibition-documents__doc">
-                    {schedule_link ?
-                        <a href={schedule_link.url} target="__blank">{schedule_link.name}</a> :
-                        'Расписание отсутствует'
+    return (
+        <>
+            <Card className="exhibition-info">
+                <div className="exhibition-info__left">
+                    <h4 className="exhibition-info__title">Информация о мероприятии</h4>
+                    {dates &&
+                        <>
+                            <h5 className="exhibition-info__subtitle">{`${declension(dates.length, ['Дата', 'Даты', 'Даты'])} проведения:`}</h5>
+                            <div className="exhibition-info__dates">
+                                {dates.map((date, i) => (
+                                    <p key={i} className="exhibition-info__date">
+                                        {`${capitalizeFirstLetter(getLocalizedWeekDay(transformDateSafariFriendly(date)))}, ${date.day < 10 ? '0' + date.day : date.day}.${date.month < 10 ? '0' + date.month : date.month}.${date.year}${date.time_start ? ' c ' + timeSecondsCutter(date.time_start) : ''}${date.time_end ? ' до ' + timeSecondsCutter(date.time_end) + ' по МСК' : ''}`}
+                                    </p>
+                                ))}
+                            </div>
+                        </>
                     }
-                </p>
-            </div>
-        </Card>
-        <Card className="exhibition-info">
-            <div className="exhibition-page__catalog">
-                <h3 className="exhibition-page__catalog-title">Каталог</h3>
-                <p className="exhibition-documents__doc">
-                    {catalog_link ?
-                        <a href={catalog_link.url} target="__blank">{catalog_link.name}</a> :
-                        'Каталог отсутствует'
+                    {city && <p className="exhibition-info__address">{`г. ${city}${address ? ', ' + address : ''}`}</p>}
+                    <table className="exhibition-info__table">
+                        <tbody>
+                            {rankTypes && !!rankTypes.length && <tr>
+                                <td>Ранг:</td>
+                                <td>{rankTypes.join(', ')}</td>
+                            </tr>}
+                            {breedTypes && !!breedTypes.length && <tr>
+                                <td>Породы:</td>
+                                <td>{resolveBreeds(rankTypes, breedTypes)}</td>
+                            </tr>}
+                        </tbody>
+                    </table>
+                </div>
+                <div className="exhibition-info__right">
+                    {dates && !!dates.length &&
+                        <CountDown startDate={dateStart} endDate={dateEnd} reportsDateEnd={reportsDateEnd}/>
                     }
-                </p>
-            </div>
-        </Card>
-        <Card className="exhibition-info">
-            <div className="exhibition-page__documents">
-                <h3 className="exhibition-page__documents-title">Документы</h3>
-                {documents_links && !!documents_links.length ?
-                    documents_links.map(doc => (
-                        <p className="exhibition-documents__doc" key={doc.id}>
-                            <a href={doc.url} target="__blank">{doc.name}</a>
-                        </p>
-                    )) :
-                    <p className="exhibition-documents__doc">Документы отсутствуют</p>
-                }
-            </div>
-        </Card>
-    </>)
+                    {/*<ul className="exhibition-info__block-list">*/}
+                    {/*    <li className="exhibition-info__block-item not-active">*/}
+                    {/*        <a href="/" onClick={clickOnLink}>Судьи</a>*/}
+                    {/*    </li>*/}
+                    {/*    <li className="exhibition-info__block-item">*/}
+                    {/*        <Link to={`/exhibitions?Alias=${club_information.club_alias}`}>Другие мероприятия организатора</Link>*/}
+                    {/*    </li>*/}
+                    {/*    <li className="exhibition-info__block-item not-active">*/}
+                    {/*        <a href="/" onClick={clickOnLink}>Платные услуги</a>*/}
+                    {/*    </li>*/}
+                    {/*</ul>*/}
+                    {/*showAlert &&
+                        <Alert
+                            title="Внимание!"
+                            text="Раздел находится в разработке."
+                            autoclose={1.5}
+                            onOk={() => setShowAlert(false)}
+                        />
+                    */}
+                </div>
+            </Card>
+            <Card className="exhibition-info">
+                <div className="exhibition-page__description">
+                    <h3 className="exhibition-page__description-title">Описание</h3>
+                    {description ?
+                        <p dangerouslySetInnerHTML={{ __html: description }} /> :
+                        <p>Описание отсутствует</p>
+                    }
+                </div>
+            </Card>
+            <Card className="exhibition-info">
+                <div className="exhibition-page__schedule">
+                    <h3 className="exhibition-page__schedule-title">Расписание</h3>
+                    <p className="exhibition-documents__doc">
+                        {schedule_link ?
+                            <a href={schedule_link.url} target="__blank">{schedule_link.name}</a> :
+                            'Расписание отсутствует'
+                        }
+                    </p>
+                </div>
+            </Card>
+            <Card className="exhibition-info">
+                <div className="exhibition-page__catalog">
+                    <h3 className="exhibition-page__catalog-title">Каталог</h3>
+                    <p className="exhibition-documents__doc">
+                        {catalog_link ?
+                            <a href={catalog_link.url} target="__blank">{catalog_link.name}</a> :
+                            'Каталог отсутствует'
+                        }
+                    </p>
+                </div>
+            </Card>
+            <Card className="exhibition-info">
+                <div className="exhibition-page__documents">
+                    <h3 className="exhibition-page__documents-title">Документы</h3>
+                    {documents_links && !!documents_links.length ?
+                        documents_links.map(doc => (
+                            <p className="exhibition-documents__doc" key={doc.id}>
+                                <a href={doc.url} target="__blank">{doc.name}</a>
+                            </p>
+                        )) :
+                        <p className="exhibition-documents__doc">Документы отсутствуют</p>
+                    }
+                </div>
+            </Card>
+        </>
+    )
 };
 
 export default React.memo(ExhibitionInfo);
