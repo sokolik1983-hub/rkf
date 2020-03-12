7afd1ee8f5fbf888fcafd82a72212d1f5d74ceb9
dadychenkoleonid <leon1258@yandex.ru>
2020-03-12 15:19:10 +0300

    fix reports date format on exhibition page

--- src/components/CountDown/index.js
+++ src/components/CountDown/index.js
@@ -65,7 +65,7 @@ const CountDown = ({startDate, endDate, reportsDateEnd, reportsLinks}) => {
                             <li className="countdown__reports-link" key={link.id}>
                                 <a href={link.url} target="_blank" rel="noopener noreferrer">{link.name}</a>
                                 {link.date_send &&
-                                    <p>Дата подачи отчёта: <span>{link.date_send}</span></p>
+                                    <p>Дата подачи отчёта: <span>{new Date(link.date_send).toLocaleDateString()}</span></p>
                                 }
                             </li>
                         ) :
