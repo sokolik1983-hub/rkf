7cd34028869a5d55548f327228b00ffdd480f244
dadychenkoleonid <leon1258@yandex.ru>
2020-03-16 15:28:22 +0300

    fix bugs on club page

--- src/pages/Club/components/ClubDescription/index.js
+++ src/pages/Club/components/ClubDescription/index.js
@@ -3,19 +3,11 @@ import Card from "../../../../components/Card";
 import './index.scss';
 
 
-const ClubDescription = ({description, clubName, federationName}) => (
+const ClubDescription = ({description}) => (
     <Card className="club-page__description">
         <h4 className="club-page__description-title"
             title="РКФ не несет ответственности за достоверность данных, размещаемых клубом в данном разделе"
         >Описание</h4>
-        {clubName && federationName &&
-            <>
-                <p className="club-page__description-info">
-                    {`Кинологическая организация "${clubName}" входит в федерацию "${federationName}".`}
-                </p>
-                <br />
-            </>
-        }
         <p className="club-page__description-info">{description}</p>
     </Card>
 );
