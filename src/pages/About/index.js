c691c121a984f983e2983c509bfbe039413a5d5c
dadychenkoleonid <leon1258@yandex.ru>
2020-03-17 17:48:03 +0300

    fix about page

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,39 +1,18 @@
-import React, {useEffect, useState} from "react";
-import Loading from "../../components/Loading";
+import React, {useState} from "react";
 import Layout from "../../components/Layouts";
 import Container from "../../components/Layouts/Container";
 import TopBanner from "./components/TopBanner";
 import NewsComponent from "../../components/NewsComponent";
 import Support from "./components/Support";
 import FeedbackComponent from "./components/Feedback";
-import DocumentsComponent from "../../components/DocumentsComponent";
-import {Request} from "../../utils/request";
-import {connectAuthVisible} from "../Login/connectors";
 import "./index.scss";
 
 
-const AboutPage = ({isAuthenticated, profile_id}) => {
-    const [info, setInfo] = useState(null);
-    const [canEdit, setCanEdit] = useState(false);
+const AboutPage = () => {
     const [page, setPage] = useState(1);
     const [needRequest, setNeedRequest] = useState(true);
-    const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        (() => Request({
-            url: '/api/Club/rkf_base_info'
-        }, data => {
-            setInfo(data);
-            setCanEdit(isAuthenticated && profile_id === data.id);
-            setLoading(false);
-        }, error => {
-            console.log(error.response);
-            setLoading(false);
-        }))();
-    }, []);
-
-    return loading ?
-        <Loading/> :
+    return (
         <Layout>
             <TopBanner/>
             <Container className="about-page__content">
@@ -46,19 +25,17 @@ const AboutPage = ({isAuthenticated, profile_id}) => {
                             setPage={setPage}
                             needRequest={needRequest}
                             setNeedRequest={setNeedRequest}
-                            canEdit={canEdit}
+                            canEdit={false}
                         />
                     </div>
                     <aside className="about-page__info">
                         <Support />
                         <FeedbackComponent/>
-                        {info.documents && !!info.documents.length &&
-                            <DocumentsComponent documents={info.documents}/>
-                        }
                     </aside>
                 </div>
             </Container>
         </Layout>
+    )
 };
 
-export default React.memo(connectAuthVisible(AboutPage));
+export default React.memo(AboutPage);
