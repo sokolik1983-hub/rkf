5cc17ee43eedb6c5794754642dd0fa7e2452277e
Николай Чистяков <grotezg@live.com>
2020-03-20 15:57:15 +0300

    [+] finish ui

--- src/pages/Docs/components/DocItem/index.js
+++ src/pages/Docs/components/DocItem/index.js
@@ -4,18 +4,22 @@ import PlusButton from "../../../../components/PlusButton";
 import {FormGroup, FormField} from "../../../../components/Form";
 import "./index.scss";
 
-
-const DocItem = ({ name, email }) => {
+const DocItem = ({ values, setValues, closeClick }) => {
     const [moreDocs, setMoreDocs] = useState(0);
-    const plusClick = () => setMoreDocs(moreDocs + 1);
+    const plusClick = e => setMoreDocs(moreDocs + 1)
+    const update = ({target}) => {
+        let patch = {[target.name]: target.value};
+        setValues({...values, ...patch});
+    }
 
     return <Card className="DocItem">
+        <img className="DocItem__cross" src="/static/icons/cross-gray.svg" onClick={closeClick}/>
         <FormGroup>
-            <FormField name='name' label='ФИО' value={name} />
-            <FormField name='email' label='Email' value={email} />
-            <FormField name='card' label='Пометная карта' type="file" />
-            <FormField name='metrics' label='Метрика щенка' type="file" />
-            {new Array(moreDocs).fill(0).map((d,i) => <FormField key={i} label={`Документ №${i + 3}`} type="file" name={`doc${i}`}/>)}
+            <FormField name='name' label='ФИО' value={values.name || ''} onChange={update} />
+            <FormField name='email' label='Email' value={values.email || ''} onChange={update} />
+            <FormField name='card' label='Пометная карта' type="file" onChange={update} />
+            <FormField name='metrics' label='Метрика щенка' type="file" onChange={update} />
+            {new Array(moreDocs).fill(0).map((d,i) => <FormField key={i} label={`Документ №${i + 3}`} type="file" name={`doc${i}`} onChange={update}/>)}
             <PlusButton onClick={plusClick}/>
         </FormGroup>
     </Card>
