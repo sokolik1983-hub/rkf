167a25e1829ec16d7ad124b2709943ec282208f1
Николай Чистяков <grotezg@live.com>
2020-03-23 17:33:01 +0300

    [+] reenable item delition

--- src/pages/Docs/components/DocApply/index.js
+++ src/pages/Docs/components/DocApply/index.js
@@ -15,6 +15,24 @@ const DocApply = ({ clubAlias }) => {
     const plusClick = e => setDocItems(docItems.concat({}));
     const clearClick = e => setDocItems([{}]);
 
+    const [docItems, setDocItems] = useState([0]);
+    const [n, setN] = useState(1);
+    const plusClick = e => {
+        setDocItems(docItems.concat(n));
+        setN(n + 1);
+    }
+    const clearClick = e => setDocItems([]);
+    const submit = () => {
+        let fd = new FormData(document.getElementsByTagName('form')[0]);
+        fetch(apiEndpoint, {method: 'POST', body: fd});
+    }
+    console.log(docItems);
+    const deleteItem = i => {
+        docItems.splice(i,1);
+        setDocItems(docItems.concat([]));
+    }
+>>>>>>> bc6039e... [+] reenable item delition
+
     return <div className="documents-page__info">
         <aside className="documents-page__left">
             <CustomMenu title="Личный кабинет">
@@ -31,22 +49,22 @@ const DocApply = ({ clubAlias }) => {
                 Это материал для страницы со списком документов
                 {data.docs.map((d,i) => <DocEntry key={i} {...d}/>)}
                                 */}
-            <Form onSuccess={x => x} action='POST'>
+            <Form onSuccess={x => x} action={apiEndpoint} initialValues={{email: data.club.email}} validationSchema={validationSchema}>
                 <Card>
                     <FormGroup>
-                        <FormField name='club_email' label='Email клуба' value={data.club.email} />
-                        <FormField name='club_bill' label='Квитанция' type="file" />
+                        <FormField name='email' label='Email клуба' />
+                        <FormField name='payment_document' label='Квитанция' type="file" />
                         <i>квитанция об оплате суммарного взноса за оформление пакета документов</i>
                     </FormGroup>
                 </Card>
                 <div className="documents-page__title-wrap">
                     <h3 className="documents-page__title">Заявители</h3>
                 </div>
-                {docItems.map((m, i) => <DocItem key={i} {...m} i={i} />)}
+                {docItems.map((m, i) => <DocItem key={m} closeClick={() => deleteItem(i)} i={m} />)}
                 <div className="flex-row">
                     <Button className="btn-primary" onClick={plusClick}>+ Добавить еще заявителя</Button>
                     <Button className="btn-transparent" onClick={clearClick}>Очистить форму</Button>
-                    <Button className="btn-green" onClick={x => console.log('click')}>Отправить</Button>
+                    <Button className="btn-green" onClick={submit}>Отправить</Button>
                 </div>
             </Form>
         </div>
