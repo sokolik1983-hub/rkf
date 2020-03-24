58721d305893871552d156ca5971f15c87a01fc2
Denis <den.bessarab@outlook.com>
2020-03-24 12:54:52 +0300

    fix menu, mobile view, styles

--- src/pages/Docs/components/DocHome/index.js
+++ src/pages/Docs/components/DocHome/index.js
@@ -2,19 +2,18 @@ import React from "react";
 import { Link } from "react-router-dom";
 import CustomMenu from "components/CustomMenu";
 import Card from 'components/Card';
-import data from "../../dummy.json";
 
 const DocHome = ({ clubAlias }) => {
 
     return <div className="documents-page__info">
         <aside className="documents-page__left">
             <CustomMenu title="Личный кабинет">
-                <Link to="/reports" title="Отчеты">Отчеты</Link>
                 <Link to={`/${clubAlias}/documents`} title="Оформление документов">Оформление документов</Link>
+                <Link to="/reports" title="Отчеты">Отчеты</Link>
+                <Link to={`/${clubAlias}`} title="Страница клуба">Страница клуба</Link>
             </CustomMenu>
         </aside>
         <div className="documents-page__right">
-            <h4 className="documents-page__title">{`Добро пожаловать в личный кабинет клуба ${data.club.display_name}`}</h4>
             <Card>
                 <h3>ЗАЯВЛЕНИЕ НА РЕГИСТРАЦИЮ ПОМЕТА</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
