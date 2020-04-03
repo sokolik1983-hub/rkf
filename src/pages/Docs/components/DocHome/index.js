23acf6096fb63a77306137f8f37b6de47da3ede8
Denis <den.bessarab@outlook.com>
2020-04-03 14:51:02 +0300

    add documents card icon

--- src/pages/Docs/components/DocHome/index.js
+++ src/pages/Docs/components/DocHome/index.js
@@ -1,10 +1,10 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import Card from "../../../../components/Card";
 import CustomMenu from "../../../../components/CustomMenu";
 
 
-const DocHome = ({clubAlias}) => (
+const DocHome = ({ clubAlias }) => (
     <div className="documents-page__info">
         <aside className="documents-page__left">
             <CustomMenu title="Личный кабинет">
@@ -15,8 +15,10 @@ const DocHome = ({clubAlias}) => (
         </aside>
         <div className="documents-page__right">
             <Card>
+                <div className="documents-page__icon" />
                 <h3>ЗАЯВЛЕНИЕ НА РЕГИСТРАЦИЮ ПОМЕТА</h3>
-                <p>В соответствии с требованиями РКФ, с заявлением на регистрацию помета так же принимаются:
+                <p>
+                    В соответствии с требованиями РКФ, с заявлением на регистрацию помета так же принимаются:
                     акт вязки, акт обследования помета, копии свидетельств о происхождении производителей,
                     копии сертификатов всех титулов и рабочих испытаний, заключения по дисплазии, и однократно -
                     оригинал диплома с сертификатной выставки РКФ, копию Свидетельства о регистрации заводской приставки FCI.</p>
@@ -27,8 +29,10 @@ const DocHome = ({clubAlias}) => (
                 </div>
             </Card>
             <Card>
+                <div className="documents-page__icon" />
                 <h3>ОФОРМЛЕНИЕ РОДОСЛОВНОЙ</h3>
-                <p>Метрика щенка не дает право на племенное использование собаки и подлежит обязательному обмену на
+                <p>
+                    Метрика щенка не дает право на племенное использование собаки и подлежит обязательному обмену на
                     свидетельство о происхождении (родословную) РКФ до достижения собакой возраста 15 месяцев.</p>
                 <hr />
                 <div className="Card__links">
