3ab0824d7059410846fe69002c210f2d8cebda8d
kolya <kc@kparc.io>
2020-04-06 16:29:57 +0300

    [+] make file inputs display validation errors

--- src/components/Form/Field/Error/index.js
+++ src/components/Form/Field/Error/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { connect, getIn } from 'formik';
 
 
-const FieldError = ({ formik, name, noTouch = false }) => {
-    const error = name ? getIn(formik.errors, name) : null;
+const FieldError = ({ formik, name, errName, noTouch = false }) => {
+    const error = name ? getIn(formik.errors, errName || name) : null;
     const touch = noTouch ? true : getIn(formik.touched, name);
     return touch && error ? <div className={noTouch ? 'FormInput__custom-error' : 'FormInput__error'}>{error}</div> : null;
 };
