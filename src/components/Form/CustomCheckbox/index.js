d81b35bf32fb647609ddd021cf0d9defe6aa83c2
kolya <kc@kparc.io>
2020-04-08 14:27:31 +0300

    [~] misc changes

--- src/components/Form/CustomCheckbox/index.js
+++ src/components/Form/CustomCheckbox/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './index.scss';
 
-const CustomCheckbox = ({id, label, checked, onChange}) => (
-    <div className="custom-checkbox">
+const CustomCheckbox = ({id, label, checked, onChange, disabled}) => (
+    <div className={`custom-checkbox ${disabled ? "disabled" : ""}`}>
         <input type="checkbox"
                id={id}
                className="custom-checkbox__input"
                checked={!!checked}
                onChange={onChange}
+               disabled={!!disabled}
         />
         <label htmlFor={id} className="custom-checkbox__label">{label}</label>
     </div>
