91e5d1002b90825cc2ae062d7290ddcb91a5f50c
kolya <kc@kparc.io>
2020-04-08 12:50:18 +0300

    [+] add cash_payment field

--- src/pages/Docs/components/DocItemList/index.js
+++ src/pages/Docs/components/DocItemList/index.js
@@ -73,14 +73,17 @@ const DocItemList = ({formik, name, doctypes, breeds, sexTypes, fedName, view, u
                     <div>
                         <FormGroup>
                             <p className={update ? 'hidden' : ''}><b>Приложите квитанцию об оплате {formik.values.declarants.length} заявок по тарифу {fedName} и заполните информацию о платеже.</b></p>
-                            <HideIf cond={view || formik.values.payment_document_accept || !statusAllowsUpdate}>
-                                <FormField fieldType="file" disabled={view} name='payment_document' label='Квитанция об оплате' accept={accept} />
+                            <FormField disabled={view || formik.values.cash_payment_accept || !statusAllowsUpdate} type="checkbox" name='cash_payment' label='Оплата наличными'/>
+                            <HideIf cond={formik.values.cash_payment}>
+                                <HideIf cond={view || formik.values.payment_document_accept || !statusAllowsUpdate}>
+                                    <FormField fieldType="file" disabled={view} name='payment_document' label='Квитанция об оплате' accept={accept} />
+                                </HideIf>
+                                <DocLink docId={formik.values.payment_document_id} label='Квитанция об оплате' showLabel={view || formik.values.payment_document_accept}/>
+                                <FormField disabled={view || formik.values.payment_date_accept || !statusAllowsUpdate} name='payment_date' label='Дата оплаты' fieldType="reactDayPicker" readOnly={true} />
+                                <FormField disabled={view || formik.values.payment_number_accept || !statusAllowsUpdate} name='payment_number' label='Номер платежного документа' />
+                                <FormField disabled={update} name='payment_name' label='ФИО плательщика/наименования юр. лица' />
+                                <FormField disabled={update} name='ogrn' label='ОГРН (для юр. лиц)' />
                             </HideIf>
-                            <DocLink docId={formik.values.payment_document_id} label='Квитанция об оплате' showLabel={view || formik.values.payment_document_accept}/>
-                            <FormField disabled={view || formik.values.payment_date_accept || !statusAllowsUpdate} name='payment_date' label='Дата оплаты' fieldType="reactDayPicker" readOnly={true} />
-                            <FormField disabled={view || formik.values.payment_number_accept || !statusAllowsUpdate} name='payment_number' label='Номер платежного документа' />
-                            <FormField disabled={update} name='payment_name' label='ФИО плательщика/наименования юр. лица' />
-                            <FormField disabled={update} name='ogrn' label='ОГРН (для юр. лиц)' />
                         </FormGroup>
                     </div>
                     <HideIf cond={view || !canSave} className="flex-row">
