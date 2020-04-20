f60f95927bad16ec2c104cc29a95ee3df373d901
Denis <den.bessarab@outlook.com>
2020-04-20 17:24:09 +0300

    add StampCards text

--- src/pages/Docs/components/DocHome/index.js
+++ src/pages/Docs/components/DocHome/index.js
@@ -73,7 +73,7 @@ const StampCards = ({ clubAlias }) => {
             <div className="documents-page__icon" />
             <h3>КЛЕЙМА</h3>
             <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                Указанные коды клейма могут быть использованы в формах оформления заявок на изготовление документов. При указании кода клейма клуба должна быть приложена электронная копия свидетельства о регистрации кода клейма или документ его заменяющий.
             </p>
             <hr />
             <div className="Card__links">
