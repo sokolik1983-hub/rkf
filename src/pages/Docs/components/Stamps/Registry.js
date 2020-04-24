900242a9b8d3d765aded4acbaaaa5b3c58100e86
Denis <den.bessarab@outlook.com>
2020-04-24 12:54:04 +0300

    add StampRegistry

--- src/pages/Docs/components/Stamps/Registry.js
+++ src/pages/Docs/components/Stamps/Registry.js
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from 'components/Card';
+import { connect } from 'react-redux';
+import Loading from "components/Loading";
+import StampsRegistry from "./components/Table";
+import { Request } from "utils/request";
 import './styles.scss';
 
-const Registry = ({ history }) => {
-    return <Card style={{ margin: 0 }}>
-        <div className="club-documents-status__head">
-            <button className="btn-backward" onClick={() => history.goBack()}>Назад</button>
-        </div>
-        <div className="Registry">
-            Registry
-        </div>
-    </Card>
+const Registry = ({ history, clubAlias, profile_id }) => {
+    const [loading, setLoading] = useState(true);
+    const [stamps, setStamps] = useState(null);
+
+    useEffect(() => {
+        (() => Request({
+            url: `/api/clubs/ClubStampCode/club?id=${profile_id}`
+        },
+            data => {
+                setStamps(data);
+                setLoading(false);
+            },
+            error => {
+                console.log(error.response);
+                setLoading(false);
+            }))();
+    }, []);
+
+    return loading ?
+        <Loading /> :
+        <Card className="responsible-persons">
+            <div className="responsible-persons__head">
+                <button className="btn-backward" onClick={() => history.goBack()}>Личный кабинет</button>
+            </div>
+            <div className="responsible-persons__table">
+                {stamps && !!stamps.length ?
+                    <StampsRegistry stamps={stamps} clubAlias={clubAlias} /> :
+                    <h2>Клейм не найдено</h2>
+                }
+            </div>
+        </Card>
 };
 
-export default Registry;
+const mapStateToProps = state => ({ profile_id: state.authentication.profile_id });
+export default connect(mapStateToProps)(Registry);
