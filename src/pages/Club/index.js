98cbc553181fc047c2fa9452d46d49d244d84eeb
Denis <den.bessarab@outlook.com>
2020-04-27 10:59:40 +0300

    add Disclaimer to specific pages

--- src/pages/Club/index.js
+++ src/pages/Club/index.js
@@ -8,6 +8,7 @@ import Loading from "../../components/Loading";
 import MenuComponent from "../../components/MenuComponent";
 import ClubHeader from "./components/ClubHeader";
 import ExhibitionsComponent from "../../components/ExhibitionsComponent";
+import Disclaimer from "components/Disclaimer";
 import ClubInfo from "./components/ClubInfo";
 import ClubDescription from "./components/ClubDescription";
 import AddArticle from "../../components/AddArticleComponent";
@@ -20,7 +21,7 @@ import { connectAuthVisible } from "../Login/connectors";
 import "./index.scss";
 
 
-const ClubPage = ({match, profile_id, is_active_profile, isAuthenticated}) => {
+const ClubPage = ({ match, profile_id, is_active_profile, isAuthenticated }) => {
     const [clubInfo, setClubInfo] = useState(null);
     const [error, setError] = useState(null);
     const [canEdit, setCanEdit] = useState(false);
@@ -49,53 +50,58 @@ const ClubPage = ({match, profile_id, is_active_profile, isAuthenticated}) => {
         <Loading /> :
         error ?
             <PageNotFound /> :
-                notActiveProfile ?
-                    <NotConfirmed/> :
-                    <Layout>
-                        <Container className="content club-page">
-                            <ClubHeader
-                                clubLogo={clubInfo.logo_link}
-                                clubImg={clubInfo.headliner_link}
-                                clubName={clubInfo.short_name || clubInfo.name || 'Название клуба отсутствует'}
-                                federationName={clubInfo.federation_name}
-                                federationAlias={clubInfo.federation_alias}
-                                canEdit={canEdit}
-                            />
-                            <ExhibitionsComponent alias={clubInfo.club_alias} />
-                            <div className="club-page__content-wrap">
-                                <div className="club-page__content">
-                                    <ClubDescription description={clubInfo.description} />
-                                    {canEdit &&
-                                        <AddArticle
-                                            clubId={clubInfo.id}
-                                            logo={clubInfo.logo_link}
-                                            setPage={setPage}
-                                            setNeedRequest={setNeedRequest}
-                                        />
-                                    }
-                                    <ClubNews
+            notActiveProfile ?
+                <NotConfirmed /> :
+                <Layout>
+                    <Container className="content club-page">
+                        <ClubHeader
+                            clubLogo={clubInfo.logo_link}
+                            clubImg={clubInfo.headliner_link}
+                            clubName={clubInfo.short_name || clubInfo.name || 'Название клуба отсутствует'}
+                            federationName={clubInfo.federation_name}
+                            federationAlias={clubInfo.federation_alias}
+                            canEdit={canEdit}
+                        />
+                        <Disclaimer>
+                            <a className="Disclaimer__support-link" href="http://support.rkf.online/%d0%b8%d0%bd%d1%81%d1%82%d1%80%d1%83%d0%ba%d1%86%d0%b8%d1%8f-%d0%bf%d0%be-%d0%b4%d0%be%d0%b1%d0%b0%d0%b2%d0%bb%d0%b5%d0%bd%d0%b8%d1%8e-%d0%bd%d0%be%d0%b2%d0%be%d1%81%d1%82%d0%b8-%d0%bd%d0%b0-%d1%81-2/" target="_blank" rel="noopener noreferrer">
+                                Инструкция по добавлению новости на странице клуба
+                            </a>
+                        </Disclaimer>
+                        <ExhibitionsComponent alias={clubInfo.club_alias} />
+                        <div className="club-page__content-wrap">
+                            <div className="club-page__content">
+                                <ClubDescription description={clubInfo.description} />
+                                {canEdit &&
+                                    <AddArticle
                                         clubId={clubInfo.id}
-                                        alias={match.params.route}
-                                        page={page}
+                                        logo={clubInfo.logo_link}
                                         setPage={setPage}
-                                        needRequest={needRequest}
                                         setNeedRequest={setNeedRequest}
                                     />
-                                </div>
-                                <Aside className="club-page__info">
-                                    <MenuComponent
-                                        alias={clubInfo.club_alias}
-                                        name={shorten(clubInfo.short_name || clubInfo.name || 'Название клуба отсутствует')}
-                                    />
-                                    <ClubInfo {...clubInfo} />
-                                </Aside>
+                                }
+                                <ClubNews
+                                    clubId={clubInfo.id}
+                                    alias={match.params.route}
+                                    page={page}
+                                    setPage={setPage}
+                                    needRequest={needRequest}
+                                    setNeedRequest={setNeedRequest}
+                                />
                             </div>
-                            <FloatingMenu
-                                alias={clubInfo.club_alias}
-                                name={shorten(clubInfo.short_name || clubInfo.name || 'Название клуба отсутствует')}
-                            />
-                        </Container>
-                    </Layout>
+                            <Aside className="club-page__info">
+                                <MenuComponent
+                                    alias={clubInfo.club_alias}
+                                    name={shorten(clubInfo.short_name || clubInfo.name || 'Название клуба отсутствует')}
+                                />
+                                <ClubInfo {...clubInfo} />
+                            </Aside>
+                        </div>
+                        <FloatingMenu
+                            alias={clubInfo.club_alias}
+                            name={shorten(clubInfo.short_name || clubInfo.name || 'Название клуба отсутствует')}
+                        />
+                    </Container>
+                </Layout>
 };
 
 export default React.memo(connectAuthVisible(ClubPage));
