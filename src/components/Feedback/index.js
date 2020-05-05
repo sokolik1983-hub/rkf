7fd0de59e57b65d70d5f3c72be3f231ab7b5344e
Denis <den.bessarab@outlook.com>
2020-05-05 10:57:58 +0300

    hotfix Feedback text wrapping

--- src/components/Feedback/index.js
+++ src/components/Feedback/index.js
@@ -60,8 +60,8 @@ const Feedback = ({ className, title }) => {
                 <div className="feedback">
                     <p>
                         Уважаемые пользователи! В случае возникновения у Вас вопросов по функционированию Цифровой Платформы российского кинологического сообщества RKF.Online просим Вас ознакомиться с разделом "База знаний" в <a
-                        href="https://help.rkf.online/" target="_blank" rel="noopener noreferrer">Центре поддержки</a>. <br/><br/>
-                        Если ответ на Ваш вопрос отсутствует - обратитесь в центр поддержки по e-mail <a href="mailto:support@rkf.online">support@rkf.online</a>. <br/><br/>
+                            href="https://help.rkf.online/" target="_blank" rel="noopener noreferrer">Центре поддержки</a>. <br /><br />
+                        Если ответ на Ваш вопрос отсутствует - обратитесь в центр поддержки <br />по e-mail <a href="mailto:support@rkf.online">support@rkf.online</a>. <br /><br />
                         Спасибо, что Вы с нами!
                     </p>
                     {/*<h1>Форма обратной связи</h1>*/}
