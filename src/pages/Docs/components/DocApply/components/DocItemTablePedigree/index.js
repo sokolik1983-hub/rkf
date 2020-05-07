69dc9f9d4d79b555703c7ee107be4986b64d8bee
kolya <kc@kparc.io>
2020-05-07 16:23:26 +0300

    [+] delete declarants

--- src/pages/Docs/components/DocApply/components/DocItemTablePedigree/index.js
+++ src/pages/Docs/components/DocApply/components/DocItemTablePedigree/index.js
@@ -15,20 +15,21 @@ import transliterate from "utils/transliterate";
 import HideIf from "components/HideIf";
 import moment from "moment";
 import "../DocItemPedigree/index.scss";
+import "./index.scss";
 
 const docConst = 6;
 
-const DocTableItem = ({date_created, status_name, id, owner_last_name, owner_first_name, owner_second_name, email, documents, activateClick}) => {
-    return <tr className={`DocItem table caps`} onClick={activateClick}>
-        <td>{date_created ? moment(date_created).format("DD.MM.YYYY") : ''}</td>
-        <td className="no-caps"><i>{status_name}</i></td>
-        <td>{id || ''}</td>
-        <td>{[owner_last_name, owner_first_name, owner_second_name].filter(f=>f).join(' ')}</td>
-        <td>{email}</td>
-        <td>{documents ? documents.length + docConst : docConst}</td>
+const DocTableItem = ({date_created, status_name, id, owner_last_name, owner_first_name, owner_second_name, email, documents, activateClick, onDelete}) => {
+    return <tr className={`DocItem table caps`}>
+        <td onClick={activateClick}>{date_created ? moment(date_created).format("DD.MM.YYYY") : ''}</td>
+        <td onClick={activateClick} className="no-caps"><i>{status_name}</i></td>
+        <td onClick={activateClick}>{id || ''}</td>
+        <td onClick={activateClick}>{[owner_last_name, owner_first_name, owner_second_name].filter(f=>f).join(' ')}</td>
+        <td onClick={activateClick}>{email}</td>
+        <td onClick={activateClick}>{documents ? documents.length + docConst : docConst}</td>
         <td>
         {/*<img className={`DocItem__chevron ${active && 'active'}`} src="/static/icons/chevron_left.svg" alt=""/>*/}
-        <DeleteButton />
+        <DeleteButton onClick={e => onDelete(e)} />
         </td>
     </tr>;
 }
