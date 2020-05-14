2dcfdb2fb52f6770111caee4ae8e3c79106d7bb4
kolya <kc@kparc.io>
2020-05-14 18:28:50 +0300

    [+] complete table

--- src/pages/Docs/components/DocApply/components/DocItemTableLitter/index.js
+++ src/pages/Docs/components/DocApply/components/DocItemTableLitter/index.js
@@ -5,14 +5,15 @@ import moment from "moment";
 import "../DocItemLitter/index.scss";
 import "./index.scss";
 
-const DocTableItem = ({date_created, statuses, status_id, id, owner_last_name, owner_first_name, owner_second_name, email, documents, activateClick, onDelete, father_foreign, mother_foreign}) => {
-    const docConst = 2 + Number(father_foreign) + Number(mother_foreign);
+// litter
+const DocTableItem = ({date_created, statuses, status_id, id, last_name, first_name, second_name, email, documents, activateClick, onDelete, father_foreign, mother_foreign}) => {
+    const docConst = 3;
     let st = statuses.find(f => status_id === f.id)
     return <tr className={`DocItem table caps`}>
         <td onClick={activateClick}>{date_created ? moment(date_created).format("DD.MM.YYYY") : ''}</td>
         <td onClick={activateClick} className="no-caps"><i>{st && st.name}</i></td>
         <td onClick={activateClick}>{id || ''}</td>
-        <td onClick={activateClick}>{[owner_last_name, owner_first_name, owner_second_name].filter(f=>f).join(' ')}</td>
+        <td onClick={activateClick}>{[last_name, first_name, second_name].filter(f=>f).join(' ')}</td>
         <td onClick={activateClick}>{email}</td>
         <td onClick={activateClick}>{documents ? documents.length + docConst : docConst}</td>
         <td><img className={`DocItem__chevron`} src="/static/icons/pen-gray.svg" alt=""/></td>
