b7c885bf674e0818ee8c16b3cade9bab3c3ac1f5
Denis <den.bessarab@outlook.com>
2020-06-02 14:04:22 +0300

    split authorization page into Login/Registration

--- src/pages/Registration/index.js
+++ src/pages/Registration/index.js
@@ -2,23 +2,22 @@ import React, { useState } from "react";
 import ClubRegistration from "./components/ClubRegistration";
 import Card from "components/Card";
 import NurseryRegistration from "./components/NurseryRegistration";
+import { compose } from "redux";
+import { Redirect } from "react-router-dom";
+import Layout from "../../components/Layouts";
+import AuthLayout from "../../components/Layouts/AuthLayout";
+import reducer from "../Login/reducer";
+import injectReducer from "../../utils/injectReducer";
+import { connectAuthVisible } from "../Login/connectors";
 import "./index.scss";
 
 
-const RegistrationPage = () => {
+const Registration = () => {
     const [activeTab, setActiveTab] = useState('club');
 
     return (
         <Card>
             <h1 className="registration-page__title">Регистрация</h1>
-            <div className="registration-page__support-links">
-                <p>
-                    <a href="https://help.rkf.online/ru/knowledge_base/article/36/category/3/#/" target="_blank" rel="noopener noreferrer">Инструкция по регистрации клуба на портале RKF.Online</a>
-                </p>
-                <p>
-                    <a href="https://help.rkf.online/ru/knowledge_base/art/52/cat/3/#/" target="_blank" rel="noopener noreferrer">Видео-инструкция по регистрации клуба на портале RKF.Online</a>
-                </p>
-            </div>
             <div className="registration-page__tabs">
                 <div className="registration-page__tabs-controls">
                     <div className={`registration-page__tab${activeTab === 'club' ? ' _active' : ''}`}
@@ -31,6 +30,12 @@ const RegistrationPage = () => {
                     >
                         Питомник
                             </div>
+                    <div className={`registration-page__tab${activeTab === 'individual' ? ' _active' : ''}`}
+                        //onClick={() => setActiveTab('individual')}
+                        onClick={() => null}
+                    >
+                        Физическое лицо
+                </div>
                 </div>
                 {activeTab === 'club' && <ClubRegistration />}
                 {activeTab === 'nursery' && <NurseryRegistration />}
@@ -39,4 +44,17 @@ const RegistrationPage = () => {
     )
 };
 
-export default React.memo(RegistrationPage);
+
+const RegistrationPage = ({ isAuthenticated }) => {
+    return isAuthenticated ?
+        <Redirect to={'/'} /> :
+        <Layout>
+            <AuthLayout className="login-page">
+                <Registration />
+            </AuthLayout>
+        </Layout>
+};
+
+const withReducer = injectReducer({ key: 'authentication', reducer: reducer });
+
+export default compose(withReducer)(connectAuthVisible(React.memo(RegistrationPage)));
