78fd0dadacdd5e16e72359f9112da0bb94e0bd2e
kolya <kc@kparc.io>
2020-06-09 13:18:09 +0300

    [+] disable folder number if it was obtained from the server

--- src/pages/Docs/components/DocApply/forms/LitterHeader/index.js
+++ src/pages/Docs/components/DocApply/forms/LitterHeader/index.js
@@ -19,6 +19,12 @@ const HeaderFormFields = connect(({formik, update, options, clubAlias, setRedire
         .forEach(key => key === "id" || formik.setFieldValue(key, declarant[key]));
     }
     const [init, setInit] = useState(false);
+    const [folder, _setFolder] = useState('');
+    const setFolder = e => {
+        if (!e) return;
+        formik.setFieldValue('folder_number', e);
+        _setFolder(e);
+    }
     useEffect(() => {
         if (!init && !formik.values.id) {
             setInit(true);
@@ -35,7 +41,7 @@ const HeaderFormFields = connect(({formik, update, options, clubAlias, setRedire
             Request({
                 url: '/api/requests/CommonRequest/folder_number'
             },
-            e => {e && formik.setFieldValue('folder_number', e)},
+            setFolder,
             e => {})
         }
     }, []);
@@ -53,7 +59,7 @@ const HeaderFormFields = connect(({formik, update, options, clubAlias, setRedire
             placeholder="Выберите федерацию"
         />
         <FormField
-            disabled={update}
+            disabled={update || folder}
             name="folder_number"
             label="Номер папки"
             placeholder="0000"
