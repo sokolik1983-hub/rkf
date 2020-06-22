8e5315a9a83e5c606343909c3c909a6723328631
Leon <leon1258@yandex.ru>
2020-06-22 17:05:48 +0300

    add evaluate

--- src/components/BookformCard/index.js
+++ src/components/BookformCard/index.js
@@ -7,25 +7,59 @@ import {Request} from "utils/request";
 import "./index.scss";
 
 const feds = {
-    "РФСС": 30629,
-    "РФЛС": 30627,
-    "РФОС": 30628,
-    "ОАНКОО/Фауна": 30630,
-    "ОАНКОО/РКК": 30632,
-    "ОАНКОО/Элита": 30631
-}
+    "РФСС": {
+        id: 30629,
+        federation: 'https://yandex.ru/poll/enter/EyPLyK531CypGWfK4kpumN',
+        support: 'https://yandex.ru/poll/enter/B76mJcZ1LRkNCYeAAojSty'
+    },
+    "РФЛС": {
+        id: 30627,
+        federation: 'https://yandex.ru/poll/enter/9uhq1djVi5RPzdbfzdbgGs',
+        support: 'https://yandex.ru/poll/enter/EYhekUnYmXY864nH47Djb1'
+    },
+    "РФОС": {
+        id: 30628,
+        federation: 'https://yandex.ru/poll/enter/5ds7dfT5GmyrcnaNyUehp7',
+        support: 'https://yandex.ru/poll/enter/B7sraQmmFGtMpy2ZZ5AbNC'
+    },
+    "ОАНКОО/Фауна": {
+        id: 30630,
+        federation: 'https://yandex.ru/poll/enter/AXxQPxJVazKNZktJSkjHFh',
+        support: 'https://yandex.ru/poll/enter/kUKs4Gzija8wsGpoR1QCi'
+    },
+    "ОАНКОО/РКК": {
+        id: 30632,
+        federation: 'https://yandex.ru/poll/enter/5orKZHTRjFTdhkR1gNHnZX',
+        support: 'https://yandex.ru/poll/enter/XpnjF2hiGAYw691DbHNxnV'
+    },
+    "ОАНКОО/Элита": {
+        id: 30631,
+        federation: 'https://yandex.ru/poll/enter/3m4RVPpkBYv1BDZgCeWA5U',
+        support: 'https://yandex.ru/poll/enter/ide3ioSZATAGvrg83cczh'
+    }
+};
 
 
 const BookformCard = ({url}) => {
     const [loading, setLoading] = useState(true);
     const [showModal, setShowModal] = useState(false);
-    const [widgetId, setWidgetId] = useState(false);
+    const [link, setLink] = useState('');
     const [federation, setFederation] = useState('');
-    const handleClick = (e, rkf) => {
+    const [color, setColor] = useState('');
+
+    const handleClick = (e, rkf, destination) => {
         e.preventDefault();
         setShowModal(true);
-        setWidgetId(rkf ? 30637 : federation && feds[federation]);
-    }
+
+        if(!destination) {
+            setColor('');
+            setLink(`https://widget.bookform.ru/${rkf ? 30637 : federation && feds[federation].id}/`);
+        } else {
+            setColor('_blue');
+            setLink(federation && feds[federation][destination]);
+        }
+    };
+
     useEffect(() => {
         (() => Request({url},
         data => {
@@ -38,19 +72,39 @@ const BookformCard = ({url}) => {
         }))();
     }, []);
 
-    return loading ? <Loading/> : <Card>
-            <div className="documents-page__icon" />
-            <h3>ЗАПИСЬ НА ОЧНЫЙ ПРИЕМ</h3>
-            <p>В данном разделе Вы можете записаться на очный прием в офисе Вашей федерации или РКФ. Для этого выберете дату и время посещения, а также тип услуги, которая Вас интересует. После подтверждения записи на Ваш e-mail будет отправлено письмо с датой и временем Вашей записи, которое необходимо будет предъявить на входе. При посещении офиса необходимо иметь с собой документ, удостоверяющий личность</p>
-            <hr />
-            <div className="Card__links">
-                {federation && <Link to={`/`} onClick={e => handleClick(e)}>Запись в {federation}</Link>}
-                <Link to={`/`} onClick={e => handleClick(e, true)}>Запись в РКФ</Link>
-            </div>
-            <Modal showModal={showModal} handleClose={() => setShowModal(false)}>
-                <iframe src={`https://widget.bookform.ru/${widgetId}/`}/>
+    return loading ?
+        <Loading/> :
+        <div className="documents-page__right">
+            <Card>
+                <div className="documents-page__icon" />
+                <h3>ЗАПИСЬ НА ОЧНЫЙ ПРИЕМ</h3>
+                <p>В данном разделе Вы можете записаться на очный прием в офисе Вашей федерации или РКФ. Для этого выберете дату и время посещения, а также тип услуги, которая Вас интересует. После подтверждения записи на Ваш e-mail будет отправлено письмо с датой и временем Вашей записи, которое необходимо будет предъявить на входе. При посещении офиса необходимо иметь с собой документ, удостоверяющий личность</p>
+                <hr />
+                <div className="Card__links">
+                    {federation && <Link to={`/`} onClick={e => handleClick(e)}>Запись в {federation}</Link>}
+                    <Link to={`/`} onClick={e => handleClick(e, true)}>Запись в РКФ</Link>
+                </div>
+            </Card>
+            <Card>
+                <div className="documents-page__icon" />
+                <h3>ОЦЕНКА РАБОТЫ ФЕДЕРАЦИИ</h3>
+                <p>В данном разделе Вы можете поделиться своими впечатлениями от посещения офиса Вашей федерации и от взаимодействия с ее службой поддержки. Опрос займет всего несколько минут, но пройти его можно не чаще одного раза в месяц, поэтому просим Вас отвечать искренне и быть очень внимательными. Помогите нам стать лучше - нам важно Ваше мнение!</p>
+                <hr />
+                <div className="Card__links">
+                    {federation && <Link to={`/`} onClick={e => handleClick(e, null, 'federation')}>Оценить работу {federation}</Link>}
+                    <Link to={`/`} onClick={e => handleClick(e, null, 'support')}>Оценить работу службы поддержки Федерации</Link>
+                </div>
+            </Card>
+            <Modal showModal={showModal}
+                   handleClose={() => {
+                       setLink('');
+                       setShowModal(false);
+                   }}
+                   className={`documents-page__modal${color ? ' ' + color : ''}`}
+            >
+                <iframe src={link}/>
             </Modal>
-        </Card>
+        </div>
 }
 
 export default React.memo(BookformCard);
