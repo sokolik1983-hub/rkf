b9ff2f1d1f2427d45e5ea350394b8931f2bb34e8
kolya <kc@kparc.io>
2020-07-10 12:12:52 +0300

    [+] restyle

--- src/components/Share/index.js
+++ src/components/Share/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import YandexShare from 'react-yandex-share';
+import './index.scss';
 
 const Share = props => <YandexShare
-          theme={{ lang: 'ru', services: 'vkontakte,facebook,odnoklassniki,moimir,twitter,viber,whatsapp,telegram', limit: 3}}
-          content={{ url: 'window.location.href' }}
+          theme={{ lang: 'ru', services: 'vkontakte,facebook,odnoklassniki,moimir,twitter,viber,whatsapp,telegram', limit: 0}}
+          content={{ url: window.location.href }}
         />
 
 export default Share
