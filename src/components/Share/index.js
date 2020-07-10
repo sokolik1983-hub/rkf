18b907c54a84fff40b4ffc07b224104b57cdb2cc
kolya <kc@kparc.io>
2020-07-10 11:51:28 +0300

    [+] link to current page

--- src/components/Share/index.js
+++ src/components/Share/index.js
@@ -3,6 +3,7 @@ import YandexShare from 'react-yandex-share';
 
 const Share = props => <YandexShare
           theme={{ lang: 'ru', services: 'vkontakte,facebook,odnoklassniki,moimir,twitter,viber,whatsapp,telegram', limit: 3}}
+          content={{ url: 'window.location.href' }}
         />
 
 export default Share
