9bd2a8e925f3c974d02e46380da913f0653dd793
kolya <kc@kparc.io>
2020-07-10 13:54:09 +0300

    [+] significantly modify logic and design

--- src/components/Share/index.js
+++ src/components/Share/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import YandexShare from 'react-yandex-share';
 import './index.scss';
 
-const Share = props => <YandexShare
+const Share = ({url}) => <span className="Share"><span className="Share__text"></span><YandexShare
           theme={{ lang: 'ru', services: 'vkontakte,facebook,odnoklassniki,moimir,twitter,viber,whatsapp,telegram', limit: 0}}
-          content={{ url: window.location.href }}
-        />
+          content={{ url: console.log(url)||url || window.location.href }}
+        /></span>
 
 export default Share
