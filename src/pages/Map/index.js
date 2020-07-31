14090c0dba09487a33da6e8d49146e3980d8c9e0
Denis <den.bessarab@outlook.com>
2020-07-31 15:54:21 +0300

    add iFrame mode to MapPage

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -2,11 +2,15 @@ import React from "react";
 import ClubsMap from "../../components/ClubsMap";
 import Layout from "../../components/Layouts";
 
-
-const MapPage = () => (
-    <Layout>
-        <ClubsMap/>
-    </Layout>
-);
+const MapPage = ({ location }) => {
+    const { hash } = location;
+    if (hash === '#iframe') {
+        return <ClubsMap />
+    } else {
+        return <Layout>
+            <ClubsMap />
+        </Layout>
+    };
+};
 
 export default React.memo(MapPage);
