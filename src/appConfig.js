a3e9b3717fee5e2f4548a9688553cc482c252e7c
Leon <leon1258@yandex.ru>
2020-08-07 18:01:33 +0300

    mobile

--- src/appConfig.js
+++ src/appConfig.js
@@ -199,38 +199,39 @@ const mainNav = [
     {
         id: 1,
         title: "Кинологические организации",
-        children: [
-            {
-                id: 1.1,
-                title: "РКФ",
-                to: '/rkf',
-                exact: true
-            },
-            {
-                id: 1.2,
-                title: "Федерации",
-                to: '/organizations/federations',
-                exact: false
-            },
-            {
-                id: 1.3,
-                title: "Клубы",
-                to: '/organizations/clubs',
-                exact: false
-            },
-            {
-                id: 1.4,
-                title: "Питомники",
-                to: '/organizations/kennels',
-                exact: false
-            },
-            {
-                id: 1.5,
-                title: "Национальные Клубы Пород",
-                to: '/organizations/national-breed-clubs',
-                exact: false
-            }
-        ]
+        to: '/organization',
+        // children: [
+        //     {
+        //         id: 1.1,
+        //         title: "РКФ",
+        //         to: '/rkf',
+        //         exact: true
+        //     },
+        //     {
+        //         id: 1.2,
+        //         title: "Федерации",
+        //         to: '/organizations/federations',
+        //         exact: false
+        //     },
+        //     {
+        //         id: 1.3,
+        //         title: "Клубы",
+        //         to: '/organizations/clubs',
+        //         exact: false
+        //     },
+        //     {
+        //         id: 1.4,
+        //         title: "Питомники",
+        //         to: '/organizations/kennels',
+        //         exact: false
+        //     },
+        //     {
+        //         id: 1.5,
+        //         title: "Национальные Клубы Пород",
+        //         to: '/organizations/national-breed-clubs',
+        //         exact: false
+        //     }
+        // ]
     },
     {
         id: 2,
