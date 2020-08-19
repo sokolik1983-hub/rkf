0a5c0be8702133cd5b03942b084c5953662c3985
Maksim Borovik <mouxez@gmail.com>
2020-08-19 16:32:36 +0300

    WIP mobile view of search btn

--- src/components/Layouts/Header/components/Search.js
+++ src/components/Layouts/Header/components/Search.js
@@ -1,25 +1,43 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
+import ReactDOM from "react-dom";
 import history from "../../../../utils/history";
 
 
 const Search = () => {
     const [searchValue, setSearchValue] = useState('');
+    const [isClicked, setIsClicked] = useState(false);
 
     const handleSubmit = e => {
         e.preventDefault();
         history.push(`/search?s=${searchValue}`);
     };
 
+    const setDisplay = (isClicked) => {
+        if (isClicked) {
+            ReactDOM.findDOMNode(".header__search").style.display = 'block';
+        }
+        ReactDOM.findDOMNode(".header__search").style.display = 'none';
+    };
+
+    useEffect(() => {
+        window.addEventListener('click', () => setDisplay(isClicked));
+        return () => window.removeEventListener('click', () => setDisplay(isClicked));
+    }, [isClicked]);
+
     return (
-        <form className="header__search" onSubmit={handleSubmit}>
-            <input
-                className="header__search-control"
-                type="text"
-                placeholder="Поиск"
-                onChange={({target}) => setSearchValue(target.value)}
-                required
-            />
-        </form>
+        <>
+            <form className="header__search" onSubmit={handleSubmit}>
+                <input
+                    className="header__search-control"
+                    type="text"
+                    placeholder="Поиск"
+                    onChange={({target}) => setSearchValue(target.value)}
+                    onClick={() => setIsClicked(true)}
+                    required
+                />
+            </form>
+            <button className="header__search-control--mobile" type="button"></button>
+        </>
     )
 };
 
