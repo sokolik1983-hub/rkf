c2ee2d9134cd6d9ab9b1dbb51d7f87ae4898f2b6
Maksim Borovik <mouxez@gmail.com>
2020-08-27 15:07:11 +0300

    add data fetch

--- src/pages/BaseSearch/components/CheckRegistration/index.js
+++ src/pages/BaseSearch/components/CheckRegistration/index.js
@@ -1,35 +1,43 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import Request, { getHeaders } from "../../../../utils/request";
-// import Loading from "components/Loading";
-// import Alert from "components/Alert";
+import Loading from "../../../../components/Loading";
+import Alert from "../../../../components/Alert";
 import Card from "../../../../components/Card";
 import './index.scss';
 
 
 const CheckRegistration = () => {
-    const [barcode, setBarcode] = useState('');
+    const [stamp_number, setStampNumber] = useState('');
+    const [stamp_code, setStampCode] = useState('');
     const [status, setStatus] = useState([]);
     const [loading, setLoading] = useState(false);
     const [alert, setAlert] = useState(null);
-    const { query } = useParams();
+    const { query, params } = useParams();
 
     useEffect(() => {
-        if (query && query.length === 13) {
-            setBarcode(query);
+        if (query && query.length === 4) {
+            setStampNumber(query);
             requestTracking(query);
         }
     }, [query]);
 
+    useEffect(() => {
+        if (params && params.length === 3) {
+            setStampCode(params);
+            requestTracking(params);
+        }
+    }, [params]);
+
     const handleSubmit = e => {
         e.preventDefault();
-        requestTracking(barcode);
+        requestTracking(stamp_number, stamp_code);
     };
 
-    const requestTracking = barcode => {
+    const requestTracking = (stamp_number, stamp_code) => {
         setLoading(true);
         Request({
-            url: `/api/requests/CommonRequest/request_tracking?barcode=${barcode}`,
+            url: `/api/requests/commonrequest/dog_registration_information?stamp_number=${stamp_number}&stamp_code=${stamp_code}`,
             options: {
                 method: "GET",
                 headers: getHeaders(),
@@ -50,50 +58,64 @@ const CheckRegistration = () => {
             <h3>Регистрационные данные собаки</h3>
             <p>В целях получения информации о факте регистрации помета в РКФ, наличии у собаки родословной или возможности ее получения введите код и номер клейма и нажмите кнопку "Поиск". Вся необходимая информация будет отображена ниже. Просим Вас использовать данную форму перед отправкой заявки на изготовление документов</p>
             <form className="search-form" onSubmit={handleSubmit}>
-            <input
-                className="search-form__input"
-                type="text"
-                pattern="[A-Z]{3}"
-                onChange={({ target }) => setBarcode(target.value)}
-                value={barcode}
-                title="Введите 3-буквенный код клейма"
-                placeholder="код клейма"
-                disabled={loading}
-                required
-            />
-            <input
-                className="search-form__input"
-                type="text"
-                pattern="[0-9]{4}"
-                onChange={({ target }) => setBarcode(target.value)}
-                value={barcode}
-                title="Введите 4-значный номер клейма"
-                placeholder="номер клейма"
-                disabled={loading}
-                required
-            />
-           {!status.length ? <div className="search-form__button">
-                <button
-                    type="submit"
+                <input
+                    className="search-form__input"
+                    type="text"
+                    pattern="[A-Z]{3}"
+                    onChange={({ target }) => setStampCode(target.value)}
+                    value={stamp_code}
+                    title="Введите 3-буквенный код клейма в формате ABC"
+                    placeholder="код клейма"
                     disabled={loading}
-                >
-                    Поиск
-                </button>
-            </div>
-            :
-            <div className="search-form__button--clear">
-                <button
-                    type="button"
+                    required
+                />
+                <input
+                    className="search-form__input"
+                    type="text"
+                    pattern="[0-9]{4}"
+                    onChange={({ target }) => setStampNumber(target.value)}
+                    value={stamp_number}
+                    title="Введите 4-значный номер клейма. Пример: 1234"
+                    placeholder="номер клейма"
                     disabled={loading}
-                    // onClick={setStatus([])}
-                >
-                <span></span>
+                    required
+                />
+                {!status.length ? <div className="search-form__button">
+                    <button
+                        type="submit"
+                        disabled={loading}
+                    >
+                        Поиск
                 </button>
-            </div>}
-        </form>
-            <p>Данный помет зарегистрирован в РКФ, на собаку может быть оформлена родословная</p>
-            <p>На собаку с введенным клеймом зарегистрирована родословная</p>
-            <p>Данный помет не зарегистрирован в РКФ. Для уточнения деталей обратитесь в клуб, выпустивший метрику щенка</p>
+                </div>
+                    :
+                    <div className="search-form__button--clear">
+                        <button
+                            type="button"
+                            disabled={loading}
+                        // onClick={setStatus([])}
+                        >
+                            <span></span>
+                        </button>
+                    </div>}
+            </form>
+            {
+                loading
+                    ? <Loading centered={false} />
+                    : !!status.length && <div className="search-form__result">
+                        <p>Данный помет зарегистрирован в РКФ, на собаку может быть оформлена родословная</p>
+                        <p>На собаку с введенным клеймом зарегистрирована родословная</p>
+                        <p>Данный помет не зарегистрирован в РКФ. Для уточнения деталей обратитесь в клуб, выпустивший метрику щенка</p>
+
+                    </div>
+            }
+            {alert &&
+                <Alert
+                    text="Номер не найден"
+                    autoclose={1.5}
+                    onOk={() => setAlert(false)}
+                />
+            }
         </Card>
     );
 }
--- src/pages/BaseSearch/components/FoundInfo/index.js
+++ src/pages/BaseSearch/components/FoundInfo/index.js
@@ -1,35 +1,43 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import Card from "../../../../components/Card";
 import Request, { getHeaders } from "../../../../utils/request";
-// import Loading from "components/Loading";
-// import Alert from "components/Alert";
+import Loading from "../../../../components/Loading";
+import Alert from "../../../../components/Alert";
+import Card from "../../../../components/Card";
 import './index.scss';
 
 
 const FoundInfo = () => {
-    const [barcode, setBarcode] = useState('');
+    const [stamp_number, setStampNumber] = useState('');
+    const [stamp_code, setStampCode] = useState('');
     const [status, setStatus] = useState([]);
     const [loading, setLoading] = useState(false);
     const [alert, setAlert] = useState(null);
-    const { query } = useParams();
+    const { query, params } = useParams();
 
     useEffect(() => {
-        if (query && query.length === 13) {
-            setBarcode(query);
+        if (query && query.length === 4) {
+            setStampNumber(query);
             requestTracking(query);
         }
     }, [query]);
 
+    useEffect(() => {
+        if (params && params.length === 3) {
+            setStampCode(params);
+            requestTracking(params);
+        }
+    }, [params]);
+
     const handleSubmit = e => {
         e.preventDefault();
-        requestTracking(barcode);
+        requestTracking(stamp_number, stamp_code);
     };
 
-    const requestTracking = barcode => {
+    const requestTracking = (stamp_number, stamp_code) => {
         setLoading(true);
         Request({
-            url: `/api/requests/CommonRequest/request_tracking?barcode=${barcode}`,
+            url: `/api/requests/commonrequest/found_dog_information?stamp_number=${stamp_number}&stamp_code=${stamp_code}`,
             options: {
                 method: "GET",
                 headers: getHeaders(),
@@ -47,52 +55,65 @@ const FoundInfo = () => {
 
     return (
         <Card className="base-search__card">
-        <h3>Информация о найденных собаках</h3>
-        <p>Если Вами была найдена собака, на теле которой проставлено клеймо - введите его код и номер в поля на данной карточке и нажмите кнопку "Поиск". В случае если данные клейма содержатся в Базе РКФ, Вам будет показан клуб, зарегистрировавший собаку, в который Вы можете обратиться для уточнения любой интересующей Вас информации.</p>
-        <form className="search-form" onSubmit={handleSubmit}>
-            <input
-                className="search-form__input"
-                type="text"
-                pattern="[A-Z]{3}"
-                onChange={({ target }) => setBarcode(target.value)}
-                value={barcode}
-                title="Введите 3-буквенный код клейма"
-                placeholder="код клейма"
-                disabled={loading}
-                required
-            />
-            <input
-                className="search-form__input"
-                type="text"
-                pattern="[0-9]{4}"
-                onChange={({ target }) => setBarcode(target.value)}
-                value={barcode}
-                title="Введите 4-значный номер клейма"
-                placeholder="номер клейма"
-                disabled={loading}
-                required
-            />
-           {!status.length ? <div className="search-form__button">
-                <button
-                    type="submit"
+            <h3>Информация о найденных собаках</h3>
+            <p>Если Вами была найдена собака, на теле которой проставлено клеймо - введите его код и номер в поля на данной карточке и нажмите кнопку "Поиск". В случае если данные клейма содержатся в Базе РКФ, Вам будет показан клуб, зарегистрировавший собаку, в который Вы можете обратиться для уточнения любой интересующей Вас информации.</p>
+            <form className="search-form" onSubmit={handleSubmit}>
+                <input
+                    className="search-form__input"
+                    type="text"
+                    pattern="[A-Z]{3}"
+                    onChange={({ target }) => setStampCode(target.value)}
+                    value={stamp_code}
+                    title="Введите 3-буквенный код клейма в формате ABC"
+                    placeholder="код клейма"
                     disabled={loading}
-                >
-                    Поиск
-                </button>
-            </div>
-            :
-            <div className="search-form__button--clear">
-                <button
-                    type="button"
+                    required
+                />
+                <input
+                    className="search-form__input"
+                    type="text"
+                    pattern="[0-9]{4}"
+                    onChange={({ target }) => setStampNumber(target.value)}
+                    value={stamp_number}
+                    title="Введите 4-значный номер клейма. Пример: 1234"
+                    placeholder="номер клейма"
                     disabled={loading}
-                    // onClick={setStatus([])}
-                >
-                <span></span>
+                    required
+                />
+                {!status.length ? <div className="search-form__button">
+                    <button
+                        type="submit"
+                        disabled={loading}
+                    >
+                        Поиск
                 </button>
-            </div>}
-        </form>
-        <p>Данная собака зарегистрирована в РКФ</p>
-    </Card>
+                </div>
+                    :
+                    <div className="search-form__button--clear">
+                        <button
+                            type="button"
+                            disabled={loading}
+                        // onClick={setStatus([])}
+                        >
+                            <span></span>
+                        </button>
+                    </div>}
+            </form>
+            {
+                loading
+                    ? <Loading centered={false} />
+                    : !!status.length && <div className="search-form__result">
+                        <p>Данная собака зарегистрирована в РКФ</p>
+                    </div>
+            }
+            {alert &&
+                <Alert
+                    text="Номер не найден"
+                    autoclose={1.5}
+                    onOk={() => setAlert(false)}
+                />
+            }
+        </Card>
     );
 }
 
