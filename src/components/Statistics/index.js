ead286519c611de9f9f8d91e066a3abfc8c7c807
Maksim Borovik <mouxez@gmail.com>
2020-09-05 22:38:52 +0300

    add data fetching & images

--- src/components/Statistics/index.js
+++ src/components/Statistics/index.js
@@ -1,30 +1,56 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Loading from "../Loading";
 import Card from "../Card";
-import {Request} from "../../utils/request";
+import { Request } from "../../utils/request";
 import declension from "../../utils/declension";
 import "./index.scss";
 
+const federationLogoClassName = (federation) => {
+    let modifier;
+    switch (federation) {
+        case 'РФЛС':
+            modifier = 'rfls'
+            break;
+        case 'РФСС':
+            modifier = 'rfss'
+            break;
+        case 'ОАНКОО':
+            modifier = 'oankoo'
+            break;
+        case 'РФОС':
+            modifier = 'rfos'
+            break;
+        default:
+            modifier = ''
+    }
+    return `statistics__federation-logo statistics__federation-logo--${modifier}`
+};
 
 const Statistics = () => {
-    const [stats, setStats] = useState({});
     const [loading, setLoading] = useState(true);
-    const {clubs_total_count, federation_clubs} = stats;
-    const clubDeclension = count => count + ' ' + declension(count, ['клуб', 'клуба', 'клубов']);
+    const [clubs, setClubs] = useState({});
+    const { clubs_total_count, federation_clubs } = clubs;
+    const clubDeclension = count => count + ' ' + declension(count, ['Клуб', 'Клуба', 'Клубов']);
+
+    const [nurseries, setNurseries] = useState({});
+    const { nurseries_total_count, federation_nurseries } = nurseries;
+    const nameDeclension = count => count + ' ' + declension(count, ['Питомник', 'Питомника', 'Питомников']);
+
+    const PromiseRequest = payload => new Promise((res, rej) => Request(payload, res, rej));
 
     useEffect(() => {
-        (() => Request({
-            url: '/api/Club/federations_clubs_count'
-        }, data => {
-            setStats(data);
+        setLoading(true);
+        Promise.all([
+            PromiseRequest({ url: `/api/Club/federations_clubs_count` }),
+            PromiseRequest({ url: `/api/nurseries/nursery/federations_nurseries_count` })
+        ]).then(result => {
+            setClubs(result[0]);
+            setNurseries(result[1]);
             setLoading(false);
-            window.scrollTo(0,0);
-        }, error => {
+        }).catch(error => {
             console.log(error.response);
-            if(error.response) alert(`Ошибка: ${error.response.status}`);
             setLoading(false);
-            window.scrollTo(0,0);
-        }))();
+        });
     }, []);
 
     return (
@@ -33,14 +59,22 @@ const Statistics = () => {
                 <Loading centered={false} /> :
                 <>
                     <h3 className="statistics__title">
-                        На RKF.Online авторизовано:<br />
-                        {clubDeclension(clubs_total_count)}
+                        На RKF.Online авторизовано<br />
                     </h3>
-                    {federation_clubs.map((federation, i) =>
-                        <p key={i}>
-                            {`${federation.federation_name} - ${clubDeclension(federation.federation_clubs_count)}`}
-                        </p>
-                    )}
+                    <div className="statistics__wrap">
+                        <span className="statistics__text">{clubDeclension(clubs_total_count)}</span>
+                        {federation_clubs.map((federation, i) =>
+                            <p className="statistics__" key={i}>
+                                <span className={federationLogoClassName(federation.federation_name)} />{`${federation.federation_name} ${federation.federation_clubs_count}`}
+                            </p>
+                        )}
+                        <span className="statistics__text">{nameDeclension(nurseries_total_count)}</span>
+                        {federation_nurseries.map((federation, i) =>
+                            <p className="statistics__" key={i}>
+                                <span className={federationLogoClassName(federation.federation_name)} />{`${federation.federation_name} ${federation.federation_nurseries_count}`}
+                            </p>
+                        )}
+                    </div>
                 </>
             }
         </Card>
