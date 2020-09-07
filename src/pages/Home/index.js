84a50da17c61948b68de24bbd64a61b3767b42e1
Maksim Borovik <mouxez@gmail.com>
2020-09-07 15:57:32 +0300

    add new Statistics component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -10,6 +10,7 @@ import HomepageSlider from "./components/HomepageSlider";
 import ExhibitionsComponent from "../../components/ExhibitionsComponent";
 // import HomepageCheckStatus from "./components/HomepageCheckStatus";
 import ClubsMap from "../../components/ClubsMap";
+import Statistics from "../../components/Statistics";
 import { RKFInfo, exhibitions } from "./config";
 import StickyBox from "react-sticky-box";
 import "./index.scss";
@@ -60,6 +61,7 @@ const HomePage = ({ homepage, cities }) => {
                                             ))}
                                         </div>
                                     </Card>
+                                    <Statistics />
                                     <Card className="home-page__map-wrap">
                                         <h3><Link className="Homepage__map-title" to="/clubs-map">Карта авторизованных клубов</Link></h3>
                                         <div className="home-page__map">
