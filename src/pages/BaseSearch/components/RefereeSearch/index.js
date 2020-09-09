ed1b27b4147db3581ee67287a369dd5e7adf9097
Maksim Borovik <mouxez@gmail.com>
2020-09-09 15:50:39 +0300

    remove wrong import

--- src/pages/BaseSearch/components/RefereeSearch/index.js
+++ src/pages/BaseSearch/components/RefereeSearch/index.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import Request, { getHeaders } from "../../../../utils/request";
+import { Request } from "../../../../utils/request";
 import Loading from "../../../../components/Loading";
 import Alert from "../../../../components/Alert";
 import Card from "../../../../components/Card";
-import {DEFAULT_IMG} from "../../../../appConfig";
+import { DEFAULT_IMG } from "../../../../appConfig";
 import '../FoundInfo/index.scss';
 
 
@@ -85,7 +85,7 @@ const RefereeSearch = () => {
                         disabled={true}
                     />
                     {stamp_code &&
-                    <button type="button" className={`search-form__cancel ${status ? `_hide` : ``}`} onClick={handleStampCodeClear}/>}
+                        <button type="button" className={`search-form__cancel ${status ? `_hide` : ``}`} onClick={handleStampCodeClear} />}
                 </div>
                 <div className="search-form__wrap">
                     <input
@@ -100,7 +100,7 @@ const RefereeSearch = () => {
                         disabled={true}
                     />
                     {stamp_number &&
-                    <button type="button" className={`search-form__cancel ${status ? `_hide` : ``}`} onClick={handleStampNumberClear} />}
+                        <button type="button" className={`search-form__cancel ${status ? `_hide` : ``}`} onClick={handleStampNumberClear} />}
                 </div>
                 <div className="search-form__wrap">
                     <input
@@ -115,17 +115,17 @@ const RefereeSearch = () => {
                         disabled={true}
                     />
                     {stamp_code &&
-                    <button type="button" className={`search-form__cancel ${status ? `_hide` : ``}`} onClick={handleStampCodeClear}/>}
+                        <button type="button" className={`search-form__cancel ${status ? `_hide` : ``}`} onClick={handleStampCodeClear} />}
                 </div>
                 {status ? <div className="search-form__button--clear">
-                        <button
-                            type="button"
-                            disabled={loading}
-                            onClick={handleReset}
-                        >
-                            <span></span>
-                        </button>
-                    </div>
+                    <button
+                        type="button"
+                        disabled={loading}
+                        onClick={handleReset}
+                    >
+                        <span></span>
+                    </button>
+                </div>
                     :
                     <div className="search-form__button">
                         <button
@@ -134,7 +134,7 @@ const RefereeSearch = () => {
                             disabled={true}
                         >
                             <svg width="20" height="20" viewBox="0 0 18 18" fill="#90999e" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M11.7106 11.0006H12.5006L16.7406 15.2606C17.1506 15.6706 17.1506 16.3406 16.7406 16.7506C16.3306 17.1606 15.6606 17.1606 15.2506 16.7506L11.0006 12.5006V11.7106L10.7306 11.4306C9.33063 12.6306 7.42063 13.2506 5.39063 12.9106C2.61063 12.4406 0.390626 10.1206 0.0506256 7.32063C-0.469374 3.09063 3.09063 -0.469374 7.32063 0.0506256C10.1206 0.390626 12.4406 2.61063 12.9106 5.39063C13.2506 7.42063 12.6306 9.33063 11.4306 10.7306L11.7106 11.0006ZM2.00063 6.50063C2.00063 8.99063 4.01063 11.0006 6.50063 11.0006C8.99063 11.0006 11.0006 8.99063 11.0006 6.50063C11.0006 4.01063 8.99063 2.00063 6.50063 2.00063C4.01063 2.00063 2.00063 4.01063 2.00063 6.50063Z" />
+                                <path d="M11.7106 11.0006H12.5006L16.7406 15.2606C17.1506 15.6706 17.1506 16.3406 16.7406 16.7506C16.3306 17.1606 15.6606 17.1606 15.2506 16.7506L11.0006 12.5006V11.7106L10.7306 11.4306C9.33063 12.6306 7.42063 13.2506 5.39063 12.9106C2.61063 12.4406 0.390626 10.1206 0.0506256 7.32063C-0.469374 3.09063 3.09063 -0.469374 7.32063 0.0506256C10.1206 0.390626 12.4406 2.61063 12.9106 5.39063C13.2506 7.42063 12.6306 9.33063 11.4306 10.7306L11.7106 11.0006ZM2.00063 6.50063C2.00063 8.99063 4.01063 11.0006 6.50063 11.0006C8.99063 11.0006 11.0006 8.99063 11.0006 6.50063C11.0006 4.01063 8.99063 2.00063 6.50063 2.00063C4.01063 2.00063 2.00063 4.01063 2.00063 6.50063Z" />
                             </svg>
                         </button>
                     </div>}
@@ -147,9 +147,9 @@ const RefereeSearch = () => {
                         {status.status === 2 ? <></> : ``}
                         {status.status === 3 ? <></> : ``}
                         {status.status === 4 ? <div className="search-form__default-content">
-                        <h3>Ничего не найдено</h3>
-                        <img src={DEFAULT_IMG.noNews} alt="Ничего не найдено" />
-                    </div> : ``}
+                            <h3>Ничего не найдено</h3>
+                            <img src={DEFAULT_IMG.noNews} alt="Ничего не найдено" />
+                        </div> : ``}
                     </div>
             }
             {alert &&
