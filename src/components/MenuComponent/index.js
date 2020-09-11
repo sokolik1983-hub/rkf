98633c3c071875e5eb20eefa47ccd97da1d028d2
Denis <den.bessarab@outlook.com>
2020-09-11 17:00:36 +0300

    fix document-status link in MenuComponent

--- src/components/MenuComponent/index.js
+++ src/components/MenuComponent/index.js
@@ -469,7 +469,7 @@ const MenuComponent = ({ alias, name, user, isFederation, noCard = false }) => {
             </>}
             
                 <li className="menu-component__item menu-component__item--documents">
-                    <Link to={`/${alias}/document-status`} className="menu-component__link" title="Статус документов">Статус документов</Link>
+                    <Link to={user === 'nursery' ? `/kennel/${alias}/document-status` : `/${alias}/document-status`} className="menu-component__link" title="Статус документов">Статус документов</Link>
                 </li>
             <li className="menu-component__item menu-component__item--club">
                 <Link to={user === 'nursery' ? `/kennel/${alias}` : `/${alias}`} className="menu-component__link not-active" title={name}>
