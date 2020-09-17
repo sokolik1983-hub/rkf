fa6da438842291a5a1cb0ad914a3c59b6ac341c9
Leon <leon1258@yandex.ru>
2020-09-17 14:55:16 +0300

    add exhibition table pdf export

--- src/pages/Exhibitions/components/ExhibitionsTable/index.js
+++ src/pages/Exhibitions/components/ExhibitionsTable/index.js
@@ -1,6 +1,7 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {Link} from "react-router-dom";
 import {Grid, GridColumn, GridDetailRow } from "@progress/kendo-react-grid";
+import {GridPDFExport} from "@progress/kendo-react-pdf";
 import {IntlProvider, LocalizationProvider, loadMessages} from "@progress/kendo-react-intl";
 import messages from "./messages.json";
 import "./index.scss";
@@ -19,52 +20,88 @@ class DetailComponent extends GridDetailRow {
                 <p>{dataItem.content}</p>
                 <p><span>Породы:</span> {dataItem.breed_string}</p>
                 <p><span>Судьи:</span> {dataItem.judges}</p>
-                <Link to={dataItem.url}>Подробнее</Link>
+                <Link to={dataItem.url}>Подробнее...</Link>
             </div>
         );
     }
 };
 
 
-const ExhibitionsTable = ({exhibitions, count, startElement, getNextExhibitions, needUpdate}) => {
-    const [expanded, setExpanded] = useState({class: 'expanded'});
+const ExhibitionsTable = ({exhibitions, count, startElement, getNextExhibitions, needUpdate, exporting, setExporting}) => {
     const [skip, setSkip] = useState(startElement);
     const [take, setTake] = useState(10);
+    const [dataItems, setDataItems] = useState([]);
+    const gridPDFExport = useRef(null);
 
     useEffect(() => {
-        if(needUpdate) setSkip(0);
+        if(needUpdate) {
+            setSkip(0);
+            setDataItems([]);
+        }
     }, [needUpdate]);
+
+    useEffect(() => {
+        if(exporting) {
+            //костыль, чтобы свернуть все раскрытые ряды таблицы
+            if(dataItems.length) {
+                setDataItems(dataItems.map(item => {
+                    item.expanded = false;
+                    return item;
+                }));
+            }
+
+            gridPDFExport.current.save(exhibitions, () => setExporting(false));
+        }
+    }, [exporting]);
 
     const onExpand = e => {
         e.dataItem.expanded = !e.dataItem.expanded;
-        setExpanded({class: 'expanded'}); //Костыль для говнокендо, придуманный рукожопыми разработчиками кендо. Что бы их говнокомпонент разворачивался, нужно запустить рендер компонента.
+
+        if(e.value) {
+            setDataItems([...dataItems.filter(item => item !== e.dataItem), e.dataItem]);
+        } else {
+            setDataItems(dataItems.filter(item => item !== e.dataItem));
+        }
     };
 
     const onPageChange = e => {
         setSkip(e.page.skip);
         setTake(e.page.take);
+        setDataItems([]);
         getNextExhibitions(e.page.skip + 1);
     };
+
+    const grid =
+        <Grid
+            data={[...exhibitions]}
+            detail={DetailComponent}
+            expandField="expanded"
+            onExpandChange={onExpand}
+            skip={skip}
+            take={take}
+            total={count}
+            scrollable="none"
+            pageable={true}
+            onPageChange={onPageChange}
+        >
+            <GridColumn field="date" title="Дата" width="170px"/>
+            <GridColumn field="club_string" title="Клуб" />
+            <GridColumn field="rank_string" title="Ранг" width="170px"/>
+        </Grid>;
 
     return (
         <div className="exhibitions-table">
             <LocalizationProvider language="ru">
                 <IntlProvider locale="ru">
-                    <Grid
-                        data={[...exhibitions]}
-                        detail={DetailComponent}
-                        expandField={expanded.class}
-                        onExpandChange={onExpand}
-                        skip={skip}
-                        take={take}
-                        total={count}
-                        pageable={true}
-                        onPageChange={onPageChange}
+                    {grid}
+                    <GridPDFExport
+                        ref={gridPDFExport}
+                        scale={0.6}
+                        margin="1cm"
+                        paperSize="A4"
                     >
-                        <GridColumn field="date" title="Дата" />
-                        <GridColumn field="club_string" title="Клуб" />
-                        <GridColumn field="rank_string" title="Ранг" />
-                    </Grid>
+                        {grid}
+                    </GridPDFExport>
                 </IntlProvider>
             </LocalizationProvider>
         </div>
