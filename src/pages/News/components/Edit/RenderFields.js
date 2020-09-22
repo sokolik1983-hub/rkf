99ad2717bc397ac9533bb99d00962c2f7c021dec
Leon <leon1258@yandex.ru>
2020-09-22 13:49:23 +0300

    part 3

--- src/pages/News/components/Edit/RenderFields.js
+++ src/pages/News/components/Edit/RenderFields.js
@@ -1,19 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import { connect } from 'formik';
-import CustomChipList from "components/Form/Field/CustomChipList";
-import { FormControls, FormField, FormGroup } from 'components/Form';
-import { DEFAULT_IMG, BAD_SITES } from "../../../../appConfig";
-import { Request } from "utils/request";
+import React, {useEffect, useState} from "react";
+import {connect} from "formik";
+import CustomChipList from "../../../../components/Form/Field/CustomChipList";
+import {FormControls, FormField, FormGroup, SubmitButton} from "../../../../components/Form";
+import AddVideoLink from "../../../../components/UserAddArticle/AddVideoLink";
+import {BAD_SITES} from "../../../../appConfig";
+import {Request} from "../../../../utils/request";
+import {getYoutubeVideoId} from "../../../../utils/video";
+import Modal from "../../../../components/Modal";
 
 
-const RenderFields = ({ fields, breeds, formik, text, imgSrc, onCancel }) => {
+const RenderFields = ({ fields, breeds, formik, text, imgSrc, videoLink, onCancel }) => {
     const [src, setSrc] = useState(imgSrc);
+    const [video, setVideo] = useState(videoLink);
     const [advertTypes, setAdvertTypes] = useState([]);
-    const { content, is_advert } = formik.values;
+    const [showModal, setShowModal] = useState(false);
+    const {content, is_advert} = formik.values;
 
     useEffect(() => {
         formik.setFieldValue('content', text);
         formik.setFieldValue('file', imgSrc);
+        formik.setFieldValue('video_link', videoLink);
+
         Request({ url: '/api/article/article_ad_types' },
             data => setAdvertTypes(data.map(d => ({ text: d.name, value: d.id }))),
             error => console.log(error.response)
@@ -54,19 +61,18 @@ const RenderFields = ({ fields, breeds, formik, text, imgSrc, onCancel }) => {
         setSrc('');
     };
 
+    const handleAddVideoLink = link => {
+        formik.setFieldValue('video_link', link);
+        setVideo(link);
+    };
+
+    const handleDeleteVideoLink = () => {
+        formik.setFieldValue('video_link', '');
+        setVideo('');
+    };
+
     return (
         <>
-            <FormGroup inline>
-                <FormField {...fields.is_advert} />
-            </FormGroup>
-            <FormGroup inline className={`article-edit__ad${!is_advert ? ' disabled' : ''}`}>
-                <FormField {...fields.advert_breed_id} options={breeds} disabled={!is_advert} />
-                <FormField {...fields.advert_cost} disabled={!is_advert} />
-                <FormField {...fields.advert_number_of_puppies} disabled={!is_advert} />
-            </FormGroup>
-            <FormGroup inline className={`article-edit__ad${!is_advert ? ' disabled' : ''}`}>
-                <CustomChipList {...fields.advert_type_id} options={advertTypes} disabled={!is_advert} />
-            </FormGroup>
             <div className="article-edit__text">
                 <FormField
                     {...fields.content}
@@ -74,26 +80,83 @@ const RenderFields = ({ fields, breeds, formik, text, imgSrc, onCancel }) => {
                     maxLength="4096"
                     rows="15"
                 />
+                {content &&
+                    <span className="article-edit__content-length">
+                        {`осталось ${4096 - content.length} знаков`}
+                    </span>
+                }
             </div>
-            <span className="article-edit__content-length">{content ? `осталось ${4096 - content.length} знаков` : ''}</span>
-            <div className="article-edit__img">
-                <label className="article-edit__img-label">
-                    <input
-                        type="file"
-                        name="file"
-                        id="file"
-                        className="article-edit__img-input"
-                        accept=".png, .jpg, .jpeg"
-                        onChange={handleChangeImg}
-                    />
-                    <img src={src || DEFAULT_IMG.noImage} alt="" />
-                </label>
-                {src && <button className="article-edit__img-delete" onClick={handleDeleteImg} />}
+            <div className="article-edit__controls">
+                {!src &&
+                    <div className="article-edit__attach-img">
+                        <input
+                            type="file"
+                            name="file"
+                            id="file"
+                            className="article-edit__img-input"
+                            accept=".png, .jpg, .jpeg"
+                            onChange={handleChangeImg}
+                        />
+                        <label htmlFor="file" className="article-edit__attach-img-label">Прикрепить изображение</label>
+                    </div>
+                }
+                {!is_advert && !video &&
+                    <button
+                        className="article-edit__attach-video"
+                        type="button"
+                        onClick={() => setShowModal(true)}>
+                        Добавить ссылку на видео
+                    </button>
+                }
+                {!video && <FormField {...fields.is_advert} className="article-edit__advert-control" />}
             </div>
-            <FormControls>
+            <div className="article-edit__media">
+                {src &&
+                    <div className="article-edit__img">
+                        <img src={src} alt="" />
+                        <button className="article-edit__img-delete" onClick={handleDeleteImg} />
+                    </div>
+                }
+                {video &&
+                    <div className="article-edit__video">
+                        <img src={`https://img.youtube.com/vi/${getYoutubeVideoId(video)}/mqdefault.jpg`} alt="" />
+                        <button className="article-edit__img-delete" onClick={handleDeleteVideoLink} />
+                    </div>
+                }
+            </div>
+            {is_advert &&
+                <div className="article-edit__advert">
+                    <FormGroup inline className="article-edit__ad">
+                        <FormField {...fields.advert_breed_id} options={breeds}/>
+                        <FormField {...fields.advert_cost}/>
+                        <FormField {...fields.advert_number_of_puppies}/>
+                    </FormGroup>
+                    <FormGroup inline className="article-edit__ad">
+                        <CustomChipList {...fields.advert_type_id} options={advertTypes}/>
+                    </FormGroup>
+                </div>
+            }
+            <FormControls className="article-edit__form-controls">
                 <button type="button" className="btn btn-simple" onClick={onCancel}>Отмена</button>
-                <button type="submit" className="btn btn-simple">Обновить</button>
+                <SubmitButton
+                    type="submit"
+                    className={`article-edit__button${formik.isValid ? ' _active' : ''}`}
+                >
+                    Обновить
+                </SubmitButton>
             </FormControls>
+            {showModal &&
+                <Modal
+                    className="article-edit__modal"
+                    showModal={showModal}
+                    handleClose={() => setShowModal(false)}
+                >
+                    <AddVideoLink
+                        setVideoLink={handleAddVideoLink}
+                        showModal={setShowModal}
+                    />
+                </Modal>
+            }
         </>
     )
 };
