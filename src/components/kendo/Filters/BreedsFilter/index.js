c4ae13d8d58ba48af71be007c9a1edded303adf8
Maksim Borovik <mouxez@gmail.com>
2020-09-29 16:42:51 +0300

    modify filters

--- src/components/kendo/Filters/BreedsFilter/index.js
+++ src/components/kendo/Filters/BreedsFilter/index.js
@@ -1,16 +1,41 @@
-import React from 'react';
-import { MultiSelect } from '@progress/kendo-react-dropdowns';
-import './index.scss';
+import React, { useState, useEffect } from "react";
+import { MultiSelect } from "@progress/kendo-react-dropdowns";
+import { listNoDataRender } from "../config.js";
+import "./index.scss";
 
-const BreedsFilterKendo = ({data, onChange, className}) => {
+const BreedsFilterKendo = ({ data, breed_ids, onChange, className }) => {
+    const [values, setValues] = useState([]);
+
+    useEffect(() => {
+        if (data.length) {
+            setValues(data.filter(option => breed_ids.indexOf(option.value) !== -1));
+        }
+    }, [data, breed_ids]);
+
+    const handleChange = options => {
+        onChange(options.map(option => option.value));
+    };
+
+    const handleFilterChange = breedId => {
+        onChange(values.filter(breed => breed.value !== breedId).map(breed => breed.value));
+    };
 
     return (
-        <MultiSelect 
+        <MultiSelect
             data={data}
-            onChange={onChange}
+            value={values}
+            onChange={handleChange}
             textField="label"
             dataItemKey="value"
             className={className}
+            placeholder="все"
+            defaultValue={{
+                value: null,
+                label: "все"
+            }}
+            filterable={true}
+            onFilterChange={handleFilterChange}
+            listNoDataRender={listNoDataRender}
         />
     );
 };
--- src/components/kendo/Filters/CitiesFilter/index.js
+++ src/components/kendo/Filters/CitiesFilter/index.js
@@ -1,16 +1,41 @@
-import React from 'react';
-import { MultiSelect } from '@progress/kendo-react-dropdowns';
-import './index.scss';
+import React, { useState, useEffect } from "react";
+import { MultiSelect } from "@progress/kendo-react-dropdowns";
+import { listNoDataRender } from "../config.js";
+import "./index.scss";
 
-const CitiesFilterKendo = ({data, onChange, className}) => {
+const CitiesFilterKendo = ({data, city_ids, onChange, className}) => {
+    const [values, setValues] = useState([]);
+
+    useEffect(() => {
+        if (data.length) {
+            setValues(data.filter(option => city_ids.indexOf(option.value) !== -1));
+        }
+    }, [data, city_ids]);
+
+    const handleChange = options => {
+        onChange(options.map(option => option.value));
+    };
+
+    const handleFilterChange = cityId => {
+        onChange(values.filter(city => city.value !== cityId).map(city => city.value));
+    };
 
     return (
         <MultiSelect 
             data={data}
-            onChange={onChange}
+            value={values}
+            onChange={handleChange}
             textField="label"
             dataItemKey="value"
             className={className}
+            placeholder="все"
+            defaultValue={{
+                value: null,
+                label: "все"
+            }}
+            filterable={true}
+            onFilterChange={handleFilterChange}
+            listNoDataRender={listNoDataRender}
         />
     );
 };
