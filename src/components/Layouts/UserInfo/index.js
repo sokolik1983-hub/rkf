26e7d3e7365f67acb867274457741f906189ed65
Maksim Borovik <mouxez@gmail.com>
2020-10-12 10:28:34 +0300

    remove middle name & change font color

--- src/components/Layouts/UserInfo/index.js
+++ src/components/Layouts/UserInfo/index.js
@@ -1,30 +1,29 @@
 import React from "react";
 import Share from "../../Share";
-import {DEFAULT_IMG} from "../../../appConfig";
+import { DEFAULT_IMG } from "../../../appConfig";
 import "./index.scss";
 
 
-const UserInfo = ({logo_link, share_link, first_name, second_name, last_name}) => (
+const UserInfo = ({ logo_link, share_link, first_name, last_name }) => (
     <div className="user-info">
         <div className="user-info__logo-wrap">
             <div
                 className="user-info__logo"
                 style={logo_link ?
-                    {backgroundImage: `url(${logo_link})`} :
-                    {backgroundImage: `url(${DEFAULT_IMG.userAvatar})`, width: '100px', minWidth: '100px'}
+                    { backgroundImage: `url(${logo_link})` } :
+                    { backgroundImage: `url(${DEFAULT_IMG.userAvatar})`, width: '100px', minWidth: '100px' }
                 }
             />
         </div>
         <div className="user-info__info">
-            {last_name && <p title={last_name}>{last_name}</p>}
             {share_link ?
                 <div className="user-info__with-share">
                     <p title={first_name || 'Аноним'}>{first_name || 'Аноним'}</p>
-                    <Share url={share_link}/>
+                    <Share url={share_link} />
                 </div> :
                 <p title={first_name || 'Аноним'}>{first_name || 'Аноним'}</p>
             }
-            {second_name && <p title={second_name}>{second_name}</p>}
+            {last_name && <p title={last_name}>{last_name}</p>}
         </div>
     </div>
 );
