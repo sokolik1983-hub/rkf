4cdf92c14eb179f0fb86ff5568113abbf45bb8fd
Maksim Borovik <mouxez@gmail.com>
2020-10-13 16:36:21 +0300

    add calendar for dysplasia

--- src/components/Patella/forms/Dysplasia/index.js
+++ src/components/Patella/forms/Dysplasia/index.js
@@ -1,7 +1,7 @@
-import React, {useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Alert from "components/Alert";
 import DocLink from "../../components/DocLink";
-import {connect, getIn} from "formik";
+import { connect, getIn } from "formik";
 import { FormGroup, FormField } from "components/Form";
 import genericForm from "utils/genericForm";
 import SubmitError from "../../components/SubmitError";
@@ -11,28 +11,31 @@ import Card from "components/Card";
 import { Request } from "utils/request";
 import HideIf from "components/HideIf";
 import FormFile from "../../components/FormFile";
+import UserDatePicker from "../../../kendo/DatePicker";
+import moment from "moment";
+import "./index.scss";
 
 // dysplasia request
-const FormFields = connect(({formik, update, view, options, alias, setRedirect, send, initial, Title, config}) => {
+const FormFields = connect(({ formik, update, view, options, alias, setRedirect, send, initial, Title, config }) => {
     // const headers = { 'Authorization': `Bearer ${localStorage.getItem("apikey")}` };
     // const statusAllowsUpdate = formik.values.status_id ? [2,4,7].includes(formik.values.status_id) : true;
     // const cash_payment = initial.cash_payment;
     const [everkAlert, setEverkAlert] = useState(false);
     const [everk, setEverk] = useState(false);
     const [init, setInit] = useState(false);
-    const PromiseRequest = url => new Promise((res,rej) => Request({url},res,rej));
+    const PromiseRequest = url => new Promise((res, rej) => Request({ url }, res, rej));
     const getEverkData = () => {
         let rfc = getIn(formik.values, 'pedigree_number');
         return PromiseRequest(`/api/dog/Dog/everk_dog/${rfc}`)
-        .then(data => data && (data.name || data.name_lat))
-        .then(name => {
-            if (!name) {
-                throw new Error("name not set");
-            }
-            setEverk && setEverk(true);
-            formik.setFieldValue(`dog_name`, name);
-        })
-        .catch(e => setEverkAlert(true));
+            .then(data => data && (data.name || data.name_lat))
+            .then(name => {
+                if (!name) {
+                    throw new Error("name not set");
+                }
+                setEverk && setEverk(true);
+                formik.setFieldValue(`dog_name`, name);
+            })
+            .catch(e => setEverkAlert(true));
     }
     const deleteClick = () => {
         formik.setFieldValue('pedigree_number', '');
@@ -40,7 +43,7 @@ const FormFields = connect(({formik, update, view, options, alias, setRedirect, 
         setEverk(false);
     }
 
-    const {stampCodes} = options;
+    const { stampCodes } = options;
     useEffect(() => {
         if (!init && !formik.values.id) {
             setInit(true);
@@ -53,79 +56,83 @@ const FormFields = connect(({formik, update, view, options, alias, setRedirect, 
                 formik.setFieldValue('declarant_id', declarant.id);
             }
             Request({
-                url: config.profileType === 'kennel' ? '/api/nurseries/nursery/nursery_federation' :'/api/Club/club_federation'
+                url: config.profileType === 'kennel' ? '/api/nurseries/nursery/nursery_federation' : '/api/Club/club_federation'
             },
-            e => {e && e.id && formik.setFieldValue('federation_id', e.id)},
-            e => {})
+                e => { e && e.id && formik.setFieldValue('federation_id', e.id) },
+                e => { })
         }
     }, []);
 
     // let federation = options.federations.find(f => f.value === formik.values.federation_id);
     // federation = federation ? federation.label : "федерации";
 
+    const handleDateChange = date => {
+        const selectedDate = moment(date.value).format(`YYYY-MM-DD`)
+        formik.setFieldValue('payment_date', selectedDate);
+    };
+
     return <>
-    {everkAlert &&
+        {everkAlert &&
             <Alert
                 title={"Ошибка"}
-                text="Номер родословной не найден в базе ВЕРК." 
+                text="Номер родословной не найден в базе ВЕРК."
                 autoclose={false}
                 okButton={true}
                 onOk={() => setEverkAlert(false)}
             />
         }
 
-    <Card>
-        <Title/>
-        {formik.values.rejected_comment && <div className="alert alert-danger">{formik.values.rejected_comment}</div>}
-        <div className="flex-row heading-row">
-            <h4 className="caps">Добавление заявки</h4>
-        </div>
-        <FormGroup inline>
-        <FormField
-            disabled={update}
-            options={options.declarants.map(m => ({value: m.id, label:m.full_name}))}
-            fieldType="reactSelect"
-            name="declarant_id"
-            label={`Ответственное лицо (<a href="${config.responsibleLink(alias)}">Создать ответственное лицо</a>)`}
-            placeholder="Выберите..." 
-        />
-        </FormGroup>
+        <Card>
+            <Title />
+            {formik.values.rejected_comment && <div className="alert alert-danger">{formik.values.rejected_comment}</div>}
+            <div className="flex-row heading-row">
+                <h4 className="caps">Добавление заявки</h4>
+            </div>
+            <FormGroup inline>
+                <FormField
+                    disabled={update}
+                    options={options.declarants.map(m => ({ value: m.id, label: m.full_name }))}
+                    fieldType="reactSelect"
+                    name="declarant_id"
+                    label={`Ответственное лицо (<a href="${config.responsibleLink(alias)}">Создать ответственное лицо</a>)`}
+                    placeholder="Выберите..."
+                />
+            </FormGroup>
 
-        <FormGroup inline>
-        <FormFile
-            name={`veterinary_contract_document`}
-            label='Заполненный договор-заявка с печатью ветеринарного учреждения и подписью ветеринарного врача (PDF, JPEG, JPG, PNG)'
-            docId={formik.values.veterinary_contract_document_id}
-            disabled={view}
-            document_type_id={31}
-            profileType={config.profileType}
-        />
-        {(config.distinction !== 'patella') && <FormFile
-            name={`roentgenogram_document`}
-            label='Рентгенограмма (PDF, JPEG, JPG, PNG)'
-            docId={formik.values.roentgenogram_document_id}
-            disabled={view}
-            document_type_id={32}
-            profileType={config.profileType}
-        />}
-        </FormGroup>
+            <FormGroup inline>
+                <FormFile
+                    name={`veterinary_contract_document`}
+                    label='Заполненный договор-заявка с печатью ветеринарного учреждения и подписью ветеринарного врача (PDF, JPEG, JPG, PNG)'
+                    docId={formik.values.veterinary_contract_document_id}
+                    disabled={view}
+                    document_type_id={31}
+                    profileType={config.profileType}
+                />
+                {(config.distinction !== 'patella') && <FormFile
+                    name={`roentgenogram_document`}
+                    label='Рентгенограмма (PDF, JPEG, JPG, PNG)'
+                    docId={formik.values.roentgenogram_document_id}
+                    disabled={view}
+                    document_type_id={32}
+                    profileType={config.profileType}
+                />}
+            </FormGroup>
 
-        <FormGroup inline>
-            <FormField disabled={view || everk} name='pedigree_number' label='№ родословной собаки' />
-            {!everk && !view && <Button className='btn btn-primary' style={{marginRight:'1em'}} onClick={e => getEverkData()}>Поиск</Button>}
-            <FormField disabled={view || everk} name='dog_name' label='Кличка собаки' />
-            {everk && !view && <Button className='btn btn-red' onClick={e => deleteClick()}>Удалить</Button>}
-        </FormGroup>
-        
+            <FormGroup inline>
+                <FormField disabled={view || everk} name='pedigree_number' label='№ родословной собаки' />
+                {!everk && !view && <Button className='btn btn-primary' style={{ marginRight: '1em' }} onClick={e => getEverkData()}>Поиск</Button>}
+                <FormField disabled={view || everk} name='dog_name' label='Кличка собаки' />
+                {everk && !view && <Button className='btn btn-red' onClick={e => deleteClick()}>Удалить</Button>}
+            </FormGroup>
 
-        <FormGroup>
-                <br/>
+            <FormGroup>
+                <br />
                 <p className={update ? 'hidden' : ''}>Приложите квитанцию об оплате заявки и заполните информацию о платеже.</p>
                 {/*<FormField disabled={view || formik.values.cash_payment_accept || !statusAllowsUpdate} fieldType="customCheckbox" name='cash_payment' label='Оплата наличными'/>*/}
                 <HideIf cond={formik.values.cash_payment}>
-                <div className="flex-row heading-row">
-                    <h4 className="caps">Информация о платеже</h4>
-                </div>
+                    <div className="flex-row heading-row">
+                        <h4 className="caps">Информация о платеже</h4>
+                    </div>
                     <FormGroup inline>
                         <FormFile
                             name='payment_document'
@@ -135,8 +142,18 @@ const FormFields = connect(({formik, update, view, options, alias, setRedirect, 
                             document_type_id={5}
                             profileType={config.profileType}
                         />
-
-                        <FormField className="special" required={false} disabled={view || formik.values.payment_accept} name='payment_date' label='Дата оплаты' readOnly={true} fieldType="formikDatePicker" />
+                        <div className="DocItem__dysplasia-payment-wrap">
+                            <div>Дата оплаты</div>
+                            <UserDatePicker
+                                onChange={handleDateChange}
+                                value={getIn(formik.values, 'payment_date') ?
+                                    new Date(getIn(formik.values, 'payment_date')) :
+                                    null
+                                }
+                                className="DocItem__dysplasia-payment"
+                                disabled={view || formik.values.payment_accept}
+                            />
+                        </div>
                         <FormField disabled={view || formik.values.payment_accept} name='payment_number' label='Номер платежного документа' />
                     </FormGroup>
                     <FormGroup inline>
@@ -148,25 +165,25 @@ const FormFields = connect(({formik, update, view, options, alias, setRedirect, 
             </FormGroup>
             {formik.values && formik.values.certificate_document_id && <>
                 <h4>Сертификат</h4>
-                <DocLink 
+                <DocLink
                     profileType={config.profileType}
                     docId={formik.values.certificate_document_id}
                     label={''}
                     showLabel={false}
                 />
             </>}
-    </Card>
-    {!view && <div className="stage-controls flex-row">
+        </Card>
+        {!view && <div className="stage-controls flex-row">
             <SubmitError />
             <Button className="btn-green btn-condensed" onClick={e => send({
                 method: formik.values.id ? "PUT" : "POST",
                 action: config.url,
                 button: 'next'
             }, formik)}>Отправить</Button>
-    </div>}
+        </div>}
     </>
 })
 
-const DysplasiaForm = (distinction, profileType) => React.memo(genericForm(FormFields, config(distinction,profileType)))
+const DysplasiaForm = (distinction, profileType) => React.memo(genericForm(FormFields, config(distinction, profileType)));
 
-export default DysplasiaForm
+export default DysplasiaForm;
