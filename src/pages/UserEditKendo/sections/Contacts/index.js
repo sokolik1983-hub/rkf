5f9d32c40caec5419d0a1f8a853d743320f88671
Denis <den.bessarab@outlook.com>
2020-10-22 16:12:57 +0300

    add phone/email to Contacts section

--- src/pages/UserEditKendo/sections/Contacts/index.js
+++ src/pages/UserEditKendo/sections/Contacts/index.js
@@ -1,12 +1,143 @@
 import React from "react";
 import { Prompt } from "react-router-dom";
-import { Form, Field, FormElement } from '@progress/kendo-react-form';
+import { Form, Field, FieldArray, FormElement } from '@progress/kendo-react-form';
 import FormDropDownList from 'pages/UserEditKendo/components/FormDropDownList';
 import FormInput from 'pages/UserEditKendo/components/FormInput';
 import FormComboBox from 'pages/UserEditKendo/components/FormComboBox';
 import { numbersOnlyValidator } from 'pages/UserEditKendo/validators';
-
 import './styles.scss';
+
+import { Error } from '@progress/kendo-react-labels';
+import { Input } from '@progress/kendo-react-inputs';
+import { Checkbox } from '@progress/kendo-react-inputs';
+import { Grid, GridColumn, GridToolbar } from '@progress/kendo-react-grid';
+
+import { FieldWrapper } from '@progress/kendo-react-form';
+import { Label } from '@progress/kendo-react-labels';
+
+
+
+const commandCell = (onRemove) => (props) => {
+    const onClick = React.useCallback(
+        (e) => {
+            e.preventDefault();
+            onRemove(props);
+        },
+        [onRemove]
+    );
+    return (
+        <td>
+            <button
+                className="k-button k-grid-remove-command"
+                onClick={onClick}
+            >
+                Remove
+            </button>
+        </td>
+    );
+};
+
+const FormGrid = (fieldArrayRenderProps) => {
+    const { validationMessage, visited } = fieldArrayRenderProps;
+    const onAdd = React.useCallback(
+        (e) => {
+            e.preventDefault();
+            fieldArrayRenderProps.onUnshift({ value: { name: '' } });
+        },
+        [fieldArrayRenderProps.onUnshift]
+    );
+    const onRemove = React.useCallback(
+        (cellProps) => fieldArrayRenderProps.onRemove({ index: cellProps.dataIndex }),
+        [fieldArrayRenderProps.onRemove]
+    );
+
+    const handleChange = React.useCallback(
+        (cellProps) => {
+            console.log(cellProps);
+            return fieldArrayRenderProps.onChange({ name: cellProps.name, value: cellProps.value })
+        },
+        [fieldArrayRenderProps.onRemove]
+    );
+
+    const nameCell = (props) => {
+        return (
+            <td>
+                <Field
+                    component={Input}
+                    name={`users[${props.dataIndex}].${props.field}`}
+                />
+            </td>
+        );
+    };
+
+
+    const FormCheckbox = (fieldRenderProps) => {
+        const { validationMessage, touched, id, valid, disabled, hint, optional, label, visited, modified, ...others } = fieldRenderProps;
+
+        const showValidationMessage = touched && validationMessage;
+        const errorId = showValidationMessage ? `${id}_error` : '';
+
+        return (
+            <FieldWrapper>
+                <Checkbox
+                    ariaDescribedBy={`${errorId}`}
+                    label={label}
+                    labelOptional={optional}
+                    valid={valid}
+                    id={id}
+                    disabled={disabled}
+                    {...others}
+                />
+                {
+                    showValidationMessage &&
+                    <Error id={errorId}>{validationMessage}</Error>
+                }
+            </FieldWrapper>
+        );
+    };
+
+
+
+    return (<>
+        {
+            visited && validationMessage &&
+            (<Error>{validationMessage}</Error>)
+        }
+        {
+            fieldArrayRenderProps.value && fieldArrayRenderProps.value.map((v, i) => {
+                return <div className="form-row" key={i}>
+                    <div className="form-group col-md-1 Contacts__custom-plus">
+                        {i === fieldArrayRenderProps.value.length - 1 && <button className="k-button k-grid-remove-command" onClick={onAdd}>
+                            <span className="k-icon k-i-plus"></span>
+                        </button>}
+                    </div>
+                    <div className="form-group col-md-4">
+                        <Field name={`contacts[${i}].value`} component={FormInput} validator={numbersOnlyValidator} />
+                    </div>
+                    <div className="form-group col-md-4">
+                        <Field name={`contacts[${i}].description`} component={FormInput} />
+                    </div>
+                    <div className="form-group col-md-2">
+                        {i === 0 && <div className="Contacts__custom-checkbox-label">Основной</div>}
+                        <div className="Contacts__custom-checkbox">
+                            <Field
+                                id={`contacts[${i}]`}
+                                name={`contacts[${i}].is_main`}
+                                component={FormCheckbox}
+                            />
+                        </div>
+                    </div>
+                    <div className="form-group col-md-1 Contacts__custom-trash">
+                        <span onClick={onRemove} className="k-icon k-i-trash" />
+                    </div>
+                </div>
+            })
+        }
+    </>
+    );
+};
+
+
 
 const Contacts = ({ initialValues, cities, setFormTouched }) => {
     const handleSubmit = (dataItem) => console.log((JSON.stringify(dataItem, null, 2)));
@@ -95,6 +226,37 @@ const Contacts = ({ initialValues, cities, setFormTouched }) => {
                                     </div>
                                 </div>
                             </div>
+                            <FieldArray
+                                name="contacts"
+                                component={FormGrid}
+                                onChange={formRenderProps.onChange}
+                            //validator={arrayLengthValidator}
+                            />
+
+                            <div className="form-row mt-3">
+                                <div className="form-group col-md-6">
+                                    <div className="row">
+                                        <div className="col-md-4">
+                                            <div className="Contacts__custom-label">E-mail</div>
+                                        </div>
+                                        <div className="col-md-8">
+                                            <Field
+                                                id={'is_hidden'}
+                                                name={'personal_information.is_hidden'}
+                                                label={''}
+                                                component={FormDropDownList}
+                                                data={[{ text: 'Показать всем', value: false, id: 1 }, { text: 'Скрыть от всех', value: true, id: 2 },]}
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <FieldArray
+                                name="contacts"
+                                component={FormGrid}
+                                onChange={formRenderProps.onChange}
+                            //validator={arrayLengthValidator}
+                            />
 
                         </fieldset>
                         <div className="k-form-buttons text-center">
