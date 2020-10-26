667588309f00ee8c0eaedbb566a9a26ee41db7d4
Maksim Borovik <mouxez@gmail.com>
2020-10-26 14:20:04 +0300

    add redirect to personal page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -6,14 +6,23 @@ import AuthLayout from "../../components/Layouts/AuthLayout";
 import LoginForm from "./components/LoginForm";
 import reducer from "./reducer";
 import injectReducer from "../../utils/injectReducer";
+import ls from "local-storage";
 import { connectAuthVisible } from "./connectors";
 
 const LoginPage = ({ isAuthenticated, is_active_profile, user_type }) => {
+    const alias = ls.get('user_info') ? ls.get('user_info').alias : '';
 
     if (isAuthenticated) {
         if (!is_active_profile && user_type === 3) return <Redirect to={'/not-confirmed'} />
         if (!is_active_profile && user_type === 4) return <Redirect to={'/kennel/activation'} />
-        return <Redirect to={'/'} />
+
+        if (is_active_profile && user_type === 3) {
+            return <Redirect to={`/${alias}`} />
+        } else if (is_active_profile && user_type === 4) {
+            return <Redirect to={`/kennel/${alias}`} />
+        } else if (user_type === 1) {
+            return <Redirect to={`/user/${alias}`} />
+        }
     }
 
     return (
