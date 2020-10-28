20fa28886c104bda96c10f953e41b4e2267f5114
Leon <leon1258@yandex.ru>
2020-10-28 14:49:50 +0300

    refactor user description

--- src/components/Layouts/UserDescription/index.js
+++ src/components/Layouts/UserDescription/index.js
@@ -1,106 +1,134 @@
-import React, {useState} from "react";
+import React, {Fragment, useState} from "react";
 import {Collapse} from "react-collapse";
 import Card from "../../Card";
 import "./index.scss";
 
 
-const UserDescription = ({city_name, address, birthday_date, emails, phones, site, socials, description}) => {
+const UserDescription = ({mainInfo, additionalInfo}) => {
     const [isOpen, setIsOpen] = useState(false);
 
     const normalizeLink = link => {
         if(!link.includes('https://') || !link.includes('http://')) {
             return 'https://' + link;
         }
-
         return link;
     };
+
+    const getAddressString = addressObj => {
+        let address = '';
+
+        if(addressObj) {
+            if (addressObj.postcode) address += `${addressObj.postcode}, `;
+            if (addressObj.city_name) address += `${addressObj.city_name}, `;
+            if (addressObj.street_name) address += `${addressObj.street_name}, `;
+            if (addressObj.house_name) address += `д. ${addressObj.house_name}, `;
+            if (addressObj.building_name) address += `стр. ${addressObj.building_name}, `;
+            if (addressObj.flat_name) address += `кв. ${addressObj.flat_name}`;
+        }
+
+        return address;
+    };
+
+    const {
+        main_phone_value,
+        main_phone_description,
+        main_phone_status,
+        main_mail_value,
+        main_mail_description,
+        main_mail_status,
+        birth_date,
+        birth_date_status
+    } = mainInfo;
+
+    const {
+        city_name,
+        address,
+        emails,
+        phones,
+        social_networks,
+        web_site,
+        description
+    } = additionalInfo || {};
 
     return (
         <Card className="user-description">
             <div className="user-description__head">
                 <h4 className="user-description__title">О себе</h4>
-                {city_name && <span className="user-description__city">{city_name}</span>}
+                {city_name &&
+                    <span className="user-description__city">{city_name}</span>
+                }
             </div>
-            {!birthday_date &&
-            (!emails || !emails.length) &&
-            (!phones || !phones.length) &&
-            !site && !address &&
-            (!socials || !socials.length) &&
-            !description &&
-                <p className="user-description__disabled">Пользователь еще не опубликовал данные о себе</p>
-            }
-            {birthday_date &&
-                <p className="user-description__item _birthday">
-                    <span className="user-description__item-title">Дата рождения:</span>&nbsp;
-                    <span>{birthday_date}</span>
-                </p>
-            }
-            {emails && !!emails.length &&
-                <p className="user-description__item _email">
-                    <span className="user-description__item-title">E-mail:</span>&nbsp;
-                    <span>
-                        {emails.map(item => (
-                            <a href={`mailto:${item.value}`}
-                               key={item.id}
-                               title={item.value}
-                            >
-                                {item.value}
-                            </a>
-                        ))}
-                    </span>
-                </p>
-            }
-            {phones && !!phones.length &&
-                <p className="user-description__item _phone">
-                    <span className="user-description__item-title">Телефон:</span>&nbsp;
-                    <span>
-                        {phones.map(item => (
-                            <span
-                               key={item.id}
-                               title={item.value}
-                            >
-                                {item.value}
-                            </span>
-                        ))}
-                    </span>
-                </p>
-            }
-            {address &&
-                <p className="user-description__item _address">
-                    <span className="user-description__item-title">Адрес:</span>&nbsp;
-                    <span>{address}</span>
-                </p>
-            }
-            {site &&
-                <p className="user-description__item _site">
-                    <span className="user-description__item-title">Сайт:</span>&nbsp;
-                    <span><a href={site} title={site} target="_blank" rel="noopener noreferrer">{site}</a></span>
-                </p>
-            }
-            {socials && !!socials.length &&
-                <p className="user-description__item _socials">
-                    <span className="user-description__item-title">Соцсети:</span>
-                    <span>
-                        {socials.map(item => (
-                            <a href={normalizeLink(item.site)}
-                               key={item.id}
-                               title={item.description || item.site}
-                               target="_blank"
-                               rel="noopener noreferrer"
-                            >
-                                {item.description || item.site}
-                            </a>
-                        ))}
-                    </span>
-                </p>
-            }
-            {description &&
+            <p className="user-description__item _birthday">
+                <span className="user-description__item-title">Дата рождения:</span>&nbsp;
+                <span>{birth_date || birth_date_status}</span>
+            </p>
+            <p className="user-description__item _email">
+                <span className="user-description__item-title">{main_mail_description || 'E-mail'}:</span>&nbsp;
+                {main_mail_value ?
+                    <a href={`mailto:${main_mail_value}`} title={main_mail_value}>{main_mail_value}</a> :
+                    <span>{main_mail_status}</span>
+                }
+            </p>
+            <p className="user-description__item _phone">
+                <span className="user-description__item-title">{main_phone_description || 'Телефон'}:</span>&nbsp;
+                <span>{main_phone_value || main_phone_status}</span>
+            </p>
+            {additionalInfo &&
                 <>
                     <Collapse isOpened={isOpen}>
-                        <div className="user-description__item _describe">
-                            <span className="user-description__item-title">Описание:</span>
-                            <div dangerouslySetInnerHTML={{__html: description}} />
-                        </div>
+                        {emails && !!emails.length &&
+                            emails.map(item =>
+                                <p className="user-description__item _email" key={item.id}>
+                                    <span className="user-description__item-title">{item.description || 'E-mail'}:</span>&nbsp;
+                                    <a href={`mailto:${item.value}`} title={item.value}>
+                                        {item.value}
+                                    </a>
+                                </p>
+                            )
+                        }
+                        {phones && !!phones.length &&
+                            phones.map(item =>
+                                <p className="user-description__item _phone" key={item.id}>
+                                    <span className="user-description__item-title">{item.description || 'Телефон'}:</span>&nbsp;
+                                    <span title={item.value}>{item.value}</span>
+                                </p>
+                            )
+                        }
+                        {getAddressString(address) &&
+                            <p className="user-description__item _address">
+                                <span className="user-description__item-title">Адрес:</span>&nbsp;
+                                <span>{getAddressString(address)}</span>
+                            </p>
+                        }
+                        {web_site &&
+                            <p className="user-description__item _site">
+                                <span className="user-description__item-title">Сайт:</span>&nbsp;
+                                <a href={web_site} title={web_site} target="_blank" rel="noopener noreferrer">{web_site}</a>
+                            </p>
+                        }
+                        {social_networks && !!social_networks.length &&
+                            <p className="user-description__item _socials">
+                                <span className="user-description__item-title">Соцсети:</span>
+                                {social_networks.map(item => (
+                                    <Fragment key={item.id}>
+                                        <br/>
+                                        <a href={normalizeLink(item.site)}
+                                           title={item.description || item.site}
+                                           target="_blank"
+                                           rel="noopener noreferrer"
+                                        >
+                                            {item.description || item.site}
+                                        </a>
+                                    </Fragment>
+                                ))}
+                            </p>
+                        }
+                        {description &&
+                            <div className="user-description__item _describe">
+                                <span className="user-description__item-title">Описание:</span>
+                                <div dangerouslySetInnerHTML={{__html: description}}/>
+                            </div>
+                        }
                     </Collapse>
                     <button
                         className={`user-description__btn${isOpen ? ' _open' : ''}`}
