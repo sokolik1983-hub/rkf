4f8a9c097fd24be5023b9fe189ff0c29628827df
Leon <leon1258@yandex.ru>
2020-10-28 20:39:18 +0300

    part 2

--- src/components/Layouts/UserInfo/ModalEditAvatar/index.js
+++ src/components/Layouts/UserInfo/ModalEditAvatar/index.js
@@ -1,112 +1,23 @@
-import React, {useState} from "react";
-import { Upload } from "@progress/kendo-react-upload";
-import { IntlProvider, LocalizationProvider, loadMessages } from "@progress/kendo-react-intl";
+import React from "react";
+import {Upload} from "@progress/kendo-react-upload";
+import {IntlProvider, LocalizationProvider, loadMessages} from "@progress/kendo-react-intl";
 import {Button} from "@progress/kendo-react-buttons";
-import Loading from "../../../../components/Loading";
 import Modal from "../../../../components/Modal";
-import Alert from "../../../../components/Alert";
-// import {Request} from "../../../../utils/request";
+import {getHeaders} from "../../../../utils/request";
 import ruMessages from "./ru.json";
 import "./index.scss";
+
 
 loadMessages(ruMessages, 'ru-RU');
 
 
 const ModalEditAvatar = ({closeModal}) => {
-    // const [loading, setLoading] = useState(false);
-    // const [alert, setAlert] = useState(null);
-
-    // const handleError = e => {
-    //     if (e.response) {
-    //         const errorText = e.response.data.errors ?
-    //             Object.values(e.response.data.errors) :
-    //             `${e.response.status} ${e.response.statusText}`;
-    //
-    //         setAlert({
-    //             title: `Ошибка!`,
-    //             text: errorText,
-    //             autoclose: 7.5,
-    //             onOk: () => setAlert(null)
-    //         });
-    //     }
-    // };
-    //
-    // const onSubmit = async file => {
-    //     setLoading(true);
-    //
-    //     console.log(file);
-    //
-    //     // await Request({
-    //     //     url: '/api/registration/cancel_deletion',
-    //     //     method: 'POST'
-    //     // }, () => {
-    //     //     setIsAnswer(false);
-    //     //     setDeleteConfirm(false);
-    //     //     setLoading(false);
-    //     // }, error => {
-    //     //     setLoading(false);
-    //     //     handleError(error);
-    //     // });
-    // };
-
-    const onAdd = event => {
-        const afterStateChange = () => {
-            event.affectedFiles
-                .filter(file => !file.validationErrors)
-                .forEach(file => {
-                    const reader = new FileReader();
-
-                    reader.onloadend = (ev) => {
-                        this.setState({
-                            filePreviews: {
-                                ...this.state.filePreviews,
-                                [file.uid]: ev.target.result
-                            }
-                        });
-                    };
-
-                    reader.readAsDataURL(file.getRawFile());
-                });
-        };
-
-        this.setState({
-            files: event.newState,
-            events: [
-                ...this.state.events,
-                `File selected: ${event.affectedFiles[0].name}`
-            ]
-        }, afterStateChange);
-    };
-
-    const onRemove = (event) => {
-        const filePreviews = {
-            ...this.state.filePreviews
-        };
-
-        event.affectedFiles.forEach(file => {
-            delete filePreviews[file.uid];
-        });
-
-        this.setState({
-            files: event.newState,
-            events: [
-                ...this.state.events,
-                `File removed: ${event.affectedFiles[0].name}`
-            ],
-            filePreviews: filePreviews
-        });
-    };
-
     const onBeforeUpload = event => {
-        console.log('headers', event.headers);
-        console.log('additionalData', event.additionalData);
-        // event.additionalData.description = 'File upload';
+        event.headers = getHeaders(true);
     };
 
     const onBeforeRemove = event => {
-        console.log('headers', event.headers);
-        console.log('additionalData', event.additionalData);
-        // event.additionalData.description = 'File remove';
+        event.headers = getHeaders(true);
     };
 
     return (
@@ -123,12 +34,12 @@ const ModalEditAvatar = ({closeModal}) => {
                                 restrictions={{
                                     allowedExtensions: ['.jpg', '.jpeg', '.png', '.gif']
                                 }}
-                                defaultFiles={[]}
                                 withCredentials={false}
+                                defaultFiles={[]}
                                 onBeforeUpload={onBeforeUpload}
                                 onBeforeRemove={onBeforeRemove}
-                                saveUrl="https://demos.telerik.com/kendo-ui/service-v4/upload/save"
-                                removeUrl="https://demos.telerik.com/kendo-ui/service-v4/upload/remove"
+                                saveUrl="/api/avatar/full_v2"
+                                removeUrl="/api/avatar/delete_v2"
                             />
                         </IntlProvider>
                     </LocalizationProvider>
@@ -143,7 +54,6 @@ const ModalEditAvatar = ({closeModal}) => {
                     </div>
                 </div>
             </div>
-            {alert && <Alert {...alert} />}
         </Modal>
     )
 };
