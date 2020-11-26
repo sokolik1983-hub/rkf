3a514c53166bd25ea4d3dadf895ac7b3553d64c1
Leon <leon1258@yandex.ru>
2020-11-26 13:51:25 +0300

    change add video link

--- src/components/UserAddArticle/AddVideoLink/index.js
+++ src/components/UserAddArticle/AddVideoLink/index.js
@@ -1,48 +1,58 @@
-import React, { useState } from "react";
-import getYouTubeID from 'get-youtube-id';
+import React from "react";
+import getYouTubeID from "get-youtube-id";
+import {Field, Form, FormElement} from "@progress/kendo-react-form";
+import FormInput from "../../kendo/Form/FormInput";
 import "./index.scss";
 
 
 const AddVideoLink = ({ setVideoLink, closeModal }) => {
-    const [link, setLink] = useState('');
-    const [error, setError] = useState('');
+    const youtubeLinkValidator = value => {
+        let error = '';
 
-    const handleChange = ({ target }) => {
-        setLink(target.value);
-        setError('');
+        if(!value) {
+            error = 'Добавьте ссылку';
+        } else {
+            const id = getYouTubeID(value);
+            error = !id ? 'Для добавления доступны только ссылки с YouTube' : ''
+        }
+
+        return error;
     };
 
-    const handleSubmit = e => {
-        e.preventDefault();
-        if (link) {
-            const id = getYouTubeID(link);
-            if (id) {
-                setVideoLink(`https://www.youtube.com/embed/${id}`);
-                closeModal();
-            } else {
-                setError('Для добавления доступны только ссылки с YouTube');
-            }
-        } else {
-            setError('Добавьте ссылку');
-        }
+    const handleSubmit = data => {
+        const id = getYouTubeID(data.youtube_link);
+        setVideoLink(`https://www.youtube.com/embed/${id}`);
+        closeModal();
     };
 
     return (
         <div className="add-video-link">
-            <form className="add-video-link__form" onSubmit={handleSubmit}>
-                <div className="add-video-link__form-row">
-                    <label className="add-video-link__form-label">Ссылка на YouTube</label>
-                    <input
-                        type="text"
-                        placeholder="Ссылка на YouTube"
-                        className={`add-video-link__form-input${error ? ' _error' : ''}`}
-                        value={link}
-                        onChange={handleChange}
-                    />
-                    {error && <p className="add-video-link__form-error">{error}</p>}
-                </div>
-                <button className="add-video-link__form-submit" type="submit" disabled={!link}>Прикрепить</button>
-            </form>
+            <Form
+                className="add-video-link__form"
+                onSubmit={handleSubmit}
+                initialValues={{youtube_link: ''}}
+                render={formRenderProps =>
+                    <FormElement>
+                        <Field
+                            id="youtube_link"
+                            name="youtube_link"
+                            placeholder="Ссылка на видео YouTube"
+                            maxLength={150}
+                            cutValue={150}
+                            component={FormInput}
+                            validator={youtubeLinkValidator}
+                        />
+                        <div className="add-video-link__form-controls">
+                            <button
+                                type="submit"
+                                className="add-video-link__form-submit"
+                                disabled={!formRenderProps.modified || !formRenderProps.valid}
+                            >Добавить
+                            </button>
+                        </div>
+                    </FormElement>
+                }
+            />
         </div>
     )
 };
