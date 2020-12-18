c706ffb3ffdc2c5f1e404099622724e51768ae43
Denis <den.bessarab@outlook.com>
2020-12-18 11:31:42 +0300

    add NewsFeed links to Notifications see-all

--- src/components/Layouts/Header/components/Notifications/index.js
+++ src/components/Layouts/Header/components/Notifications/index.js
@@ -2,18 +2,13 @@ import React, { forwardRef, useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import { CSSTransition } from "react-transition-group";
 import ls from "local-storage";
-import { LOGIN_URL, REGISTRATION_URL, DEFAULT_IMG } from "appConfig";
 import { connectLogin, connectWidgetLogin } from "pages/Login/connectors";
 import OutsideClickHandler from "react-outside-click-handler";
-import history from "utils/history";
-import { Request, getHeaders } from "utils/request";
-import Feedback from "components/Feedback";
+import { Request } from "utils/request";
 import LightTooltip from "components/LightTooltip";
 import NotificationCategories from "./NotificationCategories";
 import NotificationItem from "./NotificationItem";
-import { isDevEnv } from 'utils';
 import NotificationsContext from 'app/context';
-import * as signalR from "@microsoft/signalr";
 import Loading from "components/Loading";
 import "./styles.scss";
 
@@ -66,16 +61,8 @@ const Notifications = forwardRef(
             setShowDot(pushedNotification.hasNewMessage);
         }, [pushedNotification]);
 
-        // const [showModal, setShowModal] = useState(false);
-
-        // const alias = ls.get('user_info') ? ls.get('user_info').alias : '';
-        // const name = ls.get('user_info') ? ls.get('user_info').name : '';
-        // const logo = ls.get('user_info') ? ls.get('user_info').logo_link : logo_link;
-        // const userType = ls.get('user_info') ? ls.get('user_info').user_type : '';
-        // const accountType = ls.get('account_type') ? ls.get('account_type') : '';
-        // const personalAccess = ls.get('personal_office_access') ? ls.get('personal_office_access') : false;
-        // const firstName = ls.get('user_info') ? ls.get('user_info').first_name : '';
-        // const lastName = ls.get('user_info') ? ls.get('user_info').last_name : '';
+        const alias = ls.get('user_info') ? ls.get('user_info').alias : '';
+        const user_type = ls.get('user_info').user_type;
 
         const getNotifications = async (type = 1) => {
             setLoaded(false);
@@ -93,6 +80,21 @@ const Notifications = forwardRef(
         const handleNotificationsClick = () => {
             !open && !notifications.length && getNotifications();
             setOpen(!open);
+        }
+
+        const getNewsFeedLink = () => {
+            const buildUrl = id => user_type === 1
+                ? `/user/${alias}/news-feed/${id}`
+                : user_type === 3
+                    ? `/${alias}/news-feed/${id}`
+                    : `/nursery/${alias}/news-feed/${id}`;
+            if (currentCategory === 3) {
+                return buildUrl(4)
+            } else if (currentCategory === 4) {
+                return buildUrl(6)
+            } else {
+                return `/user/${alias}/news-feed/`
+            }
         }
 
         return (
@@ -154,7 +156,7 @@ const Notifications = forwardRef(
                                                     : <div className="NotificationItem nothing-found" style={{ textAlign: 'center' }}>Ничего не найдено</div>
                                             }
                                             <div className="Notifications__list-see-all">
-                                                <Link to={'#'} >Посмотреть все</Link>
+                                                <Link to={() => getNewsFeedLink()} >Посмотреть все</Link>
                                             </div>
                                         </div>
                                     </>}
