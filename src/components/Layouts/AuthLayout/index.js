1719a849e023ae2fa25ddcf68bbe5606b85d83fc
Leon <leon1258@yandex.ru>
2020-12-22 18:03:23 +0300

    part 1

--- src/components/Layouts/AuthLayout/index.js
+++ src/components/Layouts/AuthLayout/index.js
@@ -1,12 +1,21 @@
 import React from "react";
+import Container from "../Container";
 import {DEFAULT_IMG} from "../../../appConfig";
 import "./index.scss";
 
 
 const AuthLayout = ({img = DEFAULT_IMG.authPicture, className, children}) => (
-    <div className={`auth-layout${className ? ' ' + className : ''}`}>
-        <div className="auth-layout__content">{children}</div>
-    </div>
+    <Container className="content">
+        <div className={`auth-layout${className ? ' ' + className : ''}`}>
+            {/*<div className="auth-layout__title">*/}
+            {/*    <h1>Добро пожаловать на Rkf.Online!</h1>*/}
+            {/*    <p>Цифровую платформу Российского кинологического сообщества</p>*/}
+            {/*</div>*/}
+            <div className="auth-layout__content">
+                {children}
+            </div>
+        </div>
+    </Container>
 );
 
 export default React.memo(AuthLayout);
