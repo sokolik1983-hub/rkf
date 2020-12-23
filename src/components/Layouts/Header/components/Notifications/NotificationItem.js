e9ac387ccaa768c329c37169d3d9a7c088111495
Denis <den.bessarab@outlook.com>
2020-12-23 12:59:03 +0300

    article_is_read on click in NotificationItem

--- src/components/Layouts/Header/components/Notifications/NotificationItem.js
+++ src/components/Layouts/Header/components/Notifications/NotificationItem.js
@@ -1,16 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import moment from "moment";
+import { Request } from "utils/request";
 import "moment/locale/ru";
 moment.locale('ru');
 
-const NotificationItem = ({ article_id, short_title, short_content, create_date, logo_link, profile_alias, user_type, is_read }) => {
+const NotificationItem = ({ article_id, id, short_title, short_content, create_date, logo_link, profile_alias, user_type, is_read, redirect_link }) => {
     const profileLink = user_type === 1 ? `/user/${profile_alias}` : user_type === 3 ? `/${profile_alias}` : `/nursery/${profile_alias}`;
-    const handleItemClick = ({ target }) => {
-        const { className } = target;
-        if (className !== 'NotificationItem_link' && className !== 'NotificationItem__logo') {
-            window.location.href = `/news/${article_id}`;
-        }
+    const handleItemClick = async ({ target }) => {
+        await Request({
+            url: `/api/article/article_is_read?id=${id}`,
+            method: 'POST'
+        }, () => {
+            const { className } = target;
+            if (className !== 'NotificationItem_link' && className !== 'NotificationItem__logo') {
+                window.location.href = redirect_link ? redirect_link : `/news/${article_id}`;
+            }
+        }, error => {
+            console.log(error);
+        });
     }
 
     return (
