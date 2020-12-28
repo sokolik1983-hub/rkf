cc88adad52953cbc656f9934c329476565c0a5d2
Denis <den.bessarab@outlook.com>
2020-12-28 11:44:16 +0300

    Make NewsFeed menu position:fixed

--- src/pages/NewsFeed/components/CategoriesList.js
+++ src/pages/NewsFeed/components/CategoriesList.js
@@ -1,11 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Card from "components/Card";
 import { categories } from "../config";
 
 const CategoriesList = ({ canEdit, setModal, activeCategoryId, setActiveCategoryId, homePage }) => {
     const isActive = (value) => activeCategoryId === value ? "NewsFeed__category-item active" : "NewsFeed__category-item";
 
+    const handleCategoryClick = (id) => {
+        setActiveCategoryId(id)
+        window.scrollTo(0, 0);
+    }
     return <Card>
         <ul className="NewsFeed__inner-list">
             {categories.map(({ id, name, icon, disabled }, key) => {
@@ -16,7 +19,7 @@ const CategoriesList = ({ canEdit, setModal, activeCategoryId, setActiveCategory
                     </li>;
                 }
                 return <li
-                    onClick={() => setActiveCategoryId(id)}
+                    onClick={() => handleCategoryClick(id)}
                     className={isActive(id)}
                     key={key} >
                     <span className={icon} />
