a11dc95d0cd57ecdd03e4206fb757d3292cc0f65
Denis <den.bessarab@outlook.com>
2020-12-28 12:56:43 +0300

    Disable login/reg links in Header on Login page

--- src/components/Layouts/Header/index.js
+++ src/components/Layouts/Header/index.js
@@ -1,26 +1,26 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import Container from "../Container";
 import Search from "./components/Search";
 import Nav from "./components/Nav";
 import WidgetLogin from "./components/WidgetLogin";
 import Notifications from "./components/Notifications";
-import {connectShowFilters} from "../connectors";
+import { connectShowFilters } from "../connectors";
 import "./index.scss";
 
 
-const Header = ({withFilters, isOpenFilters, setShowFilters , login_page}) => (
+const Header = ({ withFilters, isOpenFilters, setShowFilters, login_page }) => (
     <header className="header">
         <Container className="header__content">
-            <Link to="/" className="header__logo"/>
+            <Link to="/" className="header__logo" />
             {withFilters &&
-                <button className="header__filters" onClick={() => setShowFilters({isOpenFilters: !isOpenFilters})}/>
+                <button className="header__filters" onClick={() => setShowFilters({ isOpenFilters: !isOpenFilters })} />
             }
             <Search withFilters={withFilters} />
-            <Nav/>
+            <Nav login_page={login_page} />
             <div className="header__widgets">
-                <Notifications/>
-                <WidgetLogin login_page={login_page}/>
+                <Notifications />
+                <WidgetLogin login_page={login_page} />
             </div>
         </Container>
     </header>
