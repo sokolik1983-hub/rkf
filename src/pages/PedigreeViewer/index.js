7fa30029cea048fc819546f620f4cc8c6419b055
Maksim Borovik <mouxez@gmail.com>
2021-01-15 16:32:09 +0300

    add pdf viewer

--- src/pages/PedigreeViewer/index.js
+++ src/pages/PedigreeViewer/index.js
@@ -1,20 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Viewer, { Worker } from '@phuocng/react-pdf-viewer';
-import Loading from "../../components/Loading";
 import Layout from "../../components/Layouts";
-import { Request } from "../../utils/request";
 import '@phuocng/react-pdf-viewer/cjs/react-pdf-viewer.css';
 import "./index.scss";
 
 
-const PedigreeViewer = ({url = 'http://pedigreeapi.rkf24.ru:5555/pedigree/NzU0ODM5Nzgy'}) => {
+const PedigreeViewer = ({ history, match }) => {
 
     return <Layout>
-        <div className="pedigree-viewer">
-            <iframe src={url}
-                 title="просмотр документа">Ваш браузер не поддерживает фреймы</iframe>
-        </div>
-    </Layout>
+            <div className="document-page">
+                <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.5.207/build/pdf.worker.min.js">
+                    <Viewer fileUrl={`http://pedigreeapi.rkf24.ru:5555/pedigree/NzU0ODM5Nzgy`} />
+                </Worker>
+            </div>
+        </Layout>
 };
 
 export default React.memo(PedigreeViewer);
