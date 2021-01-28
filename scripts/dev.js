ff31a90f244da6e4f9e70d8d037741fc5c12341e
Denis <den.bessarab@outlook.com>
2021-01-28 18:00:49 +0300

    Replace dev.js

--- scripts/dev.js
+++ scripts/dev.js
@@ -49,7 +49,7 @@ const argv = process.argv.slice(2);
 const writeStatsJson = argv.indexOf('--stats') !== -1;
 
 // Generate configuration
-const config = configFactory('development');
+const config = configFactory('production');
 
 // We require that you explicitly set browsers and do not fall back to
 // browserslist defaults.
