5dbf8892782639cd1d04d5f44637ea5d6dfd3a44
Maksim Borovik <mouxez@gmail.com>
2021-02-10 18:25:41 +0300

    change card name

--- src/pages/Docs/components/CheckMembership/CheckMembershipForm/index.js
+++ src/pages/Docs/components/CheckMembership/CheckMembershipForm/index.js
@@ -227,7 +227,7 @@ const CheckMembershipForm = ({ clubAlias, history, status }) => {
                 <div className="club-documents-status__head">
                     <Link to={`/${clubAlias}/documents/responsible`} className="club-documents-status__head-link">Личный кабинет</Link>
                     &nbsp;/&nbsp;
-                    <span className="user-documents__breadcrumbs-item">Подтверждение членства</span>
+                    <span className="user-documents__breadcrumbs-item">Отчёты о племенной деятельности</span>
                 </div>
                 {
                     !loaded ?
