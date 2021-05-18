38c1de8a57b3ebaece1a4b75c875cf1ea2f02565
Maksim Borovik <mouxez@gmail.com>
2021-05-18 16:12:03 +0300

    add zline bookform

--- src/components/BookformCard/index.js
+++ src/components/BookformCard/index.js
@@ -53,6 +53,7 @@ const BookformCard = ({ url, distinction }) => {
     const [authorizedAccess, setAuthorizedAccess] = useState(true);
 
     const user_type = ls.get('user_info') ? ls.get('user_info').user_type : '';
+    const user_email = ls.get('user_info') ? ls.get('user_info').mail : '';
 
     useEffect(() => {
         if (user_type === 4) {
@@ -99,6 +100,25 @@ const BookformCard = ({ url, distinction }) => {
         }
     };
 
+    const correspondenceURL = `https://zline.me/widgets/registration-for-service?service_id=27&email=` + user_email;
+
+    const handleZlineClick = (e, targetUrl, rkf) => {
+        e.preventDefault();
+        (() => Request({
+            url: `/api/registration/user_info_for_zline_session_registration`
+        }, data => {
+            setLink(targetUrl + (data.first_name ? `&first_name=${data.first_name.replaceAll(' ', '_')}` : '') + (data.last_name ? `&last_name=${data.last_name.replaceAll(' ', '_')}` : '') + (data.phone ? `&phone=${data.phone.replaceAll(' ', '_')}` : '') + (data.additional_info ? `&additional_info=${data.additional_info.replaceAll(' ', '_')}` : ''));
+            setColor('_blue');
+            setHeaderName(`Запись в ${rkf ? 'РКФ' : federation}`);
+            setShowModal(true);
+            setLoading(false);
+        }, error => {
+            console.log(error.response);
+            setLoading(false);
+        }))();
+    };
+
+
     const Bookform = <>
         {federation && authorizedAccess && <Card>
             <div className="documents-page__icon registration-icon" />
@@ -116,8 +136,13 @@ const BookformCard = ({ url, distinction }) => {
             <p>В данном разделе Вы можете записаться на очный прием в офисе РКФ. Для этого выберете дату и время посещения, а также тип услуги, которая Вас интересует. После подтверждения записи на Ваш e-mail будет отправлено письмо с датой и временем Вашей записи, которое необходимо будет предъявить на входе. При посещении офиса необходимо иметь с собой документ, удостоверяющий личность.</p>
             <p>Отменить запись в РКФ Вы можете в группе в Telegram по ссылке <a href="https://t.me/EntryRKFOnline" target="_blank" rel="noopener noreferrer">https://t.me/EntryRKFOnline</a></p>
             <hr />
-            <div className="Card__links">
-                <Link to={`/`} onClick={e => handleClick(e, true)}>Запись в РКФ</Link>
+            <div style={{ display: 'flex', justifyContent: 'flex-start', flexWrap: 'wrap' }}>
+                <div className="Card__links" style={{ marginRight: '20px' }}>
+                    <Link to={`/`} onClick={e => handleClick(e, true)}>Запись в РКФ</Link>
+                </div>
+                <div className="Card__links">
+                    <Link to={`/`} onClick={e => handleZlineClick(e, correspondenceURL, true)}>Подача корреспонденции в РКФ</Link>
+                </div>
             </div>
         </Card>
     </>;
