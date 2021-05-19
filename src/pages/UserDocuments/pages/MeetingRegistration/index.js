3600a569d04aca47975c613e04744b52eed58047
Maksim Borovik <mouxez@gmail.com>
2021-05-19 11:05:23 +0300

    add parameter

--- src/pages/UserDocuments/pages/MeetingRegistration/index.js
+++ src/pages/UserDocuments/pages/MeetingRegistration/index.js
@@ -24,7 +24,7 @@ const MeetingRegistration = () => {
     const handleZlineClick = (e, targetUrl, isRKF) => {
         e.preventDefault();
         (() => Request({
-            url: `/api/registration/user_info_for_zline_session_registration`
+            url: `/api/registration/user_info_for_zline_session_registration?alias=rkf`
         }, data => {
             setIframeLink(targetUrl + (data.first_name ? `&first_name=${data.first_name.replaceAll(' ', '_')}` : '') + (data.last_name ? `&last_name=${data.last_name.replaceAll(' ', '_')}` : '') + (data.phone ? `&phone=${data.phone.replaceAll(' ', '_')}` : '') + (data.additional_info ? `&additional_info=${data.additional_info.replaceAll(' ', '_')}` : ''));
             setIsRKF(isRKF);
