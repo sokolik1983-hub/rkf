ec8c31ee66c6dee32f1bc204ceddefe939b2ae92
Maksim Borovik <mouxez@gmail.com>
2021-05-25 14:18:24 +0300

    WIP

--- src/app/context.js
+++ src/app/context.js
@@ -8,7 +8,7 @@ import { connectAuthVisible } from "pages/Login/connectors";
 export const NotificationsContext = React.createContext(null);
 
 const hubUrl = isDevEnv()
-    ? 'https://notification.stage.uep24.ru/api/hubs/user_hub'
+    ? 'https://stage.uep24.ru/notification/user_hub'
     : 'https://rkf.online/api/hubs/user_hub';
 
 const NotificationsProvider = ({ isAuthenticated, children }) => {
