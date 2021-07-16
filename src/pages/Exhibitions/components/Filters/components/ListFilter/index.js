161640aa94c3c4b3985b5e02c52c6cd2a88214ce
sokolik1983-hub <lionsbet@outlook.com>
2021-07-16 13:32:07 +0300

    changes in exhibitions page: filter refactor and title refactor 160721

--- src/pages/Exhibitions/components/Filters/components/ListFilter/index.js
+++ src/pages/Exhibitions/components/Filters/components/ListFilter/index.js
@@ -6,18 +6,6 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-const exhibitionsMenuLi = [{
-        title: 'Все',
-    },
-    {
-        title: 'Выставочные мероприятия',
-    },
-    {
-        title: 'Племенные мероприятия',
-    },
-    {
-        title: 'Состязания и испытания рабочих качеств',
-    }]
 const ListFilter = ({ categoryId, exhibitionsForTable, standardView, setExporting, exporting, setStandardView }) => {
     const [activeType, setActiveType] = useState(0);
 
@@ -41,20 +29,30 @@ const ListFilter = ({ categoryId, exhibitionsForTable, standardView, setExportin
     return (
         <div className="exhibitions-page__list-filter">
            <div className="exhibitions-page__title-inner">
-               <h4 className="list-filter__title">Мероприятия</h4>
+                   <h4 className="list-filter__title">Мероприятия</h4>
+                   {
+                       (clientWidth < 560) ? (<button className={"exhibitions-page__control " + (standardView ? 'exhibitions-page__control--tableIcon' : 'exhibitions-page__control--backIcon')} onClick={() => setStandardView(!standardView)}>
+                       </button>) : ''
+                   }
+
                <div className="exhibitions-page__controls">
                    {!!exhibitionsForTable.length && !standardView &&
-                   <button
-                       className="exhibitions-page__control exhibitions-page__control--downloadIcon"
-                       onClick={() => setExporting(true)}
-                       disabled={exporting}
-                   >
-                       Скачать PDF
-                   </button>
+                   <div className="exhibitions-page__downloadBtn-wrap">
+                       <button
+                           className="exhibitions-page__control exhibitions-page__control--downloadIcon"
+                           onClick={() => setExporting(true)}
+                           disabled={exporting}
+                       >
+                           Скачать PDF
+                       </button>
+                   </div>
                    }
-                   <button className={"exhibitions-page__control " + (standardView ? 'exhibitions-page__control--tableIcon' : 'exhibitions-page__control--backIcon')} onClick={() => setStandardView(!standardView)}>
-                       {standardView ? 'Переключиться на табличный вид' : 'Вернуться к стандартному просмотру'}
-                   </button>
+                   {
+                       (clientWidth > 560) ? (<button className={"exhibitions-page__control " + (standardView ? 'exhibitions-page__control--tableIcon' : 'exhibitions-page__control--backIcon')} onClick={() => setStandardView(!standardView)}>
+                           {standardView ? 'Переключиться на табличный вид' : 'Вернуться к стандартному просмотру'}
+                       </button>) : ''
+                   }
+
                </div>
            </div>
 
@@ -72,14 +70,30 @@ const ListFilter = ({ categoryId, exhibitionsForTable, standardView, setExportin
                         adaptiveHeight={true}
                         variableWidth={true}
                     >
-                        {
-                           exhibitionsMenuLi.map((item, index) => (<li className="list-filter__item">
-                            <span
-                            className={`list-filter__control${activeType === index ? ' _active' : ''}`}
-                            onClick={() => handleClick(index)}
-                            >{item.title}</span>
-                            </li>))
-                        }
+                        <li className="list-filter__item">
+                        <span
+                            className={`list-filter__control${activeType === 0 ? ' _active' : ''}`}
+                            onClick={() => handleClick(0)}
+                        >Все</span>
+                        </li>
+                        <li className="list-filter__item">
+                        <span
+                            className={`list-filter__control${activeType === 1 ? ' _active' : ''}`}
+                            onClick={() => handleClick(1)}
+                        >Выставочные <br /> мероприятия</span>
+                        </li>
+                        <li className="list-filter__item">
+                        <span
+                            className={`list-filter__control${activeType === 2 ? ' _active' : ''}`}
+                            onClick={() => handleClick(2)}
+                        >Племенные <br /> мероприятия</span>
+                        </li>
+                        <li className="list-filter__item">
+                        <span
+                            className={`list-filter__control${activeType === 3 ? ' _active' : ''}`}
+                            onClick={() => handleClick(3)}
+                        >Состязания и испытания <br /> рабочих качеств</span>
+                        </li>
                     </Slider>) : (<ul className="list-filter">
                         <li className="list-filter__item">
                         <span
