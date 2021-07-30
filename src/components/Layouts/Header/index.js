6d7d66053a18a46f92e8499d429ac09e74c3780d
niko <mikheikin.nikolay@uep24.ru>
2021-07-30 12:43:39 +0300

    delete conflictÐ

--- src/components/Layouts/Header/index.js
+++ src/components/Layouts/Header/index.js
@@ -11,16 +11,7 @@ import useIsMobile from '../../../utils/useIsMobile';
 
 import './index.scss';
 
-const Header = ({
-                    isAuthenticated,
-                    withFilters,
-                    isOpenFilters,
-                    setShowFilters,
-                    login_page,
-                    setNotificationsLength,
-                    isOpen,
-                    setIsOpen
-                }) => {
+const Header = ({ isAuthenticated, withFilters, isOpenFilters, setShowFilters, login_page, setNotificationsLength, isOpen, setIsOpen }) => {
     const isMobile = useIsMobile(1080);
     const { pathname } = useLocation();
     const [openWidgets, setOpenWidgets] = useState(false);
@@ -60,19 +51,19 @@ const Header = ({
                 {!isMobile
                 && <Nav isAuthenticated={isAuthenticated} />
                 }
-                <div className='header__widgets'>
+                <div className="header__widgets">
                     {isAuthenticated &&
-                    <>
-                        {/*<div className={`header__widgets--feedback${login_page ? ' login-page' : ''}`}>*/}
-                        {/*    <Feedback isMainNav={true}/>*/}
-                        {/*</div>*/}
-                        <div onClick={hideSideMenu} className='header__widgets-notifications-wrap'>
-                            <Notifications open={openWidgets}
-                                           setOpen={setOpenWidgets}
-                                           setNotificationsLength={setNotificationsLength}
-                            />
-                        </div>
-                    </>
+                        <>
+                            {/*<div className={`header__widgets--feedback${login_page ? ' login-page' : ''}`}>*/}
+                            {/*    <Feedback isMainNav={true}/>*/}
+                            {/*</div>*/}
+                            <div onClick={hideSideMenu} className="header__widgets-notifications-wrap">
+                                <Notifications  open={openWidgets}
+                                                setOpen={setOpenWidgets}
+                                                setNotificationsLength={setNotificationsLength}
+                                />
+                            </div>
+                        </>
                     }
 
                     {isMobile  ?
@@ -84,40 +75,30 @@ const Header = ({
                             }}>
                             <div className={isOpenFilters ? 'open' : ''}>
 
-                                <svg width='20' height='18' viewBox='0 0 20 18' fill='none'
-                                     xmlns='http://www.w3.org/2000/svg'>
-                                    <path d='M3.47827 12.6608V17.4434' stroke={strokeColor} strokeWidth='1.32'
-                                          strokeMiterlimit='10' />
-                                    <path d='M0 15.0521H3.47826' stroke={strokeColor} strokeWidth='1.32'
-                                          strokeMiterlimit='10' />
-                                    <path d='M6.95654 15.0521H20' stroke={strokeColor} strokeWidth='1.32'
-                                          strokeMiterlimit='10' />
-                                    <path d='M0 2.3913H6.08696' stroke={strokeColor} strokeWidth='1.32'
-                                          strokeMiterlimit='10' />
-                                    <path d='M9.56519 2.3913H20' stroke={strokeColor} strokeWidth='1.32'
-                                          strokeMiterlimit='10' />
-                                    <path d='M6.08691 0V4.78261' stroke={strokeColor} strokeWidth='1.32'
-                                          strokeMiterlimit='10' />
-                                    <path d='M15.6522 6.33044V11.1131' stroke={strokeColor} strokeWidth='1.32'
-                                          strokeMiterlimit='10' />
-                                    <path d='M15.6522 8.72174H20' stroke={strokeColor} strokeWidth='1.32'
-                                          strokeMiterlimit='10' />
-                                    <path d='M0 8.72174H12.1739' stroke={strokeColor} strokeWidth='1.32'
-                                          strokeMiterlimit='10' />
-                                </svg>
-
-                            </div>
-                            <span style={{ color: isOpenFilters && '#3366ff' }}>Фильтр</span>
+                            <svg width="20" height="18" viewBox="0 0 20 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M3.47827 12.6608V17.4434" stroke={strokeColor} strokeWidth="1.32" strokeMiterlimit="10"/>
+                                <path d="M0 15.0521H3.47826" stroke={strokeColor}  strokeWidth="1.32" strokeMiterlimit="10"/>
+                                <path d="M6.95654 15.0521H20" stroke={strokeColor}  strokeWidth="1.32" strokeMiterlimit="10"/>
+                                <path d="M0 2.3913H6.08696" stroke={strokeColor}  strokeWidth="1.32" strokeMiterlimit="10"/>
+                                <path d="M9.56519 2.3913H20" stroke={strokeColor}  strokeWidth="1.32" strokeMiterlimit="10"/>
+                                <path d="M6.08691 0V4.78261" stroke={strokeColor}  strokeWidth="1.32" strokeMiterlimit="10"/>
+                                <path d="M15.6522 6.33044V11.1131" stroke={strokeColor}  strokeWidth="1.32" strokeMiterlimit="10"/>
+                                <path d="M15.6522 8.72174H20" stroke={strokeColor}  strokeWidth="1.32" strokeMiterlimit="10"/>
+                                <path d="M0 8.72174H12.1739" stroke={strokeColor}  strokeWidth="1.32" strokeMiterlimit="10"/>
+                            </svg>
 
                         </div>
-                        : <WidgetLogin login_page={login_page} />}
+                        <span style={{color: isOpenFilters && "#3366ff"}}>Фильтр</span>
+
+                        </div>
+                        : <WidgetLogin login_page={login_page}/>}
                 </div>
 
 
             </Container>
 
         </header>
-    );
+    )
 };
 
 export default connectAuthVisible(connectShowFilters(React.memo(Header)));
