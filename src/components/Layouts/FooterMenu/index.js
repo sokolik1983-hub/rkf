b4af4e5ea1c694a9f969f607a5c4455bbdc7d46e
niko <mikheikin.nikolay@uep24.ru>
2021-08-09 16:43:22 +0300

    little bit workingÐ

--- src/components/Layouts/FooterMenu/index.js
+++ src/components/Layouts/FooterMenu/index.js
@@ -1,22 +1,22 @@
 import React, { useState, useEffect } from 'react';
-import { NavLink, useLocation, Link } from "react-router-dom";
+import { NavLink, useLocation, Link, withRouter } from 'react-router-dom';
 import useIsMobile from "../../../utils/useIsMobile";
 import WidgetLogin from "../Header/components/WidgetLogin";
 import ls from "local-storage";
 import { connectAuthVisible } from "../../../pages/Authorization/connectors";
 import { footerNav } from "../../../appConfig";
-import { clubNav } from "../../../pages/Club/config";
-import { kennelNav } from "../NurseryLayout/config";
-import { userNav } from "../UserLayout/config";
+import { clubNav, endpointGetClubInfo } from '../../../pages/Club/config';
 import { isFederationAlias } from "../../../utils";
 import UserMenu from "../UserMenu";
 import MenuComponent from "../../MenuComponent";
 import { connectShowFilters } from "../connectors";
 import ZlineModal from "../../ZlineModal";
+import { Request } from "../../../utils/request";
 
 import "./footerMenu.scss";
 
 const FooterMenu = ({
+    history,
     match,
     is_active_profile,
     profile_id,
@@ -25,38 +25,53 @@ const FooterMenu = ({
     setShowFilters,
     setIsOpen }) => {
     const isMobile1080 = useIsMobile(1080);
-    const { alias, user_type, id, name } = ls.get('user_info') || {};
+    const { alias,  user_type, id, name } = ls.get('user_info') || {};
     const { pathname } = useLocation();
     const [canEdit, setCanEdit] = useState(false);
     const [showZlineModal, setShowZlineModal] = useState(false);
-console.log(pathname)
+    const [clubInfo, setClubInfo] = useState(null);
+    const aliasParams = match.path;
+    const isExhibitionPage = aliasParams === pathname;
+
+    useEffect(() => {
+        (() => Request({
+            url: endpointGetClubInfo + aliasParams ? aliasParams : alias
+        }, data => {
+            if (data.user_type === 4) {
+                history.replace(`/kennel/${match.params.route}/news`);
+            } else {
+                setClubInfo(data);
+                setCanEdit(isAuthenticated && is_active_profile && profile_id === data.id);
+            }
+        }, error => {
+            console.log(error.response);
+        }))();
+    }, [match]);
+
+
     useEffect(() => {
         if (alias) {
             setCanEdit(isAuthenticated && is_active_profile && profile_id === id);
         }
     }, []);
 
-    // const aliasss = match.params.route;
-    const pathAlias = pathname.substr(pathname.lastIndexOf('/') + 1);
-    //
-    const urlAlias = pathname.search('kennel') === 1 ? kennelNav(pathAlias) : clubNav(pathAlias);
-    //
-    const isFederation = pathAlias === 'rkf'
-        || pathAlias === 'rfss'
-        || pathAlias === 'rfls'
-        || pathAlias === 'rfos'
-        || pathAlias === 'oankoo';
+    const urlAlias = pathname.search('kennel') === 1 ? pathname.split('/')[2] : pathname.split('/')[1];
 
-    const checkPathForMenu = pathAlias !== 'rkf'
-        && pathAlias !== 'organizations'
-        && pathAlias !== 'exhibitions'
-        && pathAlias !== 'search'
-        && pathAlias !== 'base-search'
-        && pathAlias !== ''
-        && pathAlias !== 'uploaded-documents'
-        && pathAlias !== 'login'
-        && pathAlias !== 'registration';
-console.log("checkPathForMenu", !checkPathForMenu)
+    const isFederation = aliasParams === 'rkf'
+        || aliasParams === 'rfss'
+        || aliasParams === 'rfls'
+        || aliasParams === 'rfos'
+        || aliasParams === 'oankoo';
+
+    const checkPathForMenu = aliasParams !== 'rkf'
+        && aliasParams !== 'organizations'
+        && aliasParams !== 'exhibitions'
+        && aliasParams !== 'search'
+        && aliasParams !== 'base-search'
+        && aliasParams !== ''
+        && aliasParams !== 'uploaded-documents'
+        && aliasParams !== 'login'
+        && aliasParams !== 'registration';
     const hideSideMenu = () => {
         setShowFilters({ isOpenFilters: false });
         setIsOpen(false);
@@ -65,14 +80,20 @@ console.log("checkPathForMenu", !checkPathForMenu)
         e.preventDefault();
         setShowZlineModal(true);
     };
-console.log(isAuthenticated)
+
+
     return (
         <>
             {isMobile1080 &&
 
                 <div className="footer__menu"
                      onClick={hideSideMenu}
-                     style={{padding: !checkPathForMenu && !isFederation ? "10px 20px 5px 15px" :  isAuthenticated && isFederation ?  "10px 20px 5px 15px" :  "10px 19% 5px 15px"}}
+                     style={{padding:
+                             checkPathForMenu && !isFederation
+                                 ? "10px 20px 5px 15px"
+                                 :  isAuthenticated && isFederation
+                                     ?  "10px 20px 5px 15px"
+                                     :  "10px 19% 5px 15px"}}
                 >
                     <NavLink className="footer__menu-link" to='/'>
                         {footerNav[0].image}
@@ -101,36 +122,20 @@ console.log(isAuthenticated)
                     }
 
 
-                    {/*{isAuthenticated && !checkPathForMenu && !isFederation && user_type !== 5 && alias !== 'rkf' &&*/}
-                    {/*    <UserMenu*/}
-                    {/*        notificationsLength={notificationsLength}*/}
-                    {/*        footerNav={footerNav[4]}*/}
-                    {/*        userNav={canEdit && user_type &&*/}
-                    {/*            user_type === 1 ?*/}
-                    {/*            userNav(alias)*/}
-                    {/*            : user_type === 3 ?*/}
-                    {/*                clubNav(alias)*/}
-                    {/*                : user_type === 4 ?*/}
-                    {/*                    kennelNav(alias)*/}
-                    {/*                    : []*/}
-                    {/*        }*/}
-                    {/*    />*/}
-                    {/*}*/}
 
-                    {isAuthenticated && isFederation  &&
+                    {(urlAlias || alias) &&
+
+                        isFederationAlias( urlAlias || alias) ?
                         <MenuComponent
-                            footerNav={footerNav[4]}
-                            alias={urlAlias}
-                            name={name}
-                            isFederation={isFederationAlias}
-                        />
-                    }
-
-                    {isAuthenticated && !isFederation && !checkPathForMenu &&
-                        <UserMenu
-                            notificationsLength={notificationsLength}
-                            footerNav={footerNav[4]}
-                            userNav={urlAlias}
+                            isExhibitionPage={isExhibitionPage}
+                            alias={ urlAlias || alias}
+                            name={clubInfo?.short_name || clubInfo?.name || 'Название федерации отсутствует'}
+                            isFederation={true}
+                        /> :
+                        <UserMenu userNav={canEdit
+                            ? clubNav(clubInfo?.club_alias) // Show NewsFeed menu item to current user only
+                            : clubNav(clubInfo?.club_alias).filter(i => i.id !== 2)}
+                                  notificationsLength={notificationsLength}
                         />
                     }
 
@@ -147,4 +152,4 @@ console.log(isAuthenticated)
     )
 };
 
-export default connectAuthVisible(connectShowFilters(React.memo(FooterMenu)));
+export default withRouter(connectAuthVisible(connectShowFilters(React.memo(FooterMenu))));
