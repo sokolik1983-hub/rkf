bd85428bbc6265f39c21cd97fb65c122737c450c
Aleksey Sokolov <lionsbet@outlook.com>
2021-09-30 14:18:53 +0300

    add_styles_for_search-layout-problem

--- src/utils/blockContent.js
+++ src/utils/blockContent.js
@@ -1,7 +1,11 @@
 export function blockContent(block) {
     if(block) {
         document.querySelector('body').classList.add('hidden-content');
+        document.querySelector('.header').classList.add('hidden-content');
+        document.querySelector('html').classList.add('hidden-content');
     } else {
         document.querySelector('body').classList.remove('hidden-content');
+        document.querySelector('.header').classList.remove('hidden-content');
+        document.querySelector('html').classList.remove('hidden-content');
     }
 }
