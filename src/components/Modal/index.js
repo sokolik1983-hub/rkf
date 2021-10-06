86459563bddea656588176ea5f4f5842fa74e800
mikheikin.nikolay <mikheikin.nikolay@uep24.ru>
2021-10-06 11:03:08 +0300

    addiction useEffect

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -8,7 +8,7 @@ import './styles.scss';
 const Modal = ({ showModal, handleClose, handleX, children, noBackdrop = false, className, headerName, iconName}) => {
     useEffect(() => {
         blockContent(showModal);
-    });
+    }, []);
     return (
         ReactDOM.createPortal(
             <div className={(showModal ? 'Modal' : 'Modal--hidden') + (noBackdrop ? ' no-backdrop' : '') + (className ? ' ' + className : '')}>
