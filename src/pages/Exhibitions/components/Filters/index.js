a60ba5eb796eb903eacda6e25802a8afc7462ec6
Jen <gk_ln@mail.ru>
2021-10-21 08:46:41 +0300

    remove unused useRef

--- src/pages/Exhibitions/components/Filters/index.js
+++ src/pages/Exhibitions/components/Filters/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import StickyBox from "react-sticky-box";
 import Loading from "../../../../components/Loading";
 import UserHeader from "../../../../components/redesign/UserHeader";
