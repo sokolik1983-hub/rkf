0c91686012834274f6aa6e6ba10ec3af94fd7bcc
sokolov.aa <lionsbet@outlook.com>
2021-11-08 17:28:09 +0300

    fix-changes__08

--- src/components/CardOrganization/index.js
+++ src/components/CardOrganization/index.js
@@ -46,84 +46,21 @@ const CardOrganization = ({   id,
                             />
                             <div className="card-organization__name-wrap">
                                 <div>
-                                    <div className="card-organization__info">
-                                        <div className="card-organization__info-item">
-                                            <span className="card-organization__subtitle">{ owner_position || 'Контактное лицо' }</span>&nbsp;
-
-                                            <span>
-                                                { owner_name ?
-                                                    url ?
-                                                        <Link to={ url }>{ owner_name }</Link> :
-                                                        owner_name :
-                                                    'Не указано'
-                                                }
-                                            </span>
-                                        </div>
-
-                                        { user_type !== 0 && user_type !== 5 && phones && !!phones.length &&
-                                            <div className="card-organization__info-item">
-                                                <span className="card-organization__subtitle">Телефон</span>&nbsp;
-
-                                                <span>{ phones.join(`, `) }</span>
-                                            </div>
-                                        }
-
-                                        { user_type !== 0 && user_type !== 5 && mails && !!mails.length &&
-                                            <div className="card-organization__info-item">
-                                                <span className="card-organization__subtitle">E-mail</span>&nbsp;
-
-                                                <span>{ mails.join(`, `) }</span>
-                                            </div>
-                                        }
-
-                                        { user_type === 7 && site &&
-                                            <div className="card-organization__info-item">
-                                                <span className="card-organization__subtitle">Сайт</span>&nbsp;
-
-                                                <a href={ site.includes('http') ? site : `http://${ site }` }
-                                                   target="_blank" rel="noopener noreferrer">{ site }</a>
-                                            </div>
-                                        }
-
-                                        { user_type === 4 && breeds && !!breeds.length &&
-                                            <div className="card-organization__info-item">
-                                                <span className="card-organization__subtitle">Породы</span>&nbsp;
-
-                                                <span>{ breeds.slice(0, 4).join(`, `) }</span>
-                                            </div>
-                                        }
-                                    </div>
-
-                                    <div className="card-organization__text">
-                                        { content }
-                                    </div>
-                                </div>
-                            </div>
-                        </div> :
-
-                        <div className="card-organization__author">
-                            <span className="card-organization__logo" style={ {
-                                backgroundImage: `url(${ logo || DEFAULT_IMG.clubAvatar })`
-                            } }/>
-
-                            <div className="card-organization__container">
-                                <div className="card-organization__heading">
-                                    <span className="card-organization__name" title={ name || 'Название отсутствует' }>
-                                        { (user_type === 3 || user_type === 4 || user_type === 5 || user_type === 7) &&
+                                    <div className="card-organization__name-inner">
+                                        <Link to={url} className="card-organization__name" title={name || 'Название отсутствует'}>
+                                            {(user_type === 3 || user_type === 4 || user_type === 5 || user_type === 7) &&
                                             <>
                                                 <span>
-                                                    { user_type === 3
-                                                        ? 'Клуб'
-                                                        : user_type === 4
-                                                            ? 'Питомник' : user_type === 5
-                                                                ? 'Федерация' : user_type === 7
-                                                                    ? 'НКП' : ''
-                                                    }
+                                                    {user_type === 3 ? 'Клуб' : user_type === 4 ? 'Питомник' : user_type === 5 ? 'Федерация' : user_type === 7 ? 'НКП' : ''}
                                                 </span>
-                                                &nbsp;
+                                            &nbsp;
                                             </>
                                         }
-
+                                            <span>{name || 'Название отсутствует'}</span>
+                                        </Link>
+                                        <span className="card-organization__mark">
+                                        {active_rkf_user &&
+                                        <ActiveUserMark/>
                                         <span>{ name || 'Название отсутствует' }</span>
                                     </span>
 
