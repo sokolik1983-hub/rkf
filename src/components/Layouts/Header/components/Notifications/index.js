bd2db1223ab95852ac3f8f25007511586ece7fbc
Nikita <akalina164@mail.ru>
2021-11-24 14:20:00 +0300

    fixing_to_the_way_it_was

--- src/components/Layouts/Header/components/Notifications/index.js
+++ src/components/Layouts/Header/components/Notifications/index.js
@@ -19,19 +19,19 @@ import "./styles.scss";
 const defaultCategories = [
     {
         id: 2,
-        name: 'Все новые',
+        name: 'Новые',
         icon: '/static/new-icons/notifications/new.svg',
         count: 0
     },
     {
         id: 3,
-        name: 'Все важные',
+        name: 'Важные',
         icon: '/static/new-icons/notifications/required.svg',
         count: 0
     },
     {
         id: 4,
-        name: 'Все заявки',
+        name: 'Заявки',
         icon: '/static/new-icons/notifications/applications.svg',
         count: 0
     },
