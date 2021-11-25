f868efd2b581c426922c3e5fe070dc7d4403804a
Demidov Vsevolod <demidov.vsevolod@uep24.ru>
2021-11-25 11:23:58 +0300

    prewiev docs in new brawser tab

--- src/pages/UserDocuments/components/DocumentLink/index.js
+++ src/pages/UserDocuments/components/DocumentLink/index.js
@@ -1,44 +1,39 @@
-import React, { useState } from "react";
-import Loading from "../../../../components/Loading";
-import Modal from "../../../../components/Modal";
-import {getHeaders} from "../../../../utils/request";
-import "./index.scss";
+import React, { useState, useEffect } from 'react';
+import { getHeaders } from '../../../../utils/request';
+
+import './index.scss';
 
 
-const DocumentLink = ({docId}) => {
+const DocumentLink = ({ docId }) => {
     const headers = getHeaders();
-    const [showModal, setShowModal] = useState(false);
     const [url, setUrl] = useState('');
 
-    const getDocument = () => {
-        if (isNaN(docId) || !docId) return;
+    useEffect(() => {
+        getDocument();
+    }, []);
 
-        fetch(`/api/requests/dog_health_check_request/doghealthcheckdocument?id=` + docId, {headers})
+    const getDocument = () => {
+        if (!+docId) return;
+
+        fetch(`/api/requests/dog_health_check_request/doghealthcheckdocument?id=${docId}`, { headers })
             .then(res => res.blob())
             .then(data => URL.createObjectURL(data))
             .then(url => setUrl(url));
     };
 
+
     return (
         <>
             {!!docId &&
-                <button
-                    className="btn nomargin"
-                    type="button"
-                    onClick={() => {
-                        setShowModal(true);
-                        getDocument();
-                    }}
-                >Посмотреть</button>
+            <a
+                className='btn nomargin'
+                href={url}
+                target='_blank'
+                rel='noreferrer noopener'
+            >Посмотреть</a>
             }
-            <Modal className="document-link__modal" showModal={showModal} handleClose={() => setShowModal(false)}>
-                {url ?
-                    <embed src={url} className="document-link__embed"/> :
-                    <Loading/>
-                }
-            </Modal>
         </>
-    )
+    );
 };
 
 export default React.memo(DocumentLink);
