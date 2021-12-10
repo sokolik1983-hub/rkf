1001566508a9430ed8e683f919ae0a5b7dc90378
Demidov Vsevolod <demidov.vsevolod@uep24.ru>
2021-12-10 13:04:14 +0300

    add map filters

--- src/components/ClubsMap/index.js
+++ src/components/ClubsMap/index.js
@@ -3,16 +3,19 @@ import { Map, ObjectManager, YMaps } from 'react-yandex-maps';
 import StickyBox from 'react-sticky-box';
 import { Request } from 'utils/request';
 import CitiesFilter from '../Filters/CitiesFilter';
+import RegionsFilter from '../Filters/RegionsFilter';
 import CopyrightInfo from '../CopyrightInfo';
 import Aside from '../../components/Layouts/Aside';
 
-import './style.scss'
+import './style.scss';
 
 const ClubsMap = ({ fullScreen }) => {
     const [data, setData] = useState([]);
     const [cities, setCities] = useState([]);
+    const [regions, setRegions] = useState([]);
     const [loading, setLoading] = useState(true);
     const [targetCity, setTargetCity] = useState([1121]);
+    const [targetRegion, setTargetRegion] = useState([41]);
     const [targetCoords, setTargetCoords] = useState([55.76, 37.64]);
 
     const getYandexCities = async () => {
@@ -21,7 +24,7 @@ const ClubsMap = ({ fullScreen }) => {
         }, result => {
             setData(JSON.stringify(result));
         });
-    }
+    };
     const getCities = async () => {
         await Request({
             url: '/api/Club/cards_cities'
@@ -29,15 +32,22 @@ const ClubsMap = ({ fullScreen }) => {
             setCities(data);
         });
     };
+    const getRegions = async () => {
+        await Request({
+            url: '/api/Club/regions'
+        }, data => {
+            setRegions(data.map(r => ({ 'value': r.id, 'label': r.name })));
+        });
+    };
 
     useEffect(() => {
+        setLoading(true);
         (async () => {
-            setLoading(true);
             await getCities();
+            await getRegions();
             await getYandexCities();
-            setLoading(false);
         })();
-        console.log('cities', cities);
+        setLoading(false);
     }, []);
 
 
@@ -53,21 +63,39 @@ const ClubsMap = ({ fullScreen }) => {
     //     })();
     // },[targetCity, setTargetCity])
 
+
     console.log('targetCoords', targetCoords);
     console.log('targetCity', targetCity);
+    console.log('targetRegion', targetRegion);
+
+
+    const handleChangeCity = (event) => {
+        setTargetCity([event[1] ? event[1] : event[0]]);
+    };
+    const handleChangeRegion = (event) => {
+        console.log('рррррррррррррегионы',event);
+        setTargetRegion([event[1] ? event[1] : event[0]]);
+    };
+
 
     return (
-        <>
+        <div>
             <Aside className={'organizations-page__left'}>
                 <StickyBox offsetTop={60}>
                     <div className='organizations-page__filters'>
+                        <RegionsFilter
+                            loading={loading}
+                            regions={regions}
+                            region_ids={targetRegion}
+                            onChange={handleChangeRegion}
+                        />
                         <CitiesFilter
                             loading={loading}
                             cities={cities}
                             city_ids={targetCity}
-                            // setTargetCiti={setTargetCiti}
-                            onChange={setTargetCity}
+                            onChange={handleChangeCity}
                         />
+
                         <CopyrightInfo withSocials={true} />
                     </div>
                 </StickyBox>
@@ -94,7 +122,7 @@ const ClubsMap = ({ fullScreen }) => {
                     />
                 </Map>
             </YMaps>}
-        </>
+        </div>
     );
 };
 
