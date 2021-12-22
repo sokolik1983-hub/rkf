5ea2f7c5bd47d49642d9ca58c59ca89e966e072f
Jen <gk_ln@mail.ru>
2021-12-22 17:07:17 +0300

    task done

--- src/pages/MetricsDocPage/index.js
+++ src/pages/MetricsDocPage/index.js
@@ -5,26 +5,20 @@ import './index.scss';
 
 
 const MetricsDocPage = () => {
-
-    // const id = window.location.href.split('/shared/document/puppycard/');
     const url = window.location.href.match(/\w+/g);
     const id = url[url.length-1]
 
     const [docUrl, setDocUrl] = useState('');
-    console.log('id', id)
-
-    // /shared/document/puppycard/MQAzADQA
 
     useEffect(() => {
         fetchDoc(id);
     }, [])
 
     const fetchDoc = (id) => {
-        fetch(`api/shared/document/puppy_card?id=${id}`)
+        fetch(`/api/shared/document/puppy_card?id=${id}`)
             .then(res => res.blob())
             .then(data => URL.createObjectURL(data))
             .then(url => {
-                console.log('url', url);
                 setDocUrl(url);
             })
     }
@@ -33,16 +27,10 @@ const MetricsDocPage = () => {
         <Layout>
             <div className="metrics-doc-page">
                 <div className="metrics-doc-page__content">
-                    <h2 className="metrics-doc-page__heading">
-                        Неведомая хрень
-                    </h2>
-
                     <div className="metrics-doc-page__iframe-wrap">
                         <iframe className="metrics-doc-page__iframe"
                                 src={ docUrl }
-                                src='/'
-                                // src='blob:http://localhost:3000/3f880cc8-f871-48a5-9738-0da7a9ba89af'
-                                frameborder="0">
+                                title="metrics_document">
                         </iframe>
                     </div>
                 </div>
