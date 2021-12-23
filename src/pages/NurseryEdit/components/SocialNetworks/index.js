d53cefe81d1cb9568ece31296cbddcbdafe75af3
Demidov Vsevolod <demidov.vsevolod@uep24.ru>
2021-12-23 11:08:45 +0300

    rollback 2

--- src/pages/NurseryEdit/components/SocialNetworks/index.js
+++ src/pages/NurseryEdit/components/SocialNetworks/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import { connect, FieldArray } from "formik";
 import { FormField, FormGroup } from "components/Form";
 import Button from "components/Button";
+import Card from "components/Card";
 import "./styles.scss";
 
 const SocialNetworks = ({ socials }) => {
 
-    return <div className="SocialNetworks">
+    return <Card className="SocialNetworks">
+        <h3>Социальные сети</h3>
+
         <FieldArray
             name="socials"
             render={arrayHelpers => (
@@ -14,12 +17,12 @@ const SocialNetworks = ({ socials }) => {
                     {socials.map((social, index) => (
                         <FormGroup inline key={index}>
                             <FormField
-                                label={'Социальная сеть'}
+                                label={'Ссылка'}
                                 placeholder={'Введите ссылку'}
                                 name={`socials[${index}].site`}
                             />
                             <FormField
-                                // label={'Название'}
+                                label={'Название'}
                                 placeholder="Введите название"
                                 name={`socials[${index}].description`}
                             />
@@ -34,12 +37,12 @@ const SocialNetworks = ({ socials }) => {
                                 site: '',
                                 description: '',
                                 social_network_type_id: 1
-                            })}>Добавить социальную сеть</Button>
+                            })}>Добавить ссылку</Button>
                     </div>
                 </div>
             )}
         />
-    </div>
+    </Card>
 };
 
 export default connect(SocialNetworks);
