a5ccaea9085a8cb287441444b442d078a43bf2de
Jen <gk_ln@mail.ru>
2021-12-28 13:53:59 +0300

    changed the logic of receiving documents

--- src/components/CardNewsNew/DocumentLink/index.js
+++ src/components/CardNewsNew/DocumentLink/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { getHeaders } from '../../../utils/request';
 import { SvgIcon } from '@progress/kendo-react-common';
 import { filePdf } from '@progress/kendo-svg-icons';
@@ -9,10 +9,7 @@ import moment from 'moment';
 const DocumentLink = ({ docId, document }) => {
     const headers = getHeaders();
     const [url, setUrl] = useState('');
-
-    useEffect(() => {
-        getDocument();
-    }, []);
+    const linkRef = useRef();
 
     const getDocument = () => {
         if (!+docId) return;
@@ -20,29 +17,52 @@ const DocumentLink = ({ docId, document }) => {
         fetch(`/api/document/publicdocument?id=${docId}`, { headers })
             .then(res => res.blob())
             .then(data => URL.createObjectURL(data))
-            .then(url => setUrl(url));
+            .then(url => {
+                setUrl(url);
+                linkRef.current.click();
+            });
     };
+
+    const renderDocumentItem = () => {
+        return (
+            <>
+                <SvgIcon icon={filePdf} size="default" />
+                <div className="DocumentItem__name">
+                    {document.name}
+                    <span className="DocumentItem__date">
+                        {`Добавлено 
+                            ${moment(document.date_create).format("D MMMM YYYY")} в 
+                            ${moment(document.date_create).format("HH:mm")}`
+                        }
+                    </span>
+                </div>
+            </>
+
+        )
+    }
 
 
     return (
         !!docId &&
-        <a
-            className="DocumentItem__href"
-            href={url}
-            target="_blank"
-            rel="noreferrer noopener"
-        >
-            <SvgIcon icon={filePdf} size="default" />
-            <div className="DocumentItem__name">
-                {document.name}
-                <span className="DocumentItem__date">
-                    {`Добавлено 
-                        ${moment(document.date_create).format("D MMMM YYYY")} в 
-                        ${moment(document.date_create).format("HH:mm")}`
-                    }
-                </span>
-            </div>
-        </a>
+        <>
+            { !url ?
+                <span
+                    className="DocumentItem__href CardNewsNew__documents-item-name"
+                    onClick={ getDocument }
+                >
+                        {renderDocumentItem()}
+                    </span> :
+                <a
+                    className="DocumentItem__href"
+                    href={url}
+                    target="_blank"
+                    rel="noreferrer noopener"
+                    ref={ linkRef }
+                >
+                    {renderDocumentItem()}
+                </a> }
+        </>
+
     );
 };
 
