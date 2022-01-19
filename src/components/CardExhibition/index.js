6be6e15cb1104d66d357a7fc353f0923eb42403f
Nikita <akalina164@mail.ru>
2022-01-19 17:14:44 +0300

    fixing_LightToolTip_text

--- src/components/CardExhibition/index.js
+++ src/components/CardExhibition/index.js
@@ -22,6 +22,7 @@ const CardExhibition = ({
                             photo,
                             url,
                             ranks,
+                            ranksFull,
                             federation_name,
                             federation_link,
                             user,
@@ -103,7 +104,7 @@ const CardExhibition = ({
                         </div>
                         <div className="card-exhibition__rank">
                             <span className="card-exhibition__subtitle">Ранг</span>
-                            <LightTooltip title={ranks} enterDelay={200} leaveDelay={200}>
+                            <LightTooltip title={ranksFull} enterDelay={200} leaveDelay={200}>
                                 <p>{ranks}</p>
                             </LightTooltip>
                         </div>
