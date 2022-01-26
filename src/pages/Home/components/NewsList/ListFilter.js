61b991a4ec40b66d7e732ed9c12abad078d7ccba
Nikita <kalinichenko.nikita@uep24.ru>
2022-01-26 10:26:30 +0300

    starting_to_fix_code

--- src/pages/Home/components/NewsList/ListFilter.js
+++ src/pages/Home/components/NewsList/ListFilter.js
@@ -19,20 +19,20 @@ const ListFilter = ({changeFilter}) => {
             <div className="ListFilter__wrap">
                 <ul className="ListFilter">
                     <h3>Автор</h3>
-                    <li>
+                    <li className={activeType === null ? 'active' : undefined}>
                         <a href="/" name="rkf_and_federations" onClick={showAll} className={activeType === null ? 'active' : undefined}>Все</a>
                     </li>
-                    <li>
+                    <li className={activeType === 'rkf_and_federations' ? 'active' : undefined}>
                         <a href="/" name="rkf_and_federations" onClick={handleClick} className={activeType === 'rkf_and_federations' ? 'active' : undefined}>РКФ и Федерации</a>
                     </li>
-                    <li>
+                    <li className={activeType === 'clubs' ? 'active' : undefined}>
                         <a href="/" name="clubs" onClick={handleClick} className={activeType === 'clubs' ? 'active' : undefined}>Клубы</a>
                     </li>
-                    <li>
+                    <li className={activeType === 'nurseries' ? 'active' : undefined}>
                         <a href="/" name="nurseries" onClick={handleClick} className={activeType === 'nurseries' ? 'active' : undefined}>Питомники</a>
                     </li>
-                    <li>
-                        <span style={{ opacity: 0.5, cursor: 'default' }}>НКП</span>
+                    <li style={{ opacity: 0.5, cursor: 'default' }}>
+                        <span>НКП</span>
                     </li>
                 </ul>
             </div>
