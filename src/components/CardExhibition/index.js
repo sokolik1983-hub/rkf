92b0a7e0e5857d59da02b27a7662c98f7b535403
Nikita <akalina164@mail.ru>
2022-02-07 13:11:31 +0300

    fixing_share_for_exhibitioins

--- src/components/CardExhibition/index.js
+++ src/components/CardExhibition/index.js
@@ -142,7 +142,7 @@ const CardExhibition = ({
             <div className={`card-exhibition__controls`}>
                 <CardFooter
                     id={id}
-                    share_link={`https://rkf.online${url}`}
+                    share_link={window.location.host !== 'rkf.online' ? `https://rkf.online${url}` : `https://stage.uep24.ru${url}`}
                     is_liked={is_liked}
                     like_count={like_count}
                     likesOn={true}
