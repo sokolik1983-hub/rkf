f7c0a290b06d211ee11de9c28faa5826a8dc4f56
Nikita <akalina164@mail.ru>
2022-02-17 17:07:14 +0300

    fixing_task

--- src/pages/Specialists/components/Filters/components/ListFilter/index.js
+++ src/pages/Specialists/components/Filters/components/ListFilter/index.js
@@ -1,4 +1,4 @@
-import React, {memo, useMemo} from "react";
+import React, {memo, useMemo, useState} from "react";
 import SwipeTabs from "../../../../../../components/SwipeTabs";
 import {setFiltersToUrl, getEmptyFilters} from "../../../../utils";
 import CustomCheckbox from "../../../../../../components/Form/CustomCheckbox";
@@ -9,7 +9,7 @@ const ListFilter = ({
                         searchTypeId,
                         is_popular,
                         onChange
-}) => {
+                    }) => {
     const tabItems = useMemo(() => {
         return [
             {title: 'По породам', search_type: 4},
@@ -19,23 +19,41 @@ const ListFilter = ({
         ];
     }, []);
 
+    const [isFilter, setIsFilter] = useState(false)
+
+    const handleFilter = () => {
+        setIsFilter(!isFilter);
+        setFiltersToUrl({IsPopular: !isFilter})
+    }
+
     return (
         <div className="specialists-page__list-filter">
-            <h4 className="list-filter__title">Судьи и специалисты</h4>
-            <CustomCheckbox
-                id="most-liked"
-                label="По популярности"
-                checked={!!is_popular}
-                onChange={() => onChange(!is_popular)}
-            />
-            <SwipeTabs
-                items={tabItems}
-                activeTabIndex={tabItems.findIndex(item => item.search_type === searchTypeId)}
-                onChange={({search_type}) => setFiltersToUrl({
-                    ...getEmptyFilters(),
-                    SearchTypeId: search_type
-                })}
-            />
+            <div className="specialists-page__list-filter_header">
+                <h4 className="list-filter__title">Судьи и специалисты</h4>
+                <CustomCheckbox
+                    id="need-filter"
+                    label="Сортировка"
+                    checked={!!isFilter}
+                    onChange={handleFilter}
+                    cName="sorting-filter"
+                />
+            </div>
+
+            {!isFilter ? <SwipeTabs
+                    items={tabItems}
+                    activeTabIndex={tabItems.findIndex(item => item.search_type === searchTypeId)}
+                    onChange={({search_type}) => setFiltersToUrl({
+                        ...getEmptyFilters(),
+                        SearchTypeId: search_type
+                    })}
+                /> :
+                <CustomCheckbox
+                    id="most-liked"
+                    label="По популярности"
+                    checked={!!isFilter}
+                    onChange={handleFilter}
+                    cName="like-filter"
+                />}
         </div>
     )
 };
