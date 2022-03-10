8e11ddd20ceb9cc7965acb3e89f76e8d952f2ea2
Jen <gk_ln@mail.ru>
2022-03-10 20:03:39 +0300

    added alert for zip/unzip

--- src/pages/NewsFeed/components/NewsList/index.js
+++ src/pages/NewsFeed/components/NewsList/index.js
@@ -7,6 +7,7 @@ import NewsFeedItem from "../NewsFeedItem";
 import {Request} from "../../../../utils/request";
 import {DEFAULT_IMG} from "../../../../appConfig";
 import ControlMenu from "../ControlMenu";
+import Alert from "../../../../components/Alert";
 
 import "./styles.scss";
 
@@ -24,6 +25,7 @@ const NewsList = ({canEdit, activeCategoryId, notifySuccess, notifyError}) => {
     const [clearChecks, setClearChecks] = useState(false);
     const [isUnreadMessages, setIsUnreadMessages] = useState(false);
     const [isUpdateWithAllChecks, setIsUpdateWithAllChecks] = useState(false);
+    const [zipMessage, setZipMessage] = useState('');
 
     const allItemsIds = [];
     news.map(n => allItemsIds.push(n.id));
@@ -36,6 +38,7 @@ const NewsList = ({canEdit, activeCategoryId, notifySuccess, notifyError}) => {
         setClearChecks(false);
         setIsUnreadMessages(false);
         setIsUpdateWithAllChecks(false);
+        setZipMessage('');
 
         setStartElement(1);
         (() => getNews(1, true))();
@@ -209,6 +212,7 @@ const NewsList = ({canEdit, activeCategoryId, notifySuccess, notifyError}) => {
                     unsetAllChecks={unsetAllChecks}
                     startElement={startElement}
                     isUpdateWithAllChecks={isUpdateWithAllChecks}
+                    setZipMessage={setZipMessage}
                 />
 
                 <InfiniteScroll
@@ -246,6 +250,15 @@ const NewsList = ({canEdit, activeCategoryId, notifySuccess, notifyError}) => {
                     </div>
 
                 </InfiniteScroll>
+
+                {zipMessage &&
+                    <Alert
+                        text={zipMessage}
+                        autoclose={2}
+                        onOk={() => setZipMessage('')}
+                    />
+                }
+
             </>
             : <Card>
                 <div className="news-list__no-news">
