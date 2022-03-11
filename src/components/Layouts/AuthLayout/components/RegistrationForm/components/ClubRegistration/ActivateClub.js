33ba01f61785cc2dc891228167c2dee3838bf74f
Cherkoz <cherkozyanovvladislav@mail.ru>
2022-03-11 17:35:08 +0300

    code improvement

--- src/components/Layouts/AuthLayout/components/RegistrationForm/components/ClubRegistration/ActivateClub.js
+++ src/components/Layouts/AuthLayout/components/RegistrationForm/components/ClubRegistration/ActivateClub.js
@@ -65,10 +65,10 @@ const ActivateClub = ({club, history, logOutUser, close}) => {
     return loading ?
         <Loading/> :
         <div className="club-registration__activate">
-            <div className="club-registration__activate-close" onClick={() => close()}></div>
+            <button className="club-registration__activate-close" onClick={() => close()}/>
             <h4 className="club-registration__activate-header">{code === null ? 'Активация клуба' : 'Подтвердите ваш e-mail'}</h4>
             <div className="club-registration__activate-content">
-                <div className="club-registration__activate-content__img"></div>
+                <img className="club-registration__activate-content__img" src="/static/images/default/email.svg" alt=""/>
                 <div className="club-registration__activate-content__text">
                     {code === null ?
                         <>
