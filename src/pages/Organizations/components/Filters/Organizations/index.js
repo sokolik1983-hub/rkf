2c81548cff6d4dacba0b5a8fa2bc1a5dab60f34f
Jen <gk_ln@mail.ru>
2022-03-14 10:30:50 +0300

    fixed function

--- src/pages/Organizations/components/Filters/Organizations/index.js
+++ src/pages/Organizations/components/Filters/Organizations/index.js
@@ -14,8 +14,7 @@ const OrganizationsFilter = ({organization_type}) => {
     }
 
     const prepareFilters =  (typeId) => {
-        if (typeId !== 3 || typeId !== 4) {
-            console.log(typeId)
+        if (typeId !== 3 && typeId !== 4) {
             return getEmptyFilters();
         }
     }
