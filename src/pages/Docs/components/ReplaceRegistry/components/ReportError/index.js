ecb179cdb25e4d1a4a4e7e31bb571062975456c2
sokolov.aa <lionsbet@outlook.com>
2022-03-14 10:55:28 +0300

    решена ошибка с поведением кнопки сообщить об ошибке кинолога

--- src/pages/Docs/components/ReplaceRegistry/components/ReportError/index.js
+++ src/pages/Docs/components/ReplaceRegistry/components/ReportError/index.js
@@ -96,6 +96,8 @@ const ReportErrorForm = ({ id, setIsOpen, setNeedUpdateTable }) => {
 const ReportError = ({ id, onErrorReport, setNeedUpdateTable }) => {
     const [isModalOpen, setIsModalOpen] = useState(true);
 
+    console.log('isModalOpen', isModalOpen);
+
     const handleClose = () => {
         setIsModalOpen(false);
         onErrorReport(null);
