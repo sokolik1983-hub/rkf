1289ae4eb75253ab423da2f2b176ed871158a25e
Nikita <kalinichenko.nikita@uep24.ru>
2022-04-29 10:37:46 +0300

    fixing_code

--- src/components/InitialsAvatar/index.js
+++ src/components/InitialsAvatar/index.js
@@ -25,11 +25,10 @@ const InitialsAvatar = ({card, id, name}) => {
     const getInitialName = () => {
         let firstName;
         let lastName;
-        if(!name) {
+        if (!name) {
             firstName = useSelector(state => state.authentication.user_info.first_name ? state.authentication.user_info.first_name : state.authentication.user_info.name.split(' ')[0]).replace(/["-']/g, '');
             lastName = useSelector(state => state.authentication.user_info.last_name ? state.authentication.user_info.last_name : state.authentication.user_info.name.split(' ')[1]);
-        } else
-            if(name && name.split(' ').length > 1) {
+        } else if (name && name.split(' ').length > 1) {
             firstName = name.split(' ')[0].replace(/["-']/g, '');
             lastName = name.split(' ')[1];
         } else {
