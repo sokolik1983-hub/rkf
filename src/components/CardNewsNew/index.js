148d31d2aac94d868b772d0b4330a02375899495
Nikita <kalinichenko.nikita@uep24.ru>
2022-05-18 11:24:40 +0300

    fixing_code

--- src/components/CardNewsNew/index.js
+++ src/components/CardNewsNew/index.js
@@ -307,15 +307,13 @@ const CardNewsNew = forwardRef(({
                     <div className={`card-news-new__show-all${pictures?.length || !canCollapse ? ' _disabled' : ''}`}
                          onClick={() => setCollapsed(!collapsed)}>
                         {
-                            advert_type_id < 1 && !collapsed ? 'Подробнее...' : 'Свернуть'
+                            advert_type_id < 1 && (!collapsed ? 'Подробнее...' : 'Свернуть')
                         }
                     </div>
                     <div className={`card-news-new__show-all${!pictures || !pictures?.length ? ' _disabled' : ''}`}
                          onClick={() => setShowModal(true)}
                     >
-                            {
-                                'Подробнее...'
-                            }
+                        Подробнее...
                     </div>
                 </div>
                 {(pictures || video_link) &&
