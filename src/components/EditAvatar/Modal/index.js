985b0198682b7940c19bb559313a20322200a111
Jen <gk_ln@mail.ru>
2022-05-25 13:49:45 +0300

    change logic for avatar sizing

--- src/components/EditAvatar/Modal/index.js
+++ src/components/EditAvatar/Modal/index.js
@@ -14,15 +14,17 @@ const Modal = ({open, onClose, title, children, pageBanner}) => (
         <div className="avatar__header">
             <AppBar position="static" className="modal__top">
                 <Toolbar className="modal__title">
-                    <div className="avatar_icon"></div>
+                    <div className="avatar_icon"> </div>
                     <div className="avatar__title">{title}</div>
-                    <div className="avatar__close"><span className="modal-close" onClick={onClose}></span></div>
+                    <div className="avatar__close">
+                        <span className="modal-close" onClick={onClose}> </span>
+                    </div>
                 </Toolbar>
             </AppBar>
         </div>
         <div className="avatar__content">
             <DialogContent className="modal__content">
-            {children}
+                {children}
             </DialogContent>
         </div>
     </Dialog>
