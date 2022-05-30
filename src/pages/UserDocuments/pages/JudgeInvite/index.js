0b0ac44c11101856699332878f7ccbb6c2557ccd
Nikita <kalinichenko.nikita@uep24.ru>
2022-05-30 13:46:13 +0300

    adding_func_for_judges

--- src/pages/UserDocuments/pages/JudgeInvite/index.js
+++ src/pages/UserDocuments/pages/JudgeInvite/index.js
@@ -1,22 +1,59 @@
 import React, { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 import Loading from "../../../../components/Loading";
 import { Request, getHeaders } from "../../../../utils/request";
 import Card from "../../../../components/Card";
+import formatDate from "../../../../utils/formatDate";
+import {useSelector} from "react-redux";
+import Button from "../../../../components/Button";
 import "./index.scss";
 
 
 const JudgeInvite = ({ alias, userType }) => {
     const [loading, setLoading] = useState(true);
+    const [mainInfo, setMainInfo] = useState(null);
+    const [judgeStat, setJudgeStat] = useState(null);
+    const [judgeStatName, setJudgeStatName] = useState(null);
+    const [inviteId, setInviteId] = useState(null);
+    const [judgeId, setJudgeId] = useState(null);
+    const [judgeComm, setJudgeComm] = useState(null);
     const location = useLocation();
     const id = location.search.replace('?exhibitionId=', '');
+    const profileId = useSelector(state => state.authentication.profile_id);
+
+    const changeInviteStatus = async (x) => {
+        await Request({
+            url: `/api/exhibitions/invite/change_judge_status`,
+            method: 'PUT',
+            data: JSON.stringify({
+                invite_id : inviteId,
+                judge_id: judgeId,
+                judge_invite_status: x,
+                judge_invite_comment : judgeComm,
+            })
+        })
+    }
 
     useEffect(() => {
         (() => Request({
             url: `/api/exhibitions/invite?exhibitionId=${id}`,
             headers: getHeaders(),
         }, data => {
-            console.log('data', data)
+            console.log('data', data, 'id', profileId, alias, data.nbc_alias);
+            setMainInfo(data);
+
+            for (let i = 0; i < data.invited_judges_groups?.length; i++) {
+                for (let j = 0; j < data.invited_judges_groups[i].invited_judges.length; j++) {
+                    if (data.invited_judges_groups[i].invited_judges[j].judge_alias === alias) {
+                        setJudgeStat(data.invited_judges_groups[i].invited_judges[j].judge_invite_status) ;
+                        setJudgeStatName(data.invited_judges_groups[i].invited_judges[j].judge_invite_status_name) ;
+                        setInviteId(data.invited_judges_groups[i].invited_judges[j].invite_id) ;
+                        setJudgeId(data.invited_judges_groups[i].invited_judges[j].judge_id) ;
+                    } else {
+                        console.log("ошибочка");
+                    }
+                }
+            }
 
             setLoading(false);
         }, error => {
@@ -25,10 +62,42 @@ const JudgeInvite = ({ alias, userType }) => {
         }))();
     }, []);
 
+    // Григорьевич Александр Малышевский
+    //
+
     return loading ?
         <Loading /> :
         <Card className="exhibitions-invite">
-            пурум пум пум judge
+            {/*кнопка ЛК, "Авторизация на участие в мероприятии"*/}
+            <div className="user-documents-status__head">
+                <Link className="btn-backward" to={`/user/${alias}/documents/exhibitions/invite/registry`}>Личный кабинет</Link>
+                &nbsp;/&nbsp;
+                Авторизация на участие в мероприятии
+            </div>
+            {/*инфа о клубе, выставке*/}
+            <div className="exhibitions-invite__main-info">
+                <span>Клуб: <a href={`/club/${mainInfo.club_alias}`}>{mainInfo.club_name}</a></span>
+                <span>Мероприятие: <a href={`/exhibitions/${mainInfo.exhibition_id}`}>{mainInfo.exhibition_name}</a></span>
+                <span>Город проведения выставки: {mainInfo.exhibition_city}</span>
+                <span>Дата начала: {formatDate(mainInfo.exhibition_date_start)}</span>
+                <span>Дата окончания: {formatDate(mainInfo.exhibition_date_end)}</span>
+                <span>НКП: <a href={`/exhibitions/${mainInfo.nbc_alias}`}>{mainInfo.nbc_name}</a> {judgeStat === 2 ? 'Ваше участие согласовано' : 'В авторизации отказано'}</span>
+            </div>
+            {/*причина, если отказ*/}
+            {judgeStat !== 2 &&
+                <div>
+                Причина {judgeStatName}
+                </div>
+            }
+            {/*кнопка ОК, если отказ||Принять приглашение и отклонить приглашение, если согласовано*/}
+            <div>
+                {judgeStat !== 2 ? <Button>Ок</Button> :
+                    <div>
+                        <Button onClick={() => changeInviteStatus(2)}>Принять</Button>
+                        <Button onClick={() => changeInviteStatus(3)}>Отклонить</Button>
+                    </div>}
+            </div>
+
         </Card>
 };
 
