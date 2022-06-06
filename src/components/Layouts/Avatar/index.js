00d67162aff31052ec2685fa326eac870a8b5555
Кошиц Евгения <koshits.evgenia@uep24.ru>
2022-06-06 08:46:39 +0000

    Revert "Merge branch 'bugfix/avatar_bug' into 'development'"
    This reverts merge request !1140

--- src/components/Layouts/Avatar/index.js
+++ src/components/Layouts/Avatar/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useState } from "react";
 import { CSSTransition } from "react-transition-group";
 import { SvgIcon } from "@progress/kendo-react-common";
 import { pencil, trash } from "@progress/kendo-svg-icons";
@@ -13,298 +13,287 @@ import { isFederationAlias } from "../../../utils";
 
 
 const Avatar = ({
-                    alias,
-                    canEdit,
-                    card,
-                    data,
-                    id,
-                    logo,
-                    name,
-                    open,
-                    subclass,
-                    updateInfo,
-                    userType,
-                }) => {
+    alias,
+    canEdit,
+    card,
+    data,
+    id,
+    logo,
+    name,
+    open,
+    subclass,
+    updateInfo,
+    userType,
+}) => {
 
-    const [hover, setHover] = useState(false);
-    const [modalType, setModalType] = useState('');
-    const [logoSize, setLogoSize] = useState({});
-
-    const logoRef = useRef();
-
-    useEffect(() => {
-        logoRef.current && setLogoSize({
-            width: logoRef.current.clientWidth,
-            height: logoRef.current.clientHeight,
-        })
-    }, [logoRef]);
+const [hover, setHover] = useState(false);
+const [modalType, setModalType] = useState('');
 
     switch (data) {
         case 'canEdit':
-            return (
-                <div className={ logo ? 'user-info__logo-wrap' : 'user-info__logo-wrap empty' }
-                     onMouseEnter={ () => setHover(true) }
-                     onMouseLeave={ () => setHover(false) }
-                     onTouchStart={ () => {
+            return(
+                <div className={logo ? 'user-info__logo-wrap' : 'user-info__logo-wrap empty'}
+                     onMouseEnter={() => setHover(true)}
+                     onMouseLeave={() => setHover(false)}
+                     onTouchStart={() => {
                          setHover(true);
                          setTimeout(() => setHover(false), 3000);
-                     } }
+                     }}
                 >
-                    { canEdit &&
-                    <>
-                        <CSSTransition
-                            classNames="user-info__transition"
-                            in={ hover }
-                            timeout={ 350 }
-                            unmountOnExit
-                        >
-                            <LightTooltip
-                                title="Редактировать"
-                                enterDelay={ 200 }
-                                leaveDelay={ 200 }
+                    {canEdit &&
+                        <>
+                            <CSSTransition
+                                classNames="user-info__transition"
+                                in={hover}
+                                timeout={350}
+                                unmountOnExit
                             >
-                                <button
-                                    className="user-info__edit-btn"
-                                    type="button"
-                                    onClick={ () => setModalType('edit') }
+                                <LightTooltip
+                                    title="Редактировать"
+                                    enterDelay={200}
+                                    leaveDelay={200}
                                 >
-                                    <SvgIcon icon={ pencil } size="default"/>
-                                </button>
-                            </LightTooltip>
-                        </CSSTransition>
-                        { logo &&
-                        <CSSTransition
-                            classNames="user-info__transition"
-                            in={ hover }
-                            timeout={ 350 }
-                            unmountOnExit
-                        >
-                            <LightTooltip
-                                title="Удалить"
-                                enterDelay={ 200 }
-                                leaveDelay={ 200 }
-                            >
-                                <button
-                                    className="user-info__delete-btn"
-                                    type="button"
-                                    onClick={ () => setModalType('delete') }
+                                    <button
+                                        className="user-info__edit-btn"
+                                        type="button"
+                                        onClick={() => setModalType('edit')}
+                                    >
+                                        <SvgIcon icon={pencil} size="default"/>
+                                    </button>
+                                </LightTooltip>
+                            </CSSTransition>
+                            {logo &&
+                                <CSSTransition
+                                    classNames="user-info__transition"
+                                    in={hover}
+                                    timeout={350}
+                                    unmountOnExit
                                 >
-                                    <SvgIcon icon={ trash } size="default"/>
-                                </button>
-                            </LightTooltip>
-                        </CSSTransition>
-                        }
-                    </>
+                                    <LightTooltip
+                                        title="Удалить"
+                                        enterDelay={200}
+                                        leaveDelay={200}
+                                    >
+                                        <button
+                                            className="user-info__delete-btn"
+                                            type="button"
+                                            onClick={() => setModalType('delete')}
+                                        >
+                                            <SvgIcon icon={trash} size="default"/>
+                                        </button>
+                                    </LightTooltip>
+                                </CSSTransition>
+                            }
+                        </>
                     }
-                    { logo ?
-                        <img className="user-info__logo" src={ logo } alt="logo" ref={ logoRef }/>
+                    {logo ?
+                        <img className="user-info__logo" src={logo} alt="logo"/>
                         :
                         (userType === 3) ?
-                            <img className="user-info__logo" src={ DEFAULT_IMG.clubAvatar } alt="logo"/>
+                            <img className="user-info__logo" src={DEFAULT_IMG.clubAvatar} alt="logo"/>
                             :
                             isFederationAlias(alias) ?
-                                <img className="user-info__logo" src={ DEFAULT_IMG.userAvatar } alt="logo"/>
+                                <img className="user-info__logo" src={DEFAULT_IMG.userAvatar} alt="logo"/>
                                 :
                                 <InitialsAvatar
-                                    card={ card }
-                                    name={ name }
+                                    card={card}
+                                    name={name}
                                 />
                     }
-                    { modalType === 'edit' &&
-                    <EditAvatar
-                        setModalType={ setModalType }
-                        avatar={ logo }
-                        pageBanner={ false }
-                        name={ name }
-                        size={ logoSize }
-                    />
+                    {modalType === 'edit' &&
+                        <EditAvatar
+                            setModalType={setModalType}
+                            avatar={logo}
+                            pageBanner={false}
+                            name={name}
+                        />
                     }
-                    { modalType === 'delete' &&
-                    <ModalDeleteAvatar
-                        closeModal={ () => setModalType('') }
-                        updateInfo={ updateInfo }
-                        pageBanner={ false }
-                    />
+                    {modalType === 'delete' &&
+                        <ModalDeleteAvatar
+                            closeModal={() => setModalType('')}
+                            updateInfo={updateInfo}
+                            pageBanner={false}
+                        />
                     }
                 </div>
             );
 
         case 'logo':
-            return (
+            return(
                 <>
-                    { logo ?
-                        <div className={ `widget-login__${ subclass } ${ open && ' _active' }` }
-                             style={ { backgroundImage: `url(${ logo })` } }
+                    {logo ?
+                        <div className={`widget-login__${subclass} ${open && ' _active'}`}
+                             style={{backgroundImage: `url(${logo})`}}
                         />
                         :
                         (userType === 1 || userType === 4 || userType === 7) ?
-                            <div className={ `widget-login__${ subclass } ${ open && ' _active' }` }>
+                            <div className={`widget-login__${subclass} ${open && ' _active'}`}>
                                 <InitialsAvatar
-                                    card={ card }
-                                    name={ userType === 1 ? getInitials(name) : name }
+                                    card={card}
+                                    name={userType === 1 ? getInitials(name) : name}
                                 />
                             </div>
                             :
-                            <div className={ `widget-login__${ subclass } ${ open && ' _active' }` }
-                                 style={ { backgroundImage: `url(${ DEFAULT_IMG.clubAvatar })` } }
+                            <div className={`widget-login__${subclass} ${open && ' _active'}`}
+                                 style={{backgroundImage: `url(${DEFAULT_IMG.clubAvatar})`}}
                             />
                     }
                 </>
             );
 
         case 'nkp':
-            return (
+            return(
                 <>
-                    { logo ?
+                    {logo ?
                         <span className="card-organization__logo"
-                              style={ { backgroundImage: `url(${ logo || DEFAULT_IMG.clubAvatar })` } }
+                              style={{ backgroundImage: `url(${ logo || DEFAULT_IMG.clubAvatar })`}}
                         />
                         :
                         <InitialsAvatar
-                            id={ id }
-                            card={ card }
-                            name={ name }
+                            id={id}
+                            card={card}
+                            name={name}
                         />
                     }
                 </>
             );
 
         case 'organization':
-            return (
+            return(
                 <>
-                    { (userType === 3 || userType === 5) ?
-                        <img src={ logo || DEFAULT_IMG.clubAvatar } alt="logo"/>
+                    {(userType === 3 || userType === 5) ?
+                        <img src={logo || DEFAULT_IMG.clubAvatar} alt="logo"/>
                         :
                         logo ?
-                            <img src={ logo } alt="logo"/>
+                            <img src={logo} alt="logo"/>
                             :
                             <InitialsAvatar
-                                id={ id }
-                                card={ card }
-                                name={ name }
+                                id={id}
+                                card={card}
+                                name={name}
                             />
                     }
                 </>
             );
 
         case 'header':
-            return (
+            return(
                 <>
-                    { logo ?
-                        <img src={ logo } className={ subclass } alt="logo"/>
+                    {logo ?
+                        <img src={logo} className={subclass} alt="logo" />
                         :
                         <InitialsAvatar
-                            card={ card }
-                            name={ name }
+                            card={card}
+                            name={name}
                         />
                     }
                 </>
             );
 
         case 'article':
-            return (
+            return(
                 <>
-                    { logo && logo !== "/static/icons/default/default_avatar.svg" ?
+                    {logo && logo !== "/static/icons/default/default_avatar.svg" ?
                         <ClientAvatar
-                            size={ 40 }
-                            avatar={ logo }
+                            size={40}
+                            avatar={logo}
                         />
                         :
                         (userType === 4 || userType === 1 || userType === 7) ?
                             <InitialsAvatar
-                                card={ card }
-                                name={ name }
+                                card={card}
+                                name={name}
                             />
                             :
                             <ClientAvatar
-                                size={ 40 }
-                                avatar={ "/static/icons/default/club-avatar-new.png" }
+                                size={40}
+                                avatar={"/static/icons/default/club-avatar-new.png"}
                             />
                     }
                 </>
             );
 
         case 'mobile-user-menu':
-            return (
+            return(
                 <>
-                    { logo ?
-                        <img src={ logo } alt="menu-logo"/>
+                    {logo ?
+                        <img src={logo} alt="menu-logo" />
                         :
                         userType === 3 ?
-                            <img src={ '/static/icons/default/club-avatar.svg' } alt="menu-logo"/>
+                            <img src={'/static/icons/default/club-avatar.svg'} alt="menu-logo" />
                             :
                             <InitialsAvatar
-                                card={ card }
-                                name={ name }
+                                card={card}
+                                name={name}
                             />
                     }
                 </>
             );
 
         case 'cardnewsnew':
-            return (
+            return(
                 <>
-                    { logo ?
+                    {logo ?
                         <div className="card-news-new__left-logo"
-                             style={ { background: `url(${ logo }) center center/cover no-repeat` } }
+                             style={{background: `url(${logo}) center center/cover no-repeat`}}
                         />
                         :
                         (userType === 1 || userType === 4 || userType === 7) ?
                             <div className="card-news-new__left-logo">
                                 <InitialsAvatar
-                                    card={ card }
-                                    name={ name }
+                                    card={card}
+                                    name={name}
                                 />
                             </div>
                             :
                             <div className="card-news-new__left-logo"
-                                 style={ { background: `url(${ DEFAULT_IMG.clubAvatar }) center center/cover no-repeat` } }
+                                 style={{background: `url(${DEFAULT_IMG.clubAvatar}) center center/cover no-repeat`}}
                             />
                     }
                 </>
             );
 
         case 'nursery-docs':
-            return (
+            return(
                 <>
-                    { logo || userType === 3 ?
+                    {logo || userType === 3 ?
                         <div className="top-component__logo"
-                             style={ { backgroundImage: `url(${ logo || DEFAULT_IMG.clubAvatar })` } }
+                             style={{ backgroundImage: `url(${logo || DEFAULT_IMG.clubAvatar})` }}
                         />
                         :
                         <InitialsAvatar
-                            card={ card }
-                            name={ name }
+                            card={card}
+                            name={name}
                         />
                     }
                 </>
             );
 
         case 'specialist-card':
-            return (
+            return(
                 <>
-                    { logo
+                    {logo
                         ?
                         <span className="card-specialists__photo"
-                              to={ logo }
-                              style={ { backgroundImage: `url(${ logo })` } }
+                              to={logo}
+                              style={{ backgroundImage: `url(${logo})` }}
                         />
                         :
                         <InitialsAvatar
-                            id={ id }
-                            card={ card }
-                            name={ name }
+                            id={id}
+                            card={card}
+                            name={name}
                         />
                     }
                 </>
             );
 
         default:
-            return (
+            return(
                 <InitialsAvatar
-                    id={ id }
-                    card={ card }
-                    name={ name }
+                    id={id}
+                    card={card}
+                    name={name}
                 />
             );
     }
