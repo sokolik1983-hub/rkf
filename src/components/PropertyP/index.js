c67f674400afa79747759c14c6a139b883acfc2c
Vsevolod <demidov.vsevolod@uep24.ru>
2022-06-16 14:19:22 +0300

    style fix

--- src/components/PropertyP/index.js
+++ src/components/PropertyP/index.js
@@ -5,10 +5,9 @@ import {Link} from "react-router-dom";
 export default ({ name, value, email }) =>
     <p className="PropertyP">
         <span className="PropertyP__name">{name}: </span>
-        {email &&
+        {email ?
             <Link to={`mailto:${value}`}><span className="PropertyP__value">{value}</span></Link>
-        }
-        {!email &&
+            :
             <span className="PropertyP__value">{value}</span>
         }
     </p>
