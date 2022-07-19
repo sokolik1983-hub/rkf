40f959ebd6fd982e18260a6564a9129f91c6cebf
sokolov.aa <lionsbet@outlook.com>
2022-07-19 12:22:17 +0300

    поправлено поведение меню на главной странице

--- src/components/MenuComponentNew/components/HeaderMobileMenu.js
+++ src/components/MenuComponentNew/components/HeaderMobileMenu.js
@@ -4,6 +4,9 @@ import Avatar from "../../Layouts/Avatar";
 
 
 const HeaderMobileMenu = ({currentPageUserInfo, userType}) => {
+
+    console.log('currentPageUserInfo', currentPageUserInfo);
+
     const getLinkForName = () => {
         switch(currentPageUserInfo?.user_type || userType) {
             case 7:
