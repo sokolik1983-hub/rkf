c2808dfa925e551bd81b783ac7f25df413130854
sokolov.aa <lionsbet@outlook.com>
2022-07-19 14:17:53 +0300

    поправлено прыгающее меню на странице ркф

--- src/components/MenuComponentNew/index.js
+++ src/components/MenuComponentNew/index.js
@@ -210,8 +210,7 @@ const MenuComponentNew = () => {
     useEffect(() => {
         if(url === 'client' || (isFederationAlias(url) && currentPageNav) || currentPageUserInfo?.id === 1) {
             if(currentPageUserInfo?.club_alias === 'rkf' || currentPageUserInfo?.club_alias === 'rkf-online') {
-                const newNavWithoutDocLinks = currentPageNav.filter(item =>(item.id !== 7 && item.id !== 8));
-                setCurrentPageNav(newNavWithoutDocLinks);
+                setCurrentPageNav(currentPageNav);
             } else {
                 //На странице документов личного кабинета федерации, цеплялись ссылки от пдф документов основного меню, поэтому ставим условие
                 const newNavWithDocLinks = (linkAlias === 'documents') ?
